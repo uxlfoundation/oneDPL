1a783bc14dd924c4e87d9d4af83eff028a6e04e2
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-07 11:10:21 +0100

    Fix walk1_vector_or_scalar instantiations

--- include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
@@ -58,7 +58,7 @@ __pattern_walk1(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _ForwardIt
 
     oneapi::dpl::__par_backend_hetero::__parallel_for(
         _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-        unseq_backend::walk1_vector_or_scalar<_ExecutionPolicy, _Function, decltype(__buf.all_view())>{
+        unseq_backend::walk1_vector_or_scalar<std::decay_t<_ExecutionPolicy>, _Function, decltype(__buf.all_view())>{
             __f, static_cast<std::size_t>(__n)},
         __n, __buf.all_view())
         .__deferrable_wait();
