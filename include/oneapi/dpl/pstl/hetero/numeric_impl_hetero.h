a343120ed37bdec3f67dd9f18407a3f4c20007d8
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-13 14:06:23 +0100

    Fix specializations of __brick_copy

--- include/oneapi/dpl/pstl/hetero/numeric_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/numeric_impl_hetero.h
@@ -237,6 +237,7 @@ __pattern_adjacent_difference([[maybe_unused]] __hetero_tag<_BackendTag> __tag, 
 
     using _It1ValueT = typename ::std::iterator_traits<_ForwardIterator1>::value_type;
     using _It2ValueTRef = typename ::std::iterator_traits<_ForwardIterator2>::reference;
+    using _DecayedExecutionPolicy = std::decay_t<_ExecutionPolicy>;
 
     _ForwardIterator2 __d_last = __d_first + __n;
 
@@ -248,7 +249,7 @@ __pattern_adjacent_difference([[maybe_unused]] __hetero_tag<_BackendTag> __tag, 
             ::std::forward<_ExecutionPolicy>(__exec));
 
         __internal::__pattern_walk2_brick(__tag, __wrapped_policy, __first, __last, __d_first,
-                                          __internal::__brick_copy<__hetero_tag<_BackendTag>, _ExecutionPolicy>{});
+                                          __internal::__brick_copy<__hetero_tag<_BackendTag>, _DecayedExecutionPolicy>{});
     }
     else
 #endif
