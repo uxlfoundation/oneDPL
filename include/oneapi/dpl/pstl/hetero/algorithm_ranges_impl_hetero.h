e0d234f0935b17e2b9aa009e038d9d61f3aa2090
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-03-17 16:16:58 +0100

    [oneDPL][ranges][M] text typo fix

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -5,7 +5,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// This file incorporates work covered by the following copyright and permismission
+// This file incorporates work covered by the following copyright and permission
 // notice:
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
