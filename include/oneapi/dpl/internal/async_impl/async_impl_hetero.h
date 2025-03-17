aa72a15dc73f0dd6a60ba1ae69387de8fbc134ae
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-17 12:27:31 +0100

    Fix specializations of walk3_vectors_or_scalars

--- include/oneapi/dpl/internal/async_impl/async_impl_hetero.h
+++ include/oneapi/dpl/internal/async_impl/async_impl_hetero.h
@@ -97,7 +97,7 @@ __pattern_walk3_async(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _For
 
     auto __future = oneapi::dpl::__par_backend_hetero::__parallel_for(
         _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-        unseq_backend::walk3_vectors_or_scalars<_ExecutionPolicy, _Function, decltype(__buf1.all_view()),
+        unseq_backend::walk3_vectors_or_scalars<std::decay_t<_ExecutionPolicy>, _Function, decltype(__buf1.all_view()),
                                                 decltype(__buf2.all_view()), decltype(__buf3.all_view())>{
             __f, static_cast<size_t>(__n)},
         __n, __buf1.all_view(), __buf2.all_view(), __buf3.all_view());
