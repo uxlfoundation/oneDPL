237e1d54e6f14c360386387abb56a2af7fa4564c
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-21 16:37:44 +0100

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h - rename  __result_and_scratch_storage_impl to __result_and_scratch_storage

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -533,7 +533,7 @@ struct __result_and_scratch_storage_base
 };
 
 template <typename _T>
-struct __result_and_scratch_storage_impl : __result_and_scratch_storage_base
+struct __result_and_scratch_storage : __result_and_scratch_storage_base
 {
   private:
     using __sycl_buffer_t = sycl::buffer<_T, 1>;
@@ -581,7 +581,7 @@ struct __result_and_scratch_storage_impl : __result_and_scratch_storage_base
     }
 
   public:
-    __result_and_scratch_storage_impl(sycl::queue __q, std::size_t __result_n, std::size_t __scratch_n)
+    __result_and_scratch_storage(sycl::queue __q, std::size_t __result_n, std::size_t __scratch_n)
         : __q{__q}, __result_n{__result_n}, __scratch_n{__scratch_n},
           __use_USM_host{__use_USM_host_allocations(__q)},
           __supports_USM_device{__use_USM_allocations(__q)}
@@ -726,9 +726,6 @@ struct __result_and_scratch_storage_impl : __result_and_scratch_storage_base
     }
 };
 
-template <typename _T>
-using __result_and_scratch_storage = __result_and_scratch_storage_impl<_T>;
-
 // Tag __async_mode describe a pattern call mode which should be executed asynchronously
 struct __async_mode
 {
@@ -758,12 +755,9 @@ class __future : private std::tuple<_Args...>
         return __buf.get_host_access(sycl::read_only)[0];
     }
 
-    // Here we use __result_and_scratch_storage_impl rather than __result_and_scratch_storage because we need to
-    // match the type with the overload and are deducing the policy type. If we used __result_and_scratch_storage,
-    // it would cause issues in type deduction due to decay of the policy in that using statement.
     template <typename _T>
     constexpr auto
-    __wait_and_get_value(__result_and_scratch_storage_impl<_T>& __storage)
+    __wait_and_get_value(__result_and_scratch_storage<_T>& __storage)
     {
         return __storage.__wait_and_get_value(__my_event);
     }
