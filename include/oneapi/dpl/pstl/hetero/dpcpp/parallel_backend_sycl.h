8e205c8c26d549b6e5ef1889ceb1bb48238a499f
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-03-22 15:25:24 -0400

    removing some debug output
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1862,7 +1862,6 @@ __parallel_set_op(oneapi::dpl::__internal::__device_backend_tag __backend_tag, _
 {
     if constexpr(_SetTag::__is_one_shot_v)
     {
-        std::cout<<"parallel_set_reduce_then_scan\n";
         return __parallel_set_reduce_then_scan(__backend_tag, std::forward<_ExecutionPolicy>(__exec),
                                                std::forward<_Range1>(__rng1), std::forward<_Range2>(__rng2),
                                                std::forward<_Range3>(__result), __comp, __set_tag);
@@ -1870,7 +1869,6 @@ __parallel_set_op(oneapi::dpl::__internal::__device_backend_tag __backend_tag, _
     }
     else
     {
-        std::cout<<"parallel_set_scan\n";
         return __parallel_set_scan(__backend_tag, std::forward<_ExecutionPolicy>(__exec), std::forward<_Range1>(__rng1),
                                 std::forward<_Range2>(__rng2), std::forward<_Range3>(__result), __comp,
                                 __set_tag);
