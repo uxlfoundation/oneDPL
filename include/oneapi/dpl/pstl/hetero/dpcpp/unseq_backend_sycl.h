8ecc426f120cf30f591c8e342db7abde63cc8d96
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-25 10:40:11 +0100

    struct multiple_match_pred - remove ExecutionPolicy

--- include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
@@ -652,11 +652,9 @@ struct single_match_pred : single_match_pred_by_idx<walk_n<_Pred>>
     single_match_pred(_Pred __p) : single_match_pred_by_idx<walk_n<_Pred>>{__p} {}
 };
 
-template <typename _DecayedExecutionPolicy, typename _Pred>
+template <typename _Pred>
 struct multiple_match_pred
 {
-    static_assert(std::is_same_v<_DecayedExecutionPolicy, std::decay_t<_DecayedExecutionPolicy>>);
-
     _Pred __pred;
 
     template <typename _Idx, typename _Acc1, typename _Acc2>
