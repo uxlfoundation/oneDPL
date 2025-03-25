7f5cb1cbe30dfd4bd2e34282cf193d3178ac485d
Matthew Michel <matthew.michel@intel.com>
2025-03-25 15:27:06 -0500

    Initial naive implementation of walk_n_vectors_or_scalars
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -58,34 +58,11 @@ __pattern_walk_n(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Function
     const _Size __n = std::min({_Size(__rngs.size())...});
     if (__n > 0)
     {
-        constexpr std::size_t __num_ranges = sizeof...(_Ranges);
-        static_assert(__num_ranges <= 3, "__pattern_walk_n only supports up to three packed range parameters");
-        if constexpr (__num_ranges == 1)
-        {
-            oneapi::dpl::__par_backend_hetero::__parallel_for(
-                _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-                unseq_backend::walk1_vector_or_scalar<_ExecutionPolicy, _Function>{__f, static_cast<std::size_t>(__n)},
-                __n, std::forward<_Ranges>(__rngs)...)
-                .__deferrable_wait();
-        }
-        else if constexpr (__num_ranges == 2)
-        {
-            oneapi::dpl::__par_backend_hetero::__parallel_for(
-                _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-                unseq_backend::walk2_vectors_or_scalars<_ExecutionPolicy, _Function>{__f,
-                                                                                     static_cast<std::size_t>(__n)},
-                __n, std::forward<_Ranges>(__rngs)...)
-                .__deferrable_wait();
-        }
-        else
-        {
-            oneapi::dpl::__par_backend_hetero::__parallel_for(
-                _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-                unseq_backend::walk3_vectors_or_scalars<_ExecutionPolicy, _Function>{__f,
-                                                                                     static_cast<std::size_t>(__n)},
-                __n, std::forward<_Ranges>(__rngs)...)
-                .__deferrable_wait();
-        }
+        oneapi::dpl::__par_backend_hetero::__parallel_for(
+            _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
+            unseq_backend::walk_n_vectors_or_scalars<_ExecutionPolicy, _Function>{__f, static_cast<std::size_t>(__n)},
+            __n, std::forward<_Ranges>(__rngs)...)
+            .__deferrable_wait();
     }
     return __n;
 }
@@ -654,8 +631,8 @@ __pattern_unique_copy(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Ran
             _BackendTag{},
             oneapi::dpl::__par_backend_hetero::make_wrapped_policy<__copy_wrapper>(
                 std::forward<_ExecutionPolicy>(__exec)),
-            unseq_backend::walk2_vectors_or_scalars<_ExecutionPolicy, _CopyBrick>{_CopyBrick{},
-                                                                                  static_cast<std::size_t>(__n)},
+            unseq_backend::walk_n_vectors_or_scalars<_ExecutionPolicy, _CopyBrick>{_CopyBrick{},
+                                                                                   static_cast<std::size_t>(__n)},
             __n, std::forward<_Range1>(__rng), std::forward<_Range2>(__result))
             .get();
 
