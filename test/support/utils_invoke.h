1687c4e6dbb95442f471132e594a34a4c6da3c6b
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-26 09:31:22 +0100

    test/support/utils_invoke.h - additional changes in struct policy_container

--- test/support/utils_invoke.h
+++ test/support/utils_invoke.h
@@ -123,24 +123,21 @@ struct policy_container
     {
     }
 
-    template <typename _PolicySrc = _Policy>
-    std::enable_if_t<std::is_rvalue_reference_v<_PolicySrc>, _DecayedPolicy&&>
-    get()
+    auto get() &&
     {
         // We can move policy only once
         assert(!__moved_out);
-        __moved_out = true;
 
-        // Return policy as r-value
-        return std::move(__policy);
-    }
-
-    template <typename _PolicySrc = _Policy>
-    std::enable_if_t<!std::is_rvalue_reference_v<_PolicySrc>, const _DecayedPolicy&>
-    get()
-    {
-        // Return policy as l-value
-        return __policy;
+        if constexpr (std::is_rvalue_reference_v<_Policy>)
+        {
+            // Return policy as r-value
+            return std::move(__policy);
+        }
+        else
+        {
+            // Return policy as l-value
+            return __policy;
+        }
     }
 };
 
