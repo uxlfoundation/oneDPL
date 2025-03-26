6486a70c00537621ba6c0328b92324e5de3fcfe7
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-03-25 22:47:02 -0400

    bugfix
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1083,13 +1083,14 @@ struct __gen_set_op_from_known_balanced_path
         _SizeType __i_elem = __id * __diagonal_spacing;
         if (__i_elem >= __rng1.size() + __rng2.size())
             return std::make_tuple(std::uint32_t{0}, std::uint16_t{0});
-        _SizeType __star_offset = std::signbit(__rng1_temp_diag[__id])? 1 : 0;
-        _SizeType __rng2_temp_diag = __i_elem - std::abs(__rng1_temp_diag[__id]) + __star_offset;
+        bool __star_offset = std::signbit(__rng1_temp_diag[__id]);
+        auto __rng1_temp_diag_abs = std::abs(__rng1_temp_diag[__id]);
+        auto __rng2_temp_diag = __i_elem - __rng1_temp_diag_abs + __star_offset;
 
-        _SizeType __eles_to_process = std::min(_SizeType{__diagonal_spacing} - __star_offset,
+        _SizeType __eles_to_process = std::min(_SizeType{__diagonal_spacing} - _SizeType{__star_offset},
             __rng1.size() + __rng2.size() - (__i_elem - 1));
         
-        std::uint16_t __count = __set_op_count(__rng1, __rng2, __rng1_temp_diag[__id], __rng2_temp_diag, __eles_to_process,
+        std::uint16_t __count = __set_op_count(__rng1, __rng2, __rng1_temp_diag_abs, __rng2_temp_diag, __eles_to_process,
                                                __output_data, __comp);
         return std::make_tuple(std::uint32_t{__count}, __count);
     }
