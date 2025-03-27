35ae4439170aac3f4d27a431f73dd7d7a8c579cc
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-27 14:15:58 +0100

    Remove _ExecutionPolicy from multiple_match_pred

--- include/oneapi/dpl/pstl/hetero/dpcpp/sycl_traits.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/sycl_traits.h
@@ -391,7 +391,7 @@ struct single_match_pred_by_idx;
 template <typename _Pred>
 struct single_match_pred;
 
-template <typename _ExecutionPolicy, typename _Pred>
+template <typename _Pred>
 struct multiple_match_pred;
 
 template <typename _ExecutionPolicy, typename _Pred, typename _Tp, typename _Size>
@@ -489,9 +489,8 @@ struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::unseq_backen
 {
 };
 
-template <typename _ExecutionPolicy, typename _Pred>
-struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::unseq_backend::multiple_match_pred,
-                                                       _ExecutionPolicy, _Pred)>
+template <typename _Pred>
+struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::unseq_backend::multiple_match_pred, _Pred)>
     : oneapi::dpl::__internal::__are_all_device_copyable<_Pred>
 {
 };
