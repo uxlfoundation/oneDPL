bc6b8e490f43eb8778d3267fc59aea9dc5a61278
Sergey Kopienko <sergey.kopienko@intel.com>
2025-03-31 10:55:58 +0200

    Remove _ExecutionPolicy template param from oneapi::dpl::__internal::__kernel_sub_group_size

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -684,8 +684,8 @@ struct __parallel_radix_sort_iteration
         auto __local_scan_kernel = __kernels[1];
         auto __reorder_peer_kernel = __kernels[2];
         auto __reorder_kernel = __kernels[3];
-        ::std::size_t __count_sg_size = oneapi::dpl::__internal::__kernel_sub_group_size(__exec, __count_kernel);
-        __reorder_sg_size = oneapi::dpl::__internal::__kernel_sub_group_size(__exec, __reorder_kernel);
+        std::size_t __count_sg_size = oneapi::dpl::__internal::__kernel_sub_group_size(__exec.queue(), __count_kernel);
+        __reorder_sg_size = oneapi::dpl::__internal::__kernel_sub_group_size(__exec.queue(), __reorder_kernel);
         __scan_wg_size = sycl::min(
             __scan_wg_size, oneapi::dpl::__internal::__kernel_work_group_size(__exec.queue(), __local_scan_kernel));
         __count_wg_size = sycl::max(__count_sg_size, __reorder_sg_size);
