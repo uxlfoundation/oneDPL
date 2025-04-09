ba89ad152e0645ab3b4a3d320631d5d9250cfdf2
Sergey Kopienko <sergey.kopienko@intel.com>
2025-04-09 11:58:00 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h - declare return type for __parallel_set_op + __device_backend_tag

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1463,7 +1463,7 @@ __parallel_set_scan(oneapi::dpl::__internal::__device_backend_tag __backend_tag,
 
 template <typename _ExecutionPolicy, typename _Range1, typename _Range2, typename _Range3, typename _Compare,
           typename _IsOpDifference>
-auto
+__future<sycl::event, __result_and_scratch_storage<_ExecutionPolicy, oneapi::dpl::__internal::__difference_t<_Range3>>>
 __parallel_set_op(oneapi::dpl::__internal::__device_backend_tag __backend_tag, _ExecutionPolicy&& __exec,
                   _Range1&& __rng1, _Range2&& __rng2, _Range3&& __result, _Compare __comp,
                   _IsOpDifference __is_op_difference)
