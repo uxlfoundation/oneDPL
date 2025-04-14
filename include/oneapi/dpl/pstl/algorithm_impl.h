397ccc0030cdd45afdae9defa444ac5e5384782d
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-04-14 12:32:49 +0200

    [oneDPL][ranges][M] + fix an error for mismatch algo

--- include/oneapi/dpl/pstl/algorithm_impl.h
+++ include/oneapi/dpl/pstl/algorithm_impl.h
@@ -4126,6 +4126,9 @@ __pattern_mismatch(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, _
                    _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2,
                    _Predicate __pred)
 {
+    if(__last1 - __first1 == 0 || __last2 - __first2 == 0)
+        return {__first1, __first2};
+
     return __internal::__except_handler([&]() {
         auto __n = ::std::min(__last1 - __first1, __last2 - __first2);
         auto __result = __internal::__parallel_find(
