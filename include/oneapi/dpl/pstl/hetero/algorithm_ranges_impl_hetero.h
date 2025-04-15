0d3da217ce0b67ab565ccda906268817cedc9672
Sergey Kopienko <sergey.kopienko@intel.com>
2025-04-15 14:05:48 +0200

    include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h - fix review comment: using __pattern_transform_unary_op instead of __pattern_find_if_pred

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -293,25 +293,11 @@ __pattern_find_if(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Range&&
 }
 
 #if _ONEDPL_CPP20_RANGES_PRESENT
-template <typename _Pred, typename _Proj>
-struct __pattern_find_if_pred
-{
-    _Pred __pred;
-    _Proj __proj;
-
-    template <typename _TValue>
-    auto
-    operator()(_TValue&& __val) const
-    {
-        return std::invoke(__pred, std::invoke(__proj, std::forward<_TValue>(__val)));
-    }
-};
-
 template <typename _BackendTag, typename _ExecutionPolicy, typename _R, typename _Proj, typename _Pred>
 auto
 __pattern_find_if(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _R&& __r, _Pred __pred, _Proj __proj)
 {
-    __pattern_find_if_pred<_Pred, _Proj> __pred_1{__pred, __proj};
+    __pattern_transform_unary_op<_Pred, _Proj> __pred_1{__pred, __proj};
 
     auto __idx = oneapi::dpl::__internal::__ranges::__pattern_find_if(__tag, std::forward<_ExecutionPolicy>(__exec),
         oneapi::dpl::__ranges::views::all_read(__r), __pred_1);
