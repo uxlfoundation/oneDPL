21ae1b38afdd64843b3a5cca2529034918fac84e
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-04-16 18:52:33 +0200

    [oneDPL][ranges][M] + couple of minor fixes

--- include/oneapi/dpl/pstl/onedpl_config.h
+++ include/oneapi/dpl/pstl/onedpl_config.h
@@ -275,7 +275,7 @@
 #    define _ONEDPL_CPP23_TUPLE_LIKE_COMMON_REFERENCE_PRESENT                                                          \
         (_ONEDPL___cplusplus >= 202302L && __cpp_lib_tuple_like >= 202207L)
 #    define _ONEDPL_CPP23_RANGES_ZIP_PRESENT (_ONEDPL___cplusplus >= 202302L && __cpp_lib_ranges_zip >= 202110L)
-#    define _ONEDPL_CPP26_DEFAULT_VALUE_TYPE_PRESENT __cpp_lib_algorithm_default_value_type >= 202403L
+#    define _ONEDPL_CPP26_DEFAULT_VALUE_TYPE_PRESENT (__cpp_lib_algorithm_default_value_type >= 202403L)
 #else
 #    define _ONEDPL_CPP20_CONCEPTS_PRESENT 0
 #    define _ONEDPL_CPP23_TUPLE_LIKE_COMMON_REFERENCE_PRESENT 0
