0b16cbcc3c3f95f613a85b259a11fbfc3ef2d5c4
Sergey Kopienko <sergey.kopienko@intel.com>
2025-04-16 14:15:52 +0200

    change __parallel_reduce_by_segment_fallback to sycl::queue

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -2279,21 +2279,17 @@ struct __parallel_reduce_by_segment_fallback_fn2
 };
 } // namespace __internal
 
-template <typename _ExecutionPolicy, typename _Range1, typename _Range2, typename _Range3, typename _Range4,
+template <typename _CustomName, typename _Range1, typename _Range2, typename _Range3, typename _Range4,
           typename _BinaryPredicate, typename _BinaryOperator>
 oneapi::dpl::__internal::__difference_t<_Range3>
-__parallel_reduce_by_segment_fallback(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPolicy&& __exec,
-                                      _Range1&& __keys, _Range2&& __values, _Range3&& __out_keys,
-                                      _Range4&& __out_values, _BinaryPredicate __binary_pred,
-                                      _BinaryOperator __binary_op,
+__parallel_reduce_by_segment_fallback(oneapi::dpl::__internal::__device_backend_tag, sycl::queue& __q, _Range1&& __keys,
+                                      _Range2&& __values, _Range3&& __out_keys, _Range4&& __out_values,
+                                      _BinaryPredicate __binary_pred, _BinaryOperator __binary_op,
                                       /*known_identity=*/std::false_type)
 {
-    using _CustomName = oneapi::dpl::__internal::__policy_kernel_name<_ExecutionPolicy>;
     using __diff_type = oneapi::dpl::__internal::__difference_t<_Range1>;
     using __key_type = oneapi::dpl::__internal::__value_t<_Range1>;
     using __val_type = oneapi::dpl::__internal::__value_t<_Range2>;
-
-    sycl::queue __q_local = __exec.queue();
 
     const auto __n = __keys.size();
     // Round 1: reduce with extra indices added to avoid long segments
@@ -2316,7 +2312,7 @@ __parallel_reduce_by_segment_fallback(oneapi::dpl::__internal::__device_backend_
 
     // use work group size adjusted to shared local memory as the maximum segment size.
     std::size_t __wgroup_size =
-        oneapi::dpl::__internal::__slm_adjusted_work_group_size(__q_local, sizeof(__key_type) + sizeof(__val_type));
+        oneapi::dpl::__internal::__slm_adjusted_work_group_size(__q, sizeof(__key_type) + sizeof(__val_type));
 
     // element is copied if it is the 0th element (marks beginning of first segment), is in an index
     // evenly divisible by wg size (ensures segments are not long), or has a key not equal to the
@@ -2324,14 +2320,14 @@ __parallel_reduce_by_segment_fallback(oneapi::dpl::__internal::__device_backend_
     // TODO: replace wgroup size with segment size based on platform specifics.
     auto __intermediate_result_end =
         oneapi::dpl::__par_backend_hetero::__parallel_copy_if<__assign_key1_wrapper<_CustomName>>(
-            oneapi::dpl::__internal::__device_backend_tag{}, __q_local, __view1, __view2, __n,
+            oneapi::dpl::__internal::__device_backend_tag{}, __q, __view1, __view2, __n,
             __internal::__parallel_reduce_by_segment_fallback_fn1<_BinaryPredicate>{__binary_pred, __wgroup_size},
             unseq_backend::__brick_assign_key_position{})
             .get();
 
     //reduce by segment
     oneapi::dpl::__par_backend_hetero::__parallel_for<__reduce1_wrapper<_CustomName>>(
-        oneapi::dpl::__internal::__device_backend_tag{}, __q_local,
+        oneapi::dpl::__internal::__device_backend_tag{}, __q,
         unseq_backend::__brick_reduce_idx<_BinaryOperator, decltype(__n), _Range2>(__binary_op, __n),
         __intermediate_result_end,
         oneapi::dpl::__ranges::take_view_simple(oneapi::dpl::__ranges::views::all_read(__idx),
@@ -2360,16 +2356,15 @@ __parallel_reduce_by_segment_fallback(oneapi::dpl::__internal::__device_backend_
 
     // element is copied if it is the 0th element (marks beginning of first segment), or has a key not equal to
     // the adjacent element (end of a segment). Artificial segments based on wg size are not created.
-    auto __result_end =
-        oneapi::dpl::__par_backend_hetero::__parallel_copy_if<__assign_key2_wrapper<_CustomName>>(
-            oneapi::dpl::__internal::__device_backend_tag{}, __q_local, __view3, __view4, __view3.size(),
-            __internal::__parallel_reduce_by_segment_fallback_fn2<_BinaryPredicate>{__binary_pred},
-            unseq_backend::__brick_assign_key_position{})
-            .get();
+    auto __result_end = oneapi::dpl::__par_backend_hetero::__parallel_copy_if<__assign_key2_wrapper<_CustomName>>(
+                            oneapi::dpl::__internal::__device_backend_tag{}, __q, __view3, __view4, __view3.size(),
+                            __internal::__parallel_reduce_by_segment_fallback_fn2<_BinaryPredicate>{__binary_pred},
+                            unseq_backend::__brick_assign_key_position{})
+                            .get();
 
     //reduce by segment
     oneapi::dpl::__par_backend_hetero::__parallel_for<__reduce2_wrapper<_CustomName>>(
-        oneapi::dpl::__internal::__device_backend_tag{}, __q_local,
+        oneapi::dpl::__internal::__device_backend_tag{}, __q,
         unseq_backend::__brick_reduce_idx<_BinaryOperator, decltype(__intermediate_result_end), _Range4>(
             __binary_op, __intermediate_result_end),
         __result_end,
@@ -2416,11 +2411,10 @@ __parallel_reduce_by_segment(oneapi::dpl::__internal::__device_backend_tag, _Exe
         }
     }
 #endif
-    return __parallel_reduce_by_segment_fallback(
-        oneapi::dpl::__internal::__device_backend_tag{}, std::forward<_ExecutionPolicy>(__exec),
-        std::forward<_Range1>(__keys), std::forward<_Range2>(__values), std::forward<_Range3>(__out_keys),
-        std::forward<_Range4>(__out_values), __binary_pred, __binary_op,
-        oneapi::dpl::unseq_backend::__has_known_identity<_BinaryOperator, __val_type>{});
+    return __parallel_reduce_by_segment_fallback<_CustomName>(
+        oneapi::dpl::__internal::__device_backend_tag{}, __q_local, std::forward<_Range1>(__keys),
+        std::forward<_Range2>(__values), std::forward<_Range3>(__out_keys), std::forward<_Range4>(__out_values),
+        __binary_pred, __binary_op, oneapi::dpl::unseq_backend::__has_known_identity<_BinaryOperator, __val_type>{});
 }
 
 } // namespace __par_backend_hetero
