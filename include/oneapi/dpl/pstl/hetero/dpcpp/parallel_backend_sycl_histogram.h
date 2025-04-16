b68011c6e6033acfe2bf2623be8d4f77861feab2
Sergey Kopienko <sergey.kopienko@intel.com>
2025-04-16 09:53:38 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h - fix review comment: remove extra :: in __histogram_general_private_global_atomics_submitter call

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
@@ -486,7 +486,7 @@ __histogram_general_private_global_atomics(oneapi::dpl::__internal::__device_bac
 
     return __histogram_general_private_global_atomics_submitter<_global_atomics_name>()(
         oneapi::dpl::__internal::__device_backend_tag{}, __q, __init_event, __min_iters_per_work_item,
-        __work_group_size, ::std::forward<_Range1>(__input), ::std::forward<_Range2>(__bins), __binhash_manager);
+        __work_group_size, std::forward<_Range1>(__input), std::forward<_Range2>(__bins), __binhash_manager);
 }
 
 template <typename _CustomName, ::std::uint16_t __iters_per_work_item, typename _Range1, typename _Range2,
