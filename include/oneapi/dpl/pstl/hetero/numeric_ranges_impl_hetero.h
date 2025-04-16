940f89fedf4f600bc8c1b148a8cdf6ba07813eb1
Sergey Kopienko <sergey.kopienko@intel.com>
2025-04-16 17:48:29 +0200

    Replace ::std to std in changed lines of code

--- include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
@@ -59,7 +59,7 @@ __pattern_transform_reduce(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec,
                                                                           std::true_type /*is_commutative*/>(
                _BackendTag{}, __q_local, __binary_op1, _Functor{__binary_op2},
                unseq_backend::__init_value<_RepackedTp>{__init}, // initial value
-               std::forward<_Range1>(__rng1), ::std::forward<_Range2>(__rng2))
+               std::forward<_Range1>(__rng1), std::forward<_Range2>(__rng2))
         .get();
 }
 
