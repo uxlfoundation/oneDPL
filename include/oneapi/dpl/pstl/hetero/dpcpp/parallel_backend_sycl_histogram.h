8b05f42053d177b9fc2f06f8c963d7bbb6d8d9b6
Dan Hoeflinger <109972525+danhoeflinger@users.noreply.github.com>
2025-04-17 13:00:38 -0400

    Fix warning for histogram (#2195)
    * fix warning for histogram
    
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>
    
    * @@
    
    ---------
    
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>
    Co-authored-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
@@ -537,10 +537,10 @@ __parallel_histogram_select_kernel(oneapi::dpl::__internal::__device_backend_tag
     }
 }
 
-template <typename _CustomName, typename _Range1, typename _Range2, typename _BinHashMgr>
+template <typename _CustomName, typename _Event, typename _Range1, typename _Range2, typename _BinHashMgr>
 __future<sycl::event>
 __parallel_histogram(oneapi::dpl::__internal::__device_backend_tag __backend_tag, sycl::queue& __q,
-                     const sycl::event& __init_event, _Range1&& __input, _Range2&& __bins,
+                     const _Event& __init_event, _Range1&& __input, _Range2&& __bins,
                      const _BinHashMgr& __binhash_manager)
 {
     if (__input.size() < 1048576) // 2^20
