4e1d7f3ef9e5a77d68e446fb97e390133266ad8b
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-04-22 14:41:45 +0200

    [oneDPL][ranges][M][test] + binary_pred_const test case for find_first_of and mismatch

--- test/parallel_api/ranges/std_ranges_find_first_of.pass.cpp
+++ test/parallel_api/ranges/std_ranges_find_first_of.pass.cpp
@@ -25,9 +25,10 @@ main()
     auto find_first_of_checker = TEST_PREPARE_CALLABLE(std::ranges::find_first_of);
 
     test_range_algo<0, int, data_in_in>{medium_size}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred);
-    test_range_algo<1, int, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, proj, proj);
-    test_range_algo<2, P2, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, &P2::x, &P2::x);
-    test_range_algo<3, P2, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, &P2::proj, &P2::proj);
+    test_range_algo<1, int, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred_const);
+    test_range_algo<2, int, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, proj, proj);
+    test_range_algo<3, P2, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, &P2::x, &P2::x);
+    test_range_algo<4, P2, data_in_in>{}(dpl_ranges::find_first_of, find_first_of_checker, binary_pred, &P2::proj, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
