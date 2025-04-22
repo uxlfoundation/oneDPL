5550a97d5d9aab13b27f4648c02455bc70524f3b
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-04-22 14:38:46 +0200

    [oneDPL][ranges][M] + a fix in hetero __pattern_minmax_element

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -1743,13 +1743,9 @@ minmax_element(_ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp)
 {
     const auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend(__exec, __rng);
 
-    const auto& [__res1, __res2] = oneapi::dpl::__internal::__ranges::__pattern_minmax_element(
+    return oneapi::dpl::__internal::__ranges::__pattern_minmax_element(
         __dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec), views::all_read(::std::forward<_Range>(__rng)),
         __comp);
-
-    [[maybe_unused]] const auto& [__min_idx, __min] = __res1;
-    [[maybe_unused]] const auto& [__max_idx, __max] = __res2;
-    return {__min_idx, __max_idx};
 }
 
 template <typename _ExecutionPolicy, typename _Range>
