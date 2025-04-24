7a91fa05859f7205f95d0eec3743df3b37ddefb2
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-04-24 15:16:21 +0200

    [oneDPL][ranges][M][test] + test cases w/o comparator

--- test/parallel_api/ranges/std_ranges_max.pass.cpp
+++ test/parallel_api/ranges/std_ranges_max.pass.cpp
@@ -39,7 +39,9 @@ main()
 
     test_range_algo<4>{}(dpl_ranges::max, max_checker, std::ranges::greater{}, proj);
     test_range_algo<5, P2>{}(dpl_ranges::max, max_checker, std::ranges::greater{}, &P2::x);
-    test_range_algo<6, P2>{}(dpl_ranges::max, max_checker, std::ranges::greater{}, &P2::proj);    
+    test_range_algo<6, P2>{}(dpl_ranges::max, max_checker, std::ranges::greater{}, &P2::proj);
+
+    test_range_algo<7>{}(dpl_ranges::max, max_checker);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
