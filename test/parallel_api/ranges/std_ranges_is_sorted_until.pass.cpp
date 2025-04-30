23638c33bb8013abe7cd5583be74163edb73730a
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2025-04-30 20:32:22 +0100

    Improve is_sorted_until test

--- test/parallel_api/ranges/std_ranges_is_sorted_until.pass.cpp
+++ test/parallel_api/ranges/std_ranges_is_sorted_until.pass.cpp
@@ -26,7 +26,7 @@ main()
 
     auto generator = [](auto i) { return i == 42? 0 : i;};
 
-    test_range_algo<0, data_in, decltype(generator)>{big_sz}(
+    test_range_algo<0, int, data_in, decltype(generator)>{big_sz}(
         dpl_ranges::is_sorted_until, is_sorted_until_checker, std::ranges::less{});
 
     test_range_algo<1>{}(dpl_ranges::is_sorted_until, is_sorted_until_checker, std::ranges::less{}, proj);
