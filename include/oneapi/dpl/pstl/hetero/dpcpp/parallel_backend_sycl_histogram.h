4f7cc934a15e323d64c8bec12dc24fdf2adc4057
Dmitriy Sobolev <Dmitriy.Sobolev@intel.com>
2025-05-01 16:01:00 +0100

    clang-format

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
@@ -418,7 +418,8 @@ struct __histogram_general_private_global_atomics_submitter<__internal::__option
         const std::uint64_t __global_mem_size = __q.get_device().get_info<sycl::info::device::global_mem_size>();
         const std::uint64_t __max_groups =
             oneapi::dpl::__internal::__dpl_ceiling_div(__n, __work_group_size * __min_iters_per_work_item);
-        const std::uint64_t __max_segments = std::min(__global_mem_size / (__num_bins * sizeof(_bin_type)), __max_groups);
+        const std::uint64_t __max_segments =
+            std::min(__global_mem_size / (__num_bins * sizeof(_bin_type)), __max_groups);
 
         const std::uint64_t __iters_per_work_item =
             oneapi::dpl::__internal::__dpl_ceiling_div(__n, __max_segments * __work_group_size);
