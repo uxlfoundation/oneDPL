639f20d8b17bbe304493b2e3afdc9011f0c747cf
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-02 14:27:13 +0200

    test/parallel_api/algorithm/alg.modifying.operations/alg.partitions/stable_partition.pass.cpp - lambdas inside main() function isn't the problem

--- test/parallel_api/algorithm/alg.modifying.operations/alg.partitions/stable_partition.pass.cpp
+++ test/parallel_api/algorithm/alg.modifying.operations/alg.partitions/stable_partition.pass.cpp
@@ -135,12 +135,11 @@ struct test_non_const_stable_partition
 int
 main()
 {
-    test_by_type<std::int32_t>([](std::int32_t i) { return i; }, [](std::int32_t) { return true; }); // KSATODO move lambda out
-    test_by_type<float64_t>([](std::int32_t i) { return -i; }, [](const float64_t x) { return x < 0; }); // KSATODO move lambda out
-    test_by_type<std::int64_t>([](std::int32_t i) { return i + 1; }, [](std::int64_t x) { return x % 3 == 0; }); // KSATODO move lambda out
+    test_by_type<std::int32_t>([](std::int32_t i) { return i; }, [](std::int32_t) { return true; });
+    test_by_type<float64_t>([](std::int32_t i) { return -i; }, [](const float64_t x) { return x < 0; });
+    test_by_type<std::int64_t>([](std::int32_t i) { return i + 1; }, [](std::int64_t x) { return x % 3 == 0; });
 
 #if !TEST_DPCPP_BACKEND_PRESENT
-     // KSATODO move lambda out
     test_by_type<DataType<float32_t>>([](std::int32_t i) { return DataType<float32_t>(2 * i + 1); },
                                       [](const DataType<float32_t>& x) { return x.get_val() < 0; });
 #endif
