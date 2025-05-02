83cd7b02de95949681631b675a1bfeca43ca421a
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-02 15:18:39 +0200

    test/parallel_api/ranges/transform_inclusive_scan_ranges_sycl.pass.cpp - lambdas inside main() function isn't the problem

--- test/parallel_api/ranges/transform_inclusive_scan_ranges_sycl.pass.cpp
+++ test/parallel_api/ranges/transform_inclusive_scan_ranges_sycl.pass.cpp
@@ -35,7 +35,7 @@ main()
     int data1[max_n], data2[max_n];
 
     int init = 100;
-    auto lambda = [](auto i) { return i * i; }; // KSATODO move lambda out?
+    auto lambda = [](auto i) { return i * i; };
     {
         sycl::buffer<int> A(data, sycl::range<1>(max_n));
         sycl::buffer<int> B1(data1, sycl::range<1>(max_n));
