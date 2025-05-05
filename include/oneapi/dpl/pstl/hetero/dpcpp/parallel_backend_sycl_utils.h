cda1ebdb53acb853320806d59f4fc794399a5b2a
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-05 17:43:24 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h - fix logic error

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -803,7 +803,7 @@ class __future : private std::tuple<_Args...>
         constexpr std::size_t __size_of_tuple = std::tuple_size_v<decltype(*this)>;
 
         // __result_and_scratch_storage is the last element of the tuple if it exist
-        if constexpr (__size_of_tuple > 1)
+        if constexpr (__size_of_tuple > 0)
         {
             using __temporary_data_t = std::shared_ptr<__result_and_scratch_storage_base>;
             using __last_element_t = std::decay_t<std::tuple_element_t<__size_of_tuple - 1, decltype(*this)>>;
