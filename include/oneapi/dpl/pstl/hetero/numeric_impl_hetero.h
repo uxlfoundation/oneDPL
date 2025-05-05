61946823ed9e6beaa1cb68c8ad7dd97ceca784bc
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-05 16:48:02 +0200

    Fix __parallel_transform_scan calls

--- include/oneapi/dpl/pstl/hetero/numeric_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/numeric_impl_hetero.h
@@ -147,7 +147,7 @@ __pattern_transform_scan_base(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&
         oneapi::dpl::__par_backend_hetero::__parallel_transform_scan(
             _BackendTag{}, ::std::forward<_ExecutionPolicy>(__exec), __buf1.all_view(), __buf2.all_view(), __n,
             __unary_op, __init, __binary_op, _Inclusive{})
-            .__deferrable_wait(); // __future<sycl::event, __result_and_scratch_storage<typename _InitType::__value_type>>
+            .wait(); // Calls wait() here because we should extend the life-time of temporary data inside __future instance
     }
     else
     {
