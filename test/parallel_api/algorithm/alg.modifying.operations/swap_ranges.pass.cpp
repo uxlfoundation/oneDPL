b56a559b783c6c28f921f9a0d96c157415032e71
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-05 10:47:09 +0200

    test/parallel_api/algorithm/alg.modifying.operations/swap_ranges.pass.cpp - fix compile errors

--- test/parallel_api/algorithm/alg.modifying.operations/swap_ranges.pass.cpp
+++ test/parallel_api/algorithm/alg.modifying.operations/swap_ranges.pass.cpp
@@ -76,17 +76,15 @@ struct check_swap<wrapper<T>>
     }
 };
 
-template <typename T, typename TRef>
+template <typename T, typename T_ref>
 struct TransformOp
 {
     std::size_t& i;
+
     TransformOp(std::size_t& i_) : i(i_) {}
-    bool operator()() const
+    bool operator()(T_ref a) const
     {
-        bool operator()(T_ref a) const
-        {
-            return a == T(i++);
-        }
+        return a == T(const_cast<std::size_t&>(i)++);
     }
 };
 
@@ -110,8 +108,8 @@ struct test_one_policy
         if (check_return)
         {
             ::std::size_t i = 0;
-            bool check = all_of(actual_b, actual_e, TransformOp<T, TRef>{i}) &&
-                         all_of(data_b, data_e, TransformOp<T, TRef>{i});
+            bool check = all_of(actual_b, actual_e, TransformOp<T, T_ref>{i}) &&
+                         all_of(data_b, data_e, TransformOp<T, T_ref>{i});
 
             EXPECT_TRUE(check, "wrong effect of swap_ranges");
 
