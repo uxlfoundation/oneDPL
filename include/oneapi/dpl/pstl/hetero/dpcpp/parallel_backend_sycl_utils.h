50b43d4a5313414a66a08e05146dd3a04e6d6f83
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-07 14:12:32 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h - remove extra __future::__future(...)

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -853,12 +853,7 @@ class __future : private std::tuple<_Args...>
 
     template <typename TEvent, typename... TArgs>
     __future(TEvent&& __e, TArgs&&... __args)
-        : std::tuple<_Args...>(std::forward<TArgs>(__args)...), __my_event(std::forward<TEvent>(__e))
-    {
-    }
-
-    template <typename TEvent>
-    __future(TEvent&& __e, std::tuple<_Args...> __t) : std::tuple<_Args...>(__t), __my_event(std::forward<TEvent>(__e))
+        : __base_t(std::forward<TArgs>(__args)...), __my_event(std::forward<TEvent>(__e))
     {
     }
 
