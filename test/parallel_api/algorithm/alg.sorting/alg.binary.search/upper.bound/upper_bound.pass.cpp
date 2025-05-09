cdafe3122d9602bf38d1f29730b54c8e16895983
Sergey Kopienko <sergey.kopienko@intel.com>
2025-05-09 09:34:12 +0200

    test/parallel_api/algorithm/alg.sorting/alg.binary.search/upper.bound/upper_bound.pass.cpp - replace by TestUtils::IsLess

--- test/parallel_api/algorithm/alg.sorting/alg.binary.search/upper.bound/upper_bound.pass.cpp
+++ test/parallel_api/algorithm/alg.sorting/alg.binary.search/upper.bound/upper_bound.pass.cpp
@@ -113,8 +113,7 @@ DEFINE_TEST(test_upper_bound)
         check_and_clean(result_first, value_first, n);
 
         // call algorithm with comparator
-        auto res2 = oneapi::dpl::upper_bound(exec, first, last, value_first, value_last, result_first,
-                                             [](ValueT first, ValueT second) { return first < second; });
+        auto res2 = oneapi::dpl::upper_bound(exec, first, last, value_first, value_last, result_first, TestUtils::IsLess<ValueT>{});
         EXPECT_TRUE(std::distance(result_first, res2) == n, "wrong return value, with predicate, host policy");
         check_and_clean(result_first, value_first, n);
     }
