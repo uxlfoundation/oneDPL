6459df0785371334945fcf8d936a7b83e1de0d7a
Elvis Fefey <elvis.g.fefey@intel.com>
2025-05-21 11:15:12 -0500

    fixed resource policy using the policy_base

--- include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
@@ -16,56 +16,72 @@
 #    include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
 #endif
 
-namespace oneapi {
-namespace dpl {
-namespace experimental {
+namespace oneapi 
+{
+namespace dpl 
+{
+namespace experimental 
+{
 
 #if _DS_BACKEND_SYCL != 0
-template <typename ResourceType = sycl::queue, typename Backend = default_backend<sycl::queue>>
+template <typename ResourceType = sycl::queue, typename Backend = default_backend<ResourceType>>
 #else
 template <typename ResourceType, typename Backend>
 #endif
-class fixed_resource_policy : public policy_base<fixed_resource_policy<ResourceType, Backend>, ResourceType, Backend> {
+class fixed_resource_policy : public policy_base<fixed_resource_policy<ResourceType, Backend>, ResourceType, Backend> 
+{
+  protected:
     using base_t = policy_base<fixed_resource_policy<ResourceType, Backend>, ResourceType, Backend>;
     using resource_container_size_t = typename base_t::resource_container_size_t;
-    using resource_type = typename base_t::resource_type;
 
-    struct selector_t {
+    struct selector_t 
+    {
         typename base_t::resource_container_t resources_;
-        resource_container_size_t index_ = 0;
+	::std::size_t index_ = 0;
     };
 
     std::shared_ptr<selector_t> selector_;
-    std::size_t initial_index_ = 0;
 
-public:
-    using base_t::base_t;
+  public:
+    using resource_type = typename base_t::resource_type;
     using typename base_t::selection_type;
 
-    fixed_resource_policy(std::size_t index = 0) : initial_index_(index) { this->initialize(); }
-    fixed_resource_policy(deferred_initialization_t) : initial_index_(0) {}
-    fixed_resource_policy(const std::vector<resource_type>& u, std::size_t index = 0)
-        : initial_index_(index) {
-        this->initialize(u);
+    fixed_resource_policy(::std::size_t index = 0) 
+    { 
+        base_t::initialize(); 
+	selector_->index_ = index;
+    }
+    fixed_resource_policy(deferred_initialization_t) {}
+    fixed_resource_policy(const std::vector<resource_type>& u, ::std::size_t index = 0) 
+    { 
+        base_t::initialize(u); 
+	selector_->index_ = index;
     }
 
-    void ensure_selector_initialized() {
-        if (!selector_) selector_ = std::make_shared<selector_t>();
-    }
-
-    void initialize_impl(const std::vector<resource_type>& u) {
-        auto* s = this->template selector<selector_t>(selector_);
-        s->resources_ = u;
-        s->index_ = initial_index_;
+    void 
+    initialize_impl() 
+    {
+        if (!selector_) 
+	{
+	    selector_ = std::make_shared<selector_t>();
+	}
+	auto u = base_t::get_resources();
+        selector_->resources_ = u;
+        selector_->index_ = 0;
     }
 
     template <typename... Args>
-    selection_type select_impl(Args&&...) {
-        auto* s = this->template selector<selector_t>(selector_);
-        if (!s->resources_.empty()) {
-            return selection_type{*this, s->resources_[s->index_]};
-        }
-        return selection_type{*this};  // default constructed if empty
+    selection_type 
+    select_impl(Args&&...) 
+    {
+	if (selector_) 
+	{
+            return selection_type{*this, selector_->resources_[selector_->index_]};
+	}
+	else
+	{
+	    throw std::logic_error("select called before initialization");
+	}
     }
 };
 
