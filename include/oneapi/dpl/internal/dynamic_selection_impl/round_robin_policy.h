1c4184236d186c61c27b666a22d645f0ac7a8c1a
Elvis Fefey <elvis.g.fefey@intel.com>
2025-05-20 22:40:30 -0500

    initial policy base plus round robin inheriting

--- include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
@@ -7,165 +7,82 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef _ONEDPL_ROUND_ROBIN_POLICY_IMPL_H
-#define _ONEDPL_ROUND_ROBIN_POLICY_IMPL_H
+#ifndef _ONEDPL_ROUND_ROBIN_POLICY_H
+#define _ONEDPL_ROUND_ROBIN_POLICY_H
 
-#include <atomic>
-#include <type_traits>
-#include <vector>
-#include <memory>
-#include <stdexcept>
-#include <limits>
-#include <utility>
-#include "oneapi/dpl/internal/dynamic_selection_traits.h"
-#include "oneapi/dpl/internal/dynamic_selection_impl/scoring_policy_defs.h"
+#include "oneapi/dpl/internal/dynamic_selection_impl/policy_base.h"
+
 #if _DS_BACKEND_SYCL != 0
 #    include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
 #endif
 
-namespace oneapi
+namespace oneapi 
 {
-namespace dpl
+namespace dpl 
 {
-namespace experimental
+namespace experimental 
 {
+
 #if _DS_BACKEND_SYCL != 0
-template <typename ResourceType = sycl::queue, typename Backend = default_backend<sycl::queue>>
+template <typename ResourceType = sycl::queue, typename Backend = default_backend<ResourceType>>
 #else
-template <typename Backend>
+template <typename ResourceType, typename Backend>
 #endif
-struct round_robin_policy
+class round_robin_policy : public policy_base<round_robin_policy<ResourceType, Backend>, ResourceType, Backend> 
 {
-  private:
-    using backend_t = Backend;
-    using resource_container_t = typename backend_t::resource_container_t;
-    using resource_container_size_t = typename resource_container_t::size_type;
-    using execution_resource_t = typename backend_t::execution_resource_t;
-    using wrapped_resource_t = execution_resource_t;
+  protected:
+    using base_t = policy_base<round_robin_policy<ResourceType, Backend>, ResourceType, Backend>;
+    using resource_container_size_t = typename base_t::resource_container_size_t;
 
-  public:
-    //Policy Traits
-    using selection_type =
-        oneapi::dpl::experimental::basic_selection_handle_t<round_robin_policy<ResourceType, Backend>, execution_resource_t>;
-    using resource_type = decltype(unwrap(std::declval<wrapped_resource_t>()));
-    using wait_type = typename backend_t::wait_type;
-
-  private:
-    std::shared_ptr<backend_t> backend_;
-
-    struct state_t
+    struct selector_t 
     {
-        resource_container_t resources_;
+        typename base_t::resource_container_t resources_;
         resource_container_size_t num_contexts_;
         std::atomic<resource_container_size_t> next_context_;
     };
 
-    std::shared_ptr<state_t> state_;
+    std::shared_ptr<selector_t> selector_;
 
   public:
-    auto
-    get_resources() const
-    {
-        if (backend_)
-        {
-            return backend_->get_resources();
-        }
-        else
-        {
-            throw std::logic_error("get_resources called before initialization");
-        }
-    }
+    using resource_type = typename base_t::resource_type;
+    using typename base_t::selection_type;
 
-    void
-    initialize()
-    {
-        if (!state_)
-        {
-            backend_ = std::make_shared<backend_t>();
-            state_ = std::make_shared<state_t>();
-            state_->resources_ = get_resources();
-            state_->num_contexts_ = state_->resources_.size();
-            state_->next_context_ = 0;
-        }
-    }
-
-    void
-    initialize(const std::vector<resource_type>& u)
-    {
-        if (!state_)
-        {
-            backend_ = std::make_shared<backend_t>(u);
-            state_ = std::make_shared<state_t>();
-            state_->resources_ = get_resources();
-            state_->num_contexts_ = state_->resources_.size();
-            state_->next_context_ = 0;
-        }
-    }
-
-    round_robin_policy() { initialize(); }
-
+    round_robin_policy() { base_t::initialize(); }
     round_robin_policy(deferred_initialization_t) {}
+    round_robin_policy(const std::vector<resource_type>& u) { base_t::initialize(u); }
 
-    round_robin_policy(const std::vector<resource_type>& u) { initialize(u); }
+    void 
+    initialize_impl() 
+    {
+        if (!selector_) 
+	{
+	    selector_ = std::make_shared<selector_t>();
+	}
+	auto u = base_t::get_resources();
+        selector_->resources_ = u;
+        selector_->num_contexts_ = u.size();
+        selector_->next_context_ = 0;
+    }
 
     template <typename... Args>
-    selection_type
-    select(Args&&...)
+    selection_type 
+    select_impl(Args&&...) 
     {
-        if (state_)
-        {
-            resource_container_size_t current_context_;
-            while (true)
-            {
-                current_context_ = state_->next_context_.load();
-                resource_container_size_t new_context_;
-                new_context_ = (current_context_ + 1) % state_->num_contexts_;
+        resource_container_size_t current;
 
-                if (state_->next_context_.compare_exchange_strong(current_context_, new_context_))
-                {
-                    break;
-                }
-            }
-            auto& e = state_->resources_[current_context_];
-            return selection_type{*this, e};
+        while (true) {
+            current = selector_->next_context_.load();
+            auto next = (current + 1) % selector_->num_contexts_;
+            if (selector_->next_context_.compare_exchange_strong(current, next)) break;
         }
-        else
-        {
-            throw std::logic_error("select called before initialization");
-        }
-    }
 
-    template <typename Function, typename... Args>
-    auto
-    submit(selection_type e, Function&& f, Args&&... args)
-    {
-        if (backend_)
-        {
-            return backend_->submit(e, std::forward<Function>(f), std::forward<Args>(args)...);
-        }
-        else
-        {
-            throw std::logic_error("submit called before initialization");
-        }
-    }
-
-    auto
-    get_submission_group()
-    {
-        if (backend_)
-        {
-            return backend_->get_submission_group();
-        }
-        else
-        {
-            throw std::logic_error("get_submission_group called before initialization");
-        }
+        return selection_type{*this, selector_->resources_[current]};
     }
 };
+
 } // namespace experimental
-
 } // namespace dpl
-
 } // namespace oneapi
 
-#endif //_ONEDPL_ROUND_ROBIN_POLICY_IMPL_H
+#endif // _ONEDPL_ROUND_ROBIN_POLICY_H
+
