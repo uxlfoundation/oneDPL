b8739c1724045b774a82cb74548faf662c11bf36
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-06-03 17:01:23 -0400

    shrink partitioning by factor of 2
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1090,7 +1090,7 @@ __parallel_set_write_a_b_op(sycl::queue& __q, _Range1&& __rng1, _Range2&& __rng2
     constexpr std::uint32_t __average_input_ele_size = (sizeof(_In1ValueT) + sizeof(_In2ValueT)) / 2;
     // Partition into blocks of half SLM size
     const std::size_t __partition_size =
-         __q.get_device().template get_info<sycl::info::device::local_mem_size>() / (__average_input_ele_size * 2);
+         __q.get_device().template get_info<sycl::info::device::local_mem_size>() / (__average_input_ele_size * 4);
 
     _GenReduceInput __gen_reduce_input{_SetOperation{}, __diagonal_spacing,
         _BoundsProvider{__diagonal_spacing, __partition_size}, __comp};
