a047a3303a6347a0ec7d449103294a75a198c4bc
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-06-10 15:00:12 +0200

    [oneDPL][ranges][L3][test] + uninitialized_value_construct, uninitialized_defualt_construct tests (host)

--- test/parallel_api/ranges/std_ranges_memory_test.h
+++ test/parallel_api/ranges/std_ranges_memory_test.h
@@ -13,32 +13,62 @@
 //
 //===----------------------------------------------------------------------===//
 
+#if _ENABLE_STD_RANGES_TESTING
+
 #include <oneapi/dpl/memory>
+#include <oneapi/dpl/algorithm>
 
-#include "support/std_ranges_test.h"
-
+#include "std_ranges_test.h"
 
 namespace test_std_ranges
 {
 
-template<sycl::usm::alloc _alloc_type>
+template<typename Elem, int no_init_val>
 struct test_memory_algo
 {
 
-    void operator()(auto __algo)
+    void run_host(auto algo, auto checker, auto&&... args)
+    {
+        run_host_one_policy(oneapi::dpl::execution::seq, algo, checker, std::forward<decltype(args)>(args)...);
+        run_host_one_policy(oneapi::dpl::execution::unseq, algo, checker, std::forward<decltype(args)>(args)...);
+        run_host_one_policy(oneapi::dpl::execution::par, algo, checker,  std::forward<decltype(args)>(args)...);
+        run_host_one_policy(oneapi::dpl::execution::par_unseq, algo, checker, std::forward<decltype(args)>(args)...);
+    }
+#if TEST_DPCPP_BACKEND_PRESENT
+    template<sycl::usm::alloc _alloc_type>
+    void run_device(auto algo, auto checker, auto&&... args)
     {
 
         sycl::queue __q;
         //usm_data_transfer(sycl::queue __q, _Size __sz)
         TestUtils::usm_data_transfer<_alloc_type, T> __mem(q, medium_size);
     }
+#endif //TEST_DPCPP_BACKEND_PRESENT
 
-    void operator()(auto&& __policy, auto __algo, auto __checker, auto&&... args)
+private:
+//    template<typename Allocator>
+    void run_host_one_policy(auto&& policy, auto algo, auto checker, auto&&... args)
     {
-        __algo(std::forward<decltype(__policy)>(__policy), std::forward<decltype(args)>(args)...);
-        const bool __bres = __checker(std::forward<decltype(args)>(args)...);
-        EXPECT_TRUE(__bres, (std::string("wrong return value from memory algo with ranges: ") + typeid(__algo).name()
-            + typeid(__policy).name()).c_str());
+        const std::size_t n = medium_size;
+        std::allocator<Elem> alloc;
+        Elem* pData = alloc.allocate(n);
+
+        std::memset(pData, no_init_val, n*sizeof(Elem));
+        std::ranges::subrange r(pData, pData + n);
+
+        run(std::forward<decltype(policy)>(policy), algo, checker, std::move(r), std::forward<decltype(args)>(args)...);
+
+        alloc.deallocate(pData, n);
+    }
+
+    void run(auto&& policy, auto algo, auto checker, auto&&... args)
+    {
+        auto res = algo(std::forward<decltype(policy)>(policy), std::forward<decltype(args)>(args)...);
+        auto [bres1, bres2] = checker(res, std::forward<decltype(args)>(args)...);
+        EXPECT_TRUE(bres1, (std::string("wrong return value from memory algo with ranges: ") + typeid(algo).name()
+            + typeid(policy).name()).c_str());
+        EXPECT_TRUE(bres2, (std::string("wrong effect from memory algo with ranges: ") + typeid(algo).name()
+            + typeid(policy).name()).c_str());
     }
 };
 
