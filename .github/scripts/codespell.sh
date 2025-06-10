b1fe3d52bdb42bb23d9f583c5afc11cc301c8ae5
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-06-10 09:04:24 -0400

    fix command line
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- .github/scripts/codespell.sh
+++ .github/scripts/codespell.sh
@@ -20,7 +20,7 @@ SKIP_PATTERN='*/.github/*,*/examples/pSTL_offload/FileWordCount/*'
 IGNORE_COMMAND="sed -e /.*windows.inc.*Od\\s*=.*/d \
 -e /.*README.md.*varN\\s*=.*/d"
 
-SCAN_RESULT=$(codespell -I ${GITHUB_WORKSPACE}/.github/scripts/allowed_words.txt --quiet-level=2 --skip "${SKIP_PATTERN}" ${SCAN_TARGET})
+SCAN_RESULT=$(codespell -I ${GITHUB_WORKSPACE}/.github/scripts/allowed_words.txt --ignore-words=FILE --quiet-level=2 --skip "${SKIP_PATTERN}" ${SCAN_TARGET})
 SCAN_RESULT=$(echo -e "${SCAN_RESULT}" | ${IGNORE_COMMAND})
 echo "${SCAN_RESULT}"
 
