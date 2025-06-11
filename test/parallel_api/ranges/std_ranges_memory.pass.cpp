344a7d16b5cd2eb72370a83dc47596229405766b
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-06-11 14:37:07 +0200

    [oneDPL][ranges][L3][test] + tests for uninitialized_copy, uninitialized_move

--- test/parallel_api/ranges/std_ranges_memory.pass.cpp
+++ test/parallel_api/ranges/std_ranges_memory.pass.cpp
@@ -21,6 +21,8 @@
 #include "support/test_macros.h"
 #include "support/utils.h"
 
+#if _ENABLE_STD_RANGES_TESTING
+
 #include "std_ranges_memory_test.h"
 
 //defualt initialization, initialization by custom value (fill)
@@ -31,6 +33,7 @@ struct Elem
 
     Elem() { val1 = 1; }
     Elem(int v) { val2 = v;}
+    Elem(const Elem& elem) { val2 = elem.val2; }
 };
 
 //value initialization
@@ -40,7 +43,18 @@ struct Elem_0
     int val2;
 
     Elem_0(): val1() {}
+    Elem_0(Elem&& elem) { val2 = elem.val2; }
 };
+
+namespace test_std_ranges
+{
+template<>
+constexpr int is_two_ranges<std::remove_cvref_t<decltype(oneapi::dpl::ranges::uninitialized_copy)>> = true;
+
+template<>
+constexpr int is_two_ranges<std::remove_cvref_t<decltype(oneapi::dpl::ranges::uninitialized_move)>> = true;
+}
+#endif //_ENABLE_STD_RANGES_TESTING
 
 std::int32_t
 main()
@@ -85,10 +99,35 @@ main()
  
     test_memory_algo<Elem, -1>{}.run_host(dpl_ranges::uninitialized_fill, uninitialized_fill_checker, 2);
 
+    auto uninitialized_copy_move_checker = 
+        [](const auto& res, auto&& r_in, auto&& r_out) {
+            using InRange = std::remove_cvref_t<decltype(r_in)>;
+            using OutRange = std::remove_cvref_t<decltype(r_out)>;
+
+            using Size = std::common_type_t<std::ranges::range_size_t<InRange>, std::ranges::range_size_t<OutRange>>;
+            const Size sz = std::ranges::min((Size)std::ranges::size(r_in), (Size)std::ranges::size(r_out));
+
+            const bool bres1 = (res.in == std::ranges::borrowed_iterator_t<InRange>(std::ranges::begin(r_in) + sz)
+                && res.out == std::ranges::borrowed_iterator_t<OutRange>(std::ranges::begin(r_out) + sz));
+            
+            const bool bres2 = std::ranges::all_of(std::ranges::take_view(r_out, sz), [](const auto& v) { return v.val1 == -1;})
+                && std::ranges::equal(std::ranges::take_view(r_in, sz), std::ranges::take_view(r_out, sz), 
+                       [](const auto& v1, const auto& v2) { return v1.val2 == v2.val2;})
+                && std::ranges::all_of(std::ranges::drop_view(r_out, sz), [](const auto& v) { return v.val1 == -1 && v.val2 == -1;});
+
+            return std::pair<bool, bool>{bres1, bres2};
+        };
+
+    test_memory_algo<Elem, -1>{}.run_host(dpl_ranges::uninitialized_copy, uninitialized_copy_move_checker);    
+    test_memory_algo<Elem_0, -1>{}.run_host(dpl_ranges::uninitialized_move, uninitialized_copy_move_checker);
+
 #if TEST_DPCPP_BACKEND_PRESENT
     test_memory_algo<Elem, -1>{}.run_device(dpl_ranges::uninitialized_default_construct, uninitialized_default_construct_checker);
     test_memory_algo<Elem_0, -1>{}.run_device(dpl_ranges::uninitialized_value_construct, uninitialized_value_construct_checker);
     test_memory_algo<Elem, -1>{}.run_device(dpl_ranges::uninitialized_fill, uninitialized_fill_checker, 2);
+
+    test_memory_algo<Elem, -1>{}.run_device(dpl_ranges::uninitialized_copy, uninitialized_copy_move_checker);
+    test_memory_algo<Elem_0, -1>{}.run_device(dpl_ranges::uninitialized_move, uninitialized_copy_move_checker);
 #endif //TEST_DPCPP_BACKEND_PRESENT
 
 #endif //_ENABLE_STD_RANGES_TESTING
