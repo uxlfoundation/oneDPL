145cb11493d84a6b5fd8c0f0b44d339f513cffb6
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-06-16 23:15:04 +0200

    Revert "[oneDPL] + std::remove_cv_t<std::remove_reference_t<_Tp>> value"
    This reverts commit d3164acd16d341cba44664a093791469736dbd06.

--- include/oneapi/dpl/pstl/tuple_impl.h
+++ include/oneapi/dpl/pstl/tuple_impl.h
@@ -278,7 +278,7 @@ struct __value_holder
     __value_holder(_Up&& t) : value(::std::forward<_Up>(t))
     {
     }
-    std::remove_cv_t<std::remove_reference_t<_Tp>> value;
+    _Tp value;
 };
 
 // Necessary to make tuple trivially_copy_assignable. This type decided
