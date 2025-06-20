7b8650c8986838c5efd96a4914479389cdad9713
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-06-20 11:40:14 +0200

    [oneDPL][ranges][L2][test] + typo fixes

--- test/parallel_api/ranges/std_ranges_set_union.pass.cpp
+++ test/parallel_api/ranges/std_ranges_set_union.pass.cpp
@@ -23,14 +23,14 @@ main()
     namespace dpl_ranges = oneapi::dpl::ranges;
 
     auto set_union_checker = [](std::ranges::random_access_range auto&& r1,
-                                std::ranges::random_access_range auto&& r2
+                                std::ranges::random_access_range auto&& r2,
                                 std::ranges::random_access_range auto&& r_out, auto&&... args)
     {
         auto res = std::ranges::set_union(std::forward<decltype(r1)>(r1), std::forward<decltype(r2)>(r2),
             std::ranges::begin(r_out), std::forward<decltype(args)>(args)...);
 
         using ret_type = std::ranges::set_union_result<std::ranges::borrowed_iterator_t<decltype(r1)>,
-                                                       std::ranges::borrowed_iterator_t<decltype(r2)>
+                                                       std::ranges::borrowed_iterator_t<decltype(r2)>,
                                                        std::ranges::borrowed_iterator_t<decltype(r_out)>>;
         return ret_type{res.in1, res.in2, res.out};
     };
@@ -38,9 +38,9 @@ main()
     test_range_algo<0, int, data_in_in_out>{big_sz}(dpl_ranges::set_union, set_union_checker);
     test_range_algo<1, int, data_in_in_out>{big_sz}(dpl_ranges::set_union, set_union_checker, std::ranges::less{}, proj);
 
-    test_range_algo<2, int, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, std::ranges::greater{}, proj, proj);
-    test_range_algo<3, P2, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, pred, &P2::x, &P2::x);
-    test_range_algo<4, P2, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, pred, &P2::proj, &P2::proj);
+    test_range_algo<2, int, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, std::ranges::less{}, proj, proj);
+    test_range_algo<3, P2, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, std::ranges::less{}, &P2::x, &P2::x);
+    test_range_algo<4, P2, data_in_in_out>{}(dpl_ranges::set_union, set_union_checker, std::ranges::less{}, &P2::proj, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
