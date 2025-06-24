f1f3df9b87a895f548792c458d1a10426023027f
Sergey Kopienko <sergey.kopienko@intel.com>
2025-06-24 09:43:49 +0200

    test/parallel_api/iterator/zip_iterator_merge.pass.cpp - fix review comment

--- test/parallel_api/iterator/zip_iterator_merge.pass.cpp
+++ test/parallel_api/iterator/zip_iterator_merge.pass.cpp
@@ -100,7 +100,7 @@ DEFINE_TEST(test_merge)
         for (size_t i = 0; i < std::min(res_size, exp_size); ++i)
         {
             if (i < size2 * 2)
-                EXPECT_EQ(*(host_first3 + i), i, "wrong effect from merge (tuple) #1");
+                EXPECT_EQ(i, *(host_first3 + i), "wrong effect from merge (tuple) #1");
             else
                 EXPECT_EQ(*(host_first3 + i), *(host_first1 + i - size2), "wrong effect from merge (tuple) #2");
         }
