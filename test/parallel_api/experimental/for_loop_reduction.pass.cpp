00da170917459994842f503a772c60ec77f0a8fb
Sergey Kopienko <sergey.kopienko@intel.com>
2025-06-24 18:10:14 +0200

    test/parallel_api/experimental/for_loop_reduction.pass.cpp - fix review comment

--- test/parallel_api/experimental/for_loop_reduction.pass.cpp
+++ test/parallel_api/experimental/for_loop_reduction.pass.cpp
@@ -157,9 +157,9 @@ struct test_body_predefined_bits
             bit_xor_exp ^= *iter;
         }
 
-        EXPECT_EQ(bit_or_var, bit_or_exp, "wrong result of reduction_bit_or");
-        EXPECT_EQ(bit_and_var, bit_and_exp, "wrong result of reduction_bit_and");
-        EXPECT_EQ(bit_xor_var, bit_xor_exp, "wrong result of reduction_bit_xor");
+        EXPECT_EQ(bit_or_exp, bit_or_var, "wrong result of reduction_bit_or");
+        EXPECT_EQ(bit_and_exp, bit_and_var, "wrong result of reduction_bit_and");
+        EXPECT_EQ(bit_xor_exp, bit_xor_var, "wrong result of reduction_bit_xor");
     }
 
     template <typename Policy, typename Iterator, typename Size>
