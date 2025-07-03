0df2c6150f3d319f408c7d62c537779918efdd26
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-07-03 14:02:48 -0400

    remove stale TODO
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
@@ -632,7 +632,6 @@ struct __get_bounds_partitioned
         // diagonal index of the tile begin
         const _SizeType __wg_begin_idx = (__id / __tile_size) * __tile_size;
         const _SizeType __signed_tile_size = static_cast<_SizeType>(__tile_size);
-        //TODO: ensure partitioning fills in last diagonal
         const _SizeType __wg_end_idx = std::min(
             ((static_cast<_SizeType>(__id) / __signed_tile_size) + 1) * __signed_tile_size, __rng_tmp_diag.size() - 1);
 
