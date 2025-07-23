74135637da1f702b815f86bc50af5df52e55eace
Matthew Michel <matthew.michel@intel.com>
2025-07-23 10:28:37 -0500

    Pass scratch size to __get_usm_or_buffer_accessor_ptr
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1516,7 +1516,7 @@ struct __parallel_find_or_impl_multiple_wgs<__or_tag_check, __internal::__option
                 __result_storage.template __get_scratch_acc<sycl::access_mode::write>(__cgh, __dpl_sycl::__no_init{});
 
             __cgh.single_task<KernelNameInit...>([__scratch_acc, __init_value]() {
-                auto __scratch_ptr = __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc);
+                auto __scratch_ptr = __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc, 1);
                 *__scratch_ptr = __init_value;
             });
         });
@@ -1560,7 +1560,7 @@ struct __parallel_find_or_impl_multiple_wgs<__or_tag_check, __internal::__option
                     if (__local_idx == 0 && __found_local != __init_value)
                     {
                         auto __scratch_ptr =
-                            __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc);
+                            __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc, 1);
 
                         __dpl_sycl::__atomic_ref<_AtomicType, sycl::access::address_space::global_space> __found(
                             *__scratch_ptr);
@@ -1575,7 +1575,7 @@ struct __parallel_find_or_impl_multiple_wgs<__or_tag_check, __internal::__option
             auto __res_acc = __result_storage.template __get_result_acc<sycl::access_mode::write>(__cgh);
             __cgh.depends_on(__event);
             __cgh.single_task<KernelNameWriteBack...>([__scratch_acc, __res_acc]() {
-                auto __scratch_ptr = __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc);
+                auto __scratch_ptr = __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__scratch_acc, 1);
                 auto __res_ptr = __result_and_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__res_acc);
                 *__res_ptr = *__scratch_ptr;
             });
