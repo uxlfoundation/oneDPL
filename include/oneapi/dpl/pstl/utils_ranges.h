d1ee3df5da4295098dc6606fa953d48ee717df9d
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-12 11:54:14 +0200

    include/oneapi/dpl/pstl/utils_ranges.h - insert inline before static constexpr inside class / struct

--- include/oneapi/dpl/pstl/utils_ranges.h
+++ include/oneapi/dpl/pstl/utils_ranges.h
@@ -240,7 +240,7 @@ class zip_view
 
   public:
     using value_type = oneapi::dpl::__internal::tuple<oneapi::dpl::__internal::__value_t<_Ranges>...>;
-    static constexpr ::std::size_t __num_ranges = sizeof...(_Ranges);
+    inline static constexpr ::std::size_t __num_ranges = sizeof...(_Ranges);
 
     explicit zip_view(_Ranges... __args) : __m_ranges(__args...) {}
 
