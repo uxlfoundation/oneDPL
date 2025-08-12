e1857fc950dd577037f85f78ebd1dd4fdba37e55
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-12 10:23:31 +0200

    test/general/implementation_details/test_sycl_iterator_operations.pass.cpp - fix review comment: introduce namespace alias oneapi::dpl::__internal

--- test/general/implementation_details/test_sycl_iterator_operations.pass.cpp
+++ test/general/implementation_details/test_sycl_iterator_operations.pass.cpp
@@ -140,7 +140,7 @@ void
 test_iterators_possibly_equal()
 {
     // Check some internals from oneapi::dpl::__internal
-    using namespace oneapi::dpl::__internal;
+    namespace __internal = oneapi::dpl::__internal;
 
     constexpr std::size_t count = 0;
     sycl::buffer<int> buf1(count);
@@ -151,39 +151,39 @@ test_iterators_possibly_equal()
     auto& it1Ref = it1;
     auto& it2Ref = it2;
 
-    EXPECT_TRUE(__iterators_possibly_equal(it1, it1), "wrong __iterators_possibly_equal result");
-    EXPECT_TRUE(__iterators_possibly_equal(it1, it1Ref), "wrong __iterators_possibly_equal result");
-    EXPECT_TRUE(__iterators_possibly_equal(it1Ref, it1), "wrong __iterators_possibly_equal result");
-    EXPECT_TRUE(__iterators_possibly_equal(it1Ref, it1Ref), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(it1, it2), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(it1Ref, it2), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(it1, it2Ref), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(it1Ref, it2Ref), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(oneapi::dpl::begin(buf1), it2), "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(oneapi::dpl::begin(buf1), it2Ref),
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(it1, it1), "wrong __iterators_possibly_equal result");
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(it1, it1Ref), "wrong __iterators_possibly_equal result");
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(it1Ref, it1), "wrong __iterators_possibly_equal result");
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(it1Ref, it1Ref), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1, it2), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1Ref, it2), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1, it2Ref), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1Ref, it2Ref), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(oneapi::dpl::begin(buf1), it2), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(oneapi::dpl::begin(buf1), it2Ref),
                  "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(oneapi::dpl::begin(buf1), oneapi::dpl::begin(buf2)),
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(oneapi::dpl::begin(buf1), oneapi::dpl::begin(buf2)),
                  "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(it1, oneapi::dpl::begin(buf2)), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1, oneapi::dpl::begin(buf2)), "wrong __iterators_possibly_equal result");
 
-    EXPECT_FALSE(__iterators_possibly_equal(oneapi::dpl::begin(buf1), nullptr),
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(oneapi::dpl::begin(buf1), nullptr),
                  "wrong __iterators_possibly_equal result");
-    EXPECT_FALSE(__iterators_possibly_equal(nullptr, oneapi::dpl::begin(buf2)),
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(nullptr, oneapi::dpl::begin(buf2)),
                  "wrong __iterators_possibly_equal result");
 
     // sub - buffer vs it's "root" buffer (expect true)
     sycl::buffer<int, 1> buf11{buf1, sycl::range<1>{0}, sycl::range<1>{0}};
-    EXPECT_TRUE(__iterators_possibly_equal(oneapi::dpl::end(buf1), oneapi::dpl::begin(buf11)),
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(oneapi::dpl::end(buf1), oneapi::dpl::begin(buf11)),
                 "wrong __iterators_possibly_equal result");
 
     // sub - buffer vs sub - buffer which share a "root" buffer(expect true)
     sycl::buffer<int, 1> buf12{buf1, sycl::range<1>{0}, sycl::range<1>{0}};
-    EXPECT_TRUE(__iterators_possibly_equal(oneapi::dpl::begin(buf11), oneapi::dpl::end(buf12)),
+    EXPECT_TRUE(__internal::__iterators_possibly_equal(oneapi::dpl::begin(buf11), oneapi::dpl::end(buf12)),
                 "wrong __iterators_possibly_equal result");
 
     // two sycl_iterators pointing to different elements in the same "root" buffer(expect false)
     auto it1next = it1 + 1;
-    EXPECT_FALSE(__iterators_possibly_equal(it1, it1next), "wrong __iterators_possibly_equal result");
+    EXPECT_FALSE(__internal::__iterators_possibly_equal(it1, it1next), "wrong __iterators_possibly_equal result");
 
     {
         float floatData = .0;
@@ -193,13 +193,13 @@ test_iterators_possibly_equal()
         std::vector<int>::iterator intData = dataVec.begin();
 
         // check pointer + pointer
-        EXPECT_TRUE(__iterators_possibly_equal(intData, intData), "wrong __iterators_possibly_equal result");
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(intData, intData), "wrong __iterators_possibly_equal result");
         // check const pointer + pointer
-        EXPECT_TRUE(__iterators_possibly_equal(intConstData, intData), "wrong __iterators_possibly_equal result");
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(intConstData, intData), "wrong __iterators_possibly_equal result");
         // check pointer + const pointer
-        EXPECT_TRUE(__iterators_possibly_equal(intData, intConstData), "wrong __iterators_possibly_equal result");
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(intData, intConstData), "wrong __iterators_possibly_equal result");
         // check pointer + pointer to other type
-        EXPECT_FALSE(__iterators_possibly_equal(intData, &floatData), "wrong __iterators_possibly_equal result");
+        EXPECT_FALSE(__internal::__iterators_possibly_equal(intData, &floatData), "wrong __iterators_possibly_equal result");
     }
 
     {
@@ -209,14 +209,14 @@ test_iterators_possibly_equal()
         const float floatData = .0;
 
         // Check pointer to const data + pointer to data
-        EXPECT_TRUE(__iterators_possibly_equal(&intConstData, &intData), "wrong __iterators_possibly_equal result");
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(&intConstData, &intData), "wrong __iterators_possibly_equal result");
         // Check pointer to data + pointer to const data
-        EXPECT_TRUE(__iterators_possibly_equal(&intData, &intConstData), "wrong __iterators_possibly_equal result");
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(&intData, &intConstData), "wrong __iterators_possibly_equal result");
         // Check pointer to const data + pointer to const data
-        EXPECT_TRUE(__iterators_possibly_equal(&intConstData, &intConstData),
+        EXPECT_TRUE(__internal::__iterators_possibly_equal(&intConstData, &intConstData),
                     "wrong __iterators_possibly_equal result");
         // check pointer + pointer to other const type
-        EXPECT_FALSE(__iterators_possibly_equal(intData, &floatData), "wrong __iterators_possibly_equal result");
+        EXPECT_FALSE(__internal::__iterators_possibly_equal(intData, &floatData), "wrong __iterators_possibly_equal result");
     }
 }
 
