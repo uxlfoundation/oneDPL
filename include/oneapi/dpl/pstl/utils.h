7df3123bd129aaa365276ee441ab8d0cd584ebda
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-12 15:35:26 +0200

    Reworking the comparison check logic

--- include/oneapi/dpl/pstl/utils.h
+++ include/oneapi/dpl/pstl/utils.h
@@ -843,19 +843,7 @@ inline constexpr bool __is_equality_comparable_with_v = std::equality_comparable
 
 #else
 
-template <typename _Iterator, typename = void>
-struct __has_base_iterator : std::false_type
-{
-};
-
 template <typename _Iterator>
-struct __has_base_iterator<
-    _Iterator, std::enable_if_t<__is_iterator_type_v<decltype(std::declval<std::decay_t<_Iterator>>().base())>>>
-    : std::true_type
-{
-};
-
-template <typename _Iterator, typename = void>
 struct __base_iterator_type
 {
     // If no base is available, use the iterator type itself. This supports iterator adapters
@@ -863,10 +851,12 @@ struct __base_iterator_type
     using __type = std::decay_t<_Iterator>;
 };
 
+// Specialization for reverse iterators to extract the base iterator type.
 template <typename _Iterator>
-struct __base_iterator_type<_Iterator, std::void_t<decltype(std::declval<std::decay_t<_Iterator>>().base())>>
+struct __base_iterator_type<std::reverse_iterator<_Iterator>>
 {
-    using __type = decltype(std::declval<std::decay_t<_Iterator>>().base());
+    // The base iterator type of a reverse iterator is the type returned by its base() method.
+    using __type = decltype(std::declval<std::reverse_iterator<_Iterator>>().base());
 };
 
 template <typename _Iterator1, typename _Iterator2, typename = void>
@@ -882,19 +872,9 @@ struct __has_equality_op<_Iterator1, _Iterator2,
 };
 
 // Checks equality comparability of two iterators.
-// Pre-C++20 iterator adapters (move_iterator, reverse_iterator) have overly permissive operator== definitions that
-// cause SFINAE checks to pass for operators that cannot then compile. This issue requires more than simple
-// __has_equality_op checks to determine if two iterators are equality comparable. We must recursively check base
-// iterator types first to avoid build errors with incompatible base types.
 template <typename _Iterator1, typename _Iterator2>
-struct __is_equality_comparable_with
-    : std::conditional_t<
-          std::disjunction_v<__has_base_iterator<_Iterator1>, __has_base_iterator<_Iterator2>>,
-          // MS STL encounters build errors if we check current iterator before confirming bases are comparable
-          std::conjunction<__is_equality_comparable_with<typename __base_iterator_type<_Iterator1>::__type,
-                                                         typename __base_iterator_type<_Iterator2>::__type>,
-                           __has_equality_op<_Iterator1, _Iterator2>>,
-          __has_equality_op<_Iterator1, _Iterator2>>
+struct __is_equality_comparable_with : __has_equality_op<typename __base_iterator_type<_Iterator1>::__type,
+                                                         typename __base_iterator_type<_Iterator2>::__type>
 {
 };
 
