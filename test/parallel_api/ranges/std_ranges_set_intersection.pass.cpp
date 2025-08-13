9b756d3c9a5b9f034cc9038fd3dc64abb8c8342d
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-13 14:03:59 +0200

    [oneDPL][ranges][L2][tests] + a test for ranges::set_intersection

---
+++ test/parallel_api/ranges/std_ranges_set_intersection.pass.cpp
@@ -0,0 +1,48 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#include "std_ranges_test.h"
+
+std::int32_t
+main()
+{
+#if _ENABLE_STD_RANGES_TESTING
+    using namespace test_std_ranges;
+    namespace dpl_ranges = oneapi::dpl::ranges;
+
+    auto set_intersection_checker = [](std::ranges::random_access_range auto&& r1,
+                                       std::ranges::random_access_range auto&& r2,
+                                       std::ranges::random_access_range auto&& r_out, auto&&... args)
+    {
+        auto res = std::ranges::set_intersection(std::forward<decltype(r1)>(r1), std::forward<decltype(r2)>(r2),
+                                                 std::ranges::begin(r_out), std::forward<decltype(args)>(args)...);
+
+        using ret_type = std::ranges::set_intersection_result<std::ranges::borrowed_iterator_t<decltype(r1)>,
+                                                              std::ranges::borrowed_iterator_t<decltype(r2)>,
+                                                              std::ranges::borrowed_iterator_t<decltype(r_out)>>;
+        return ret_type{res.in1, res.in2, res.out};
+    };
+
+    test_range_algo<0, int, data_in_in_out>{big_sz}(dpl_ranges::set_intersection, set_intersection_checker);
+    test_range_algo<0, int, data_in_in_out>{big_sz}(dpl_ranges::set_intersection, set_intersection_checker);
+    test_range_algo<1, int, data_in_in_out>{big_sz}(dpl_ranges::set_intersection, set_intersection_checker, std::ranges::less{}, proj);
+
+    test_range_algo<2, int, data_in_in_out>{}(dpl_ranges::set_intersection, set_intersection_checker, std::ranges::less{}, proj, proj);
+    test_range_algo<3,  P2, data_in_in_out>{}(dpl_ranges::set_intersection, set_intersection_checker, std::ranges::less{}, &P2::x, &P2::x);
+    test_range_algo<4,  P2, data_in_in_out>{}(dpl_ranges::set_intersection, set_intersection_checker, std::ranges::less{}, &P2::proj, &P2::proj);
+#endif //_ENABLE_STD_RANGES_TESTING
+
+    return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
+}
