e6b513f94d69f09fc871f7e971301c7a27be5444
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-14 10:26:44 +0200

    include/oneapi/dpl/pstl/utils.h - extract __is_equality_comparable_with_impl

--- include/oneapi/dpl/pstl/utils.h
+++ include/oneapi/dpl/pstl/utils.h
@@ -842,19 +842,25 @@ struct __has_equality_op<_Iterator1, _Iterator2,
 };
 
 template <typename _Iterator1, typename _Iterator2>
-struct __is_equality_comparable_with : __has_equality_op<_Iterator1, _Iterator2>
+struct __is_equality_comparable_with_impl : __has_equality_op<_Iterator1, _Iterator2>
 {
 };
 
 template <typename _Iterator1, typename _Iterator2>
-struct __is_equality_comparable_with<std::reverse_iterator<_Iterator1>, std::reverse_iterator<_Iterator2>>
-    : __is_equality_comparable_with<_Iterator1, _Iterator2>
+struct __is_equality_comparable_with_impl<std::reverse_iterator<_Iterator1>, std::reverse_iterator<_Iterator2>>
+    : __is_equality_comparable_with_impl<_Iterator1, _Iterator2>
 {
 };
 
 template <typename _Iterator1, typename _Iterator2>
-struct __is_equality_comparable_with<std::move_iterator<_Iterator1>, std::move_iterator<_Iterator2>>
-    : __is_equality_comparable_with<_Iterator1, _Iterator2>
+struct __is_equality_comparable_with_impl<std::move_iterator<_Iterator1>, std::move_iterator<_Iterator2>>
+    : __is_equality_comparable_with_impl<_Iterator1, _Iterator2>
+{
+};
+
+template <typename _Iterator1, typename _Iterator2>
+struct __is_equality_comparable_with
+    : __is_equality_comparable_with_impl<std::decay_t<_Iterator1>, std::decay_t<_Iterator2>>
 {
 };
 
