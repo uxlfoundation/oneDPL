7b85e8fd8b5d372fbcb6981c068bdda490167a84
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-14 09:45:34 +0200

    include/oneapi/dpl/pstl/utils.h - introduce __clean_iterator_t

--- include/oneapi/dpl/pstl/utils.h
+++ include/oneapi/dpl/pstl/utils.h
@@ -56,6 +56,9 @@ namespace dpl
 namespace __internal
 {
 
+template <typename _Iterator>
+using __clean_iterator_t = std::remove_reference_t<_Iterator>;
+
 template <typename Iterator>
 using is_const_iterator =
     typename ::std::is_const<::std::remove_pointer_t<typename ::std::iterator_traits<Iterator>::pointer>>;
