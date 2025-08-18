fb31bf6862ab1fdddab660d6eba7cff3f03f49c6
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-18 16:07:11 +0200

    test/parallel_api/ranges/std_ranges_includes.pass.cpp - expand test

--- test/parallel_api/ranges/std_ranges_includes.pass.cpp
+++ test/parallel_api/ranges/std_ranges_includes.pass.cpp
@@ -24,10 +24,10 @@ main()
 
     auto includes_checker = TEST_PREPARE_CALLABLE(std::ranges::includes);
 
-    test_range_algo<0, int, data_in_in>{big_sz}(dpl_ranges::includes, includes_checker);
-    test_range_algo<1, int, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{});
-
-    test_range_algo<2, int, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, proj);
+    test_range_algo<0,  int, data_in_in>{big_sz}(dpl_ranges::includes, includes_checker);
+    test_range_algo<1,  int, data_in_in>{      }(dpl_ranges::includes, includes_checker, std::ranges::less{});
+    test_range_algo<2,  int, data_in_in>{      }(dpl_ranges::includes, includes_checker, std::ranges::less{}, proj);
+    test_range_algo<3 , int, data_in_in>{      }(dpl_ranges::includes, includes_checker, std::ranges::less{}, proj, proj);
 
     // Check with different projections,
     // but when includes returns true - to make sure that the projections are applied correctly.
@@ -35,11 +35,10 @@ main()
     // but the second is transformed to [0, 3, 6, ...] by its projection.
     auto x1 = [](auto&& v) { return v; };
     auto x3 = [](auto&& v) { return v * 3; };
-    test_range_algo<3, int, data_in_in, decltype(x3), decltype(x1)>{}(
-        dpl_ranges::includes, includes_checker, std::ranges::less{}, x1, x3);
+    test_range_algo<4, int, data_in_in, decltype(x3), decltype(x1)>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, x1, x3);
 
-    test_range_algo<4, P2, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, &P2::x, &P2::x);
-    test_range_algo<5, P2, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, &P2::proj, &P2::proj);
+    test_range_algo<5, P2, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, &P2::x, &P2::x);
+    test_range_algo<6, P2, data_in_in>{}(dpl_ranges::includes, includes_checker, std::ranges::less{}, &P2::proj, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
