87feed94543cfdd184021e0d11ad480928305cdc
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-18 11:58:32 +0200

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - implement __pattern_set_intersection + __parallel_tag

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -778,9 +778,14 @@ __brick_set_intersection(_R1&& __r1, _R2&& __r2, _OutRange&& __out_r, _Comp __co
                                          __comp, __proj1, __proj2);
 }
 
+template <typename _R1, typename _R2, typename _OutRange>
+using __pattern_set_intersection_return_t =
+    std::ranges::set_intersection_result<std::ranges::borrowed_iterator_t<_R1>, std::ranges::borrowed_iterator_t<_R2>,
+                                         std::ranges::borrowed_iterator_t<_OutRange>>;
+
 template <typename _Tag, typename _ExecutionPolicy, typename _R1, typename _R2, typename _OutRange, typename _Comp,
           typename _Proj1, typename _Proj2>
-auto
+__pattern_set_intersection_return_t<_R1, _R2, _OutRange>
 __pattern_set_intersection(_Tag __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2, _OutRange&& __out_r,
                            _Comp __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
@@ -792,12 +797,84 @@ __pattern_set_intersection(_Tag __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R
 
 template <class _IsVector, typename _ExecutionPolicy, typename _R1, typename _R2, typename _OutRange, typename _Comp,
           typename _Proj1, typename _Proj2>
-auto
-__pattern_set_intersection(__parallel_tag<_IsVector>, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2,
+__pattern_set_intersection_return_t<_R1, _R2, _OutRange>
+__pattern_set_intersection(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2,
                            _OutRange&& __out_r, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
-    return std::ranges::set_intersection(std::forward<_R1>(__r1), std::forward<_R2>(__r2), std::ranges::begin(__out_r),
-                                         __comp, __proj1, __proj2);
+    using _RandomAccessIterator1 = std::ranges::iterator_t<_R1>;
+    using _RandomAccessIterator2 = std::ranges::iterator_t<_R2>;
+    using _Tp = std::ranges::range_value_t<_OutRange>;
+
+    const auto __n1 = std::ranges::size(__r1);
+    const auto __n2 = std::ranges::size(__r2);
+
+    auto __first1 = std::ranges::begin(__r1);
+    auto __last1 = __first1 + __n1;
+    auto __first2 = std::ranges::begin(__r2);
+    auto __last2 = __first2 + __n2;
+    auto __result = std::ranges::begin(__out_r);
+
+    // intersection is empty
+    if (__n1 == 0 || __n2 == 0)
+        return __pattern_set_intersection_return_t<_R1, _R2, _OutRange>{__first1, __first2, __result};
+
+    // testing  whether the sequences are intersected
+    auto __left_bound_seq_1 =
+        oneapi::dpl::__internal::__pstl_lower_bound(oneapi::dpl::__internal::_SubscriptAdapter{}, __first1, __last1,
+                                                    std::invoke(__proj2, *__first2), __comp, __proj1);
+    //{1} < {2}: seq 2 is wholly greater than seq 1, so, the intersection is empty
+    if (__left_bound_seq_1 == __last1)
+        return __pattern_set_intersection_return_t<_R1, _R2, _OutRange>{__first1, __first2, __result};
+
+    // testing  whether the sequences are intersected
+    auto __left_bound_seq_2 =
+        oneapi::dpl::__internal::__pstl_lower_bound(oneapi::dpl::__internal::_SubscriptAdapter{}, __first2, __last2,
+                                                    std::invoke(__proj1, *__first1), __comp, __proj2);
+    //{2} < {1}: seq 1 is wholly greater than seq 2, so, the intersection is empty
+    if (__left_bound_seq_2 == __last2)
+        return __pattern_set_intersection_return_t<_R1, _R2, _OutRange>{__first1, __first2, __result};
+
+    const auto __m1 = __last1 - __left_bound_seq_1 + __n2;
+    if (__m1 > oneapi::dpl::__internal::__set_algo_cut_off)
+    {
+        //we know proper offset due to [first1; left_bound_seq_1) < [first2; last2)
+        auto __out_last = oneapi::dpl::__internal::__parallel_set_union_op(
+            __tag, std::forward<_ExecutionPolicy>(__exec), __left_bound_seq_1, __last1, __first2, __last2, __result,
+            __comp,
+            [](_RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2,
+               _RandomAccessIterator2 __last2, _Tp* __result, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2) {
+                return oneapi::dpl::__utils::__set_intersection_construct(
+                    __first1, __last1, __first2, __last2, __result, __comp,
+                    oneapi::dpl::__internal::__op_uninitialized_copy<_ExecutionPolicy>{},
+                    /*CopyFromFirstSet = */ std::true_type{}, __proj1, __proj2);
+            },
+            __proj1, __proj2);
+
+        return __pattern_set_intersection_return_t<_R1, _R2, _OutRange>{__last1, __last2, __out_last};
+    }
+
+    const auto __m2 = __last2 - __left_bound_seq_2 + __n1;
+    if (__m2 > oneapi::dpl::__internal::__set_algo_cut_off)
+    {
+        //we know proper offset due to [first2; left_bound_seq_2) < [first1; last1)
+        auto __out_last = oneapi::dpl::__internal::__parallel_set_union_op(
+            __tag, std::forward<_ExecutionPolicy>(__exec), __first1, __last1, __left_bound_seq_2, __last2, __result,
+            __comp,
+            [](_RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2,
+               _RandomAccessIterator2 __last2, _Tp* __result, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2) {
+                return oneapi::dpl::__utils::__set_intersection_construct(
+                    __first1, __last1, __first2, __last2, __result, __comp,
+                    oneapi::dpl::__internal::__op_uninitialized_copy<_ExecutionPolicy>{},
+                    /*CopyFromFirstSet = */ std::false_type{}, __proj1, __proj2);
+            },
+            __proj1, __proj2);
+
+        return __pattern_set_intersection_return_t<_R1, _R2, _OutRange>{__last1, __last2, __out_last};
+    }
+
+    // [left_bound_seq_1; last1) and [left_bound_seq_2; last2) - use serial algorithm
+    return std::ranges::set_intersection(__left_bound_seq_1, __last1, __left_bound_seq_2, __last2,
+                                         std::ranges::begin(__out_r), __comp, __proj1, __proj2);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
