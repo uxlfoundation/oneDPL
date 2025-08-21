e1c5ab08e2bb344e8bc19306b8ba14f1c625ac72
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2025-08-21 19:29:29 +0100

    Improve set_symmetric_difference test

--- test/parallel_api/ranges/std_ranges_set_symmetric_difference.pass.cpp
+++ test/parallel_api/ranges/std_ranges_set_symmetric_difference.pass.cpp
@@ -29,7 +29,80 @@ int out_size_with_empty_in1<std::remove_cvref_t<decltype(oneapi::dpl::ranges::se
     return in2_size;
 }
 }
-#endif
+
+struct A
+{
+    int a;
+    operator int() const { return a; }
+};
+
+struct B
+{
+    int b;
+    operator int() const { return b; }
+};
+
+void test_mixed_types_host()
+{
+    std::vector<A> r1 = {{1}, {2}, {5}};
+    std::vector<B> r2 = {{0}, {2}, {2}, {3}};
+
+    std::vector<int> out_expected = {1, 5, 3};
+
+    std::vector<int> out_seq(out_expected.size(), 0xCD);
+    std::vector<int> out_par(out_expected.size(), 0xCD);
+    std::vector<int> out_unseq(out_expected.size(), 0xCD);
+    std::vector<int> out_par_unseq(out_expected.size(), 0xCD);
+
+    auto proj_a = [](const A& a) { return a.a; };
+    auto proj_b = [](const B& b) { return b.b; };
+
+    oneapi::dpl::ranges::set_symmetric_difference(
+        oneapi::dpl::execution::seq, r1, r2, out_seq, std::ranges::less{}, proj_a, proj_b);
+    oneapi::dpl::ranges::set_symmetric_difference(
+        oneapi::dpl::execution::par, r1, r2, out_par, std::ranges::less{}, proj_a, proj_b);
+    oneapi::dpl::ranges::set_symmetric_difference(
+        oneapi::dpl::execution::unseq, r1, r2, out_unseq, std::ranges::less{}, proj_a, proj_b);
+    oneapi::dpl::ranges::set_symmetric_difference(
+        oneapi::dpl::execution::par_unseq, r1, r2, out_par_unseq, std::ranges::less{}, proj_a, proj_b);
+
+    EXPECT_EQ_RANGES(out_expected, out_seq, "wrong result with seq policy");
+    EXPECT_EQ_RANGES(out_expected, out_par, "wrong result with par policy");
+    EXPECT_EQ_RANGES(out_expected, out_unseq, "wrong result with unseq policy");
+    EXPECT_EQ_RANGES(out_expected, out_par_unseq, "wrong result with par_unseq policy");
+}
+
+#if TEST_DPCPP_BACKEND_PRESENT
+void test_mixed_types_device()
+{
+    auto policy = TestUtils::get_dpcpp_test_policy();
+    sycl::queue q = policy.queue();
+    if (q.get_device().has(sycl::aspect::usm_shared_allocations))
+    {
+        using r1_alloc_t = sycl::usm_allocator<A, sycl::usm::alloc::shared>;
+        using r2_alloc_t = sycl::usm_allocator<B, sycl::usm::alloc::shared>;
+        using r_out_alloc_t = sycl::usm_allocator<int, sycl::usm::alloc::shared>;
+
+        std::vector<A, r1_alloc_t> v1({{1}, {2}, {5}}, r1_alloc_t(q));
+        std::vector<B, r2_alloc_t> v2({{0}, {2}, {2}, {3}}, r2_alloc_t(q));
+        std::vector<int> out_expected = {1, 3, 5};
+
+        std::vector<int, r_out_alloc_t> out(out_expected.size(), 0xCD, r_out_alloc_t(q));
+
+        // Wrap vector with a USM allocator into the subrange because it is not device copyable
+        std::ranges::subrange r1(v1.data(), v1.data() + v1.size());
+        std::ranges::subrange r2(v2.data(), v2.data() + v2.size());
+        std::ranges::subrange r_out(out.data(), out.data() + out.size());
+
+        auto proj_a = [](const A& a) { return a.a; };
+        auto proj_b = [](const B& b) { return b.b; };
+
+        oneapi::dpl::ranges::set_symmetric_difference(policy, r1, r2, r_out, std::ranges::less{}, proj_a, proj_b);
+        EXPECT_EQ_RANGES(out_expected, out, "wrong result with device policy");
+    }
+}
+#endif // TEST_DPCPP_BACKEND_PRESENT
+#endif // _ENABLE_STD_RANGES_TESTING
 
 std::int32_t
 main()
@@ -63,6 +136,13 @@ main()
 
     test_range_algo<3,  P2, data_in_in_out, mul1_t, div3_t>{}(dpl_ranges::set_symmetric_difference, checker, std::ranges::less{}, &P2::x, &P2::x);
     test_range_algo<4,  P2, data_in_in_out, mul1_t, div3_t>{}(dpl_ranges::set_symmetric_difference, checker, std::ranges::less{}, &P2::proj, &P2::proj);
+
+    // Check if projections are applied to the right sequences and trigger a compile-time error if not
+    test_mixed_types_host();
+#if TEST_DPCPP_BACKEND_PRESENT
+    test_mixed_types_device();
+#endif
+
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
