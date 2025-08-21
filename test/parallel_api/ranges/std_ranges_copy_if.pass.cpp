6be021ea785c500fa9ff1d21121bf7af54611460
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-21 18:25:59 +0200

    test/parallel_api/ranges/std_ranges_copy_if.pass.cpp - remove extra code from checker

--- test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
@@ -25,12 +25,8 @@ main()
     auto copy_if_checker = [](std::ranges::random_access_range auto&& r_in,
                            std::ranges::random_access_range auto&& r_out, auto&&... args)
     {
-        auto res = std::ranges::copy_if(std::forward<decltype(r_in)>(r_in), std::ranges::begin(r_out),
+        return std::ranges::copy_if(std::forward<decltype(r_in)>(r_in), std::ranges::begin(r_out),
             std::forward<decltype(args)>(args)...);
-
-        using ret_type = std::ranges::copy_if_result<std::ranges::borrowed_iterator_t<decltype(r_in)>,
-            std::ranges::borrowed_iterator_t<decltype(r_out)>>;
-        return ret_type{res.in, res.out};
     };
 
     test_range_algo<0, int, data_in_out>{big_sz}(dpl_ranges::copy_if,  copy_if_checker, pred);
