b2edc81a630b748df15ae50c78e4f341ab590cd6
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-21 18:25:44 +0200

    test/parallel_api/ranges/std_ranges_set_difference.pass.cpp - remove extra code from checker

--- test/parallel_api/ranges/std_ranges_set_difference.pass.cpp
+++ test/parallel_api/ranges/std_ranges_set_difference.pass.cpp
@@ -41,12 +41,8 @@ main()
                       std::ranges::random_access_range auto&& r2,
                       std::ranges::random_access_range auto&& r_out, auto&&... args)
     {
-        auto res = std::ranges::set_difference(std::forward<decltype(r1)>(r1), std::forward<decltype(r2)>(r2),
-                                               std::ranges::begin(r_out), std::forward<decltype(args)>(args)...);
-
-        using ret_type = std::ranges::set_difference_result<std::ranges::borrowed_iterator_t<decltype(r1)>,
-                                                            std::ranges::borrowed_iterator_t<decltype(r_out)>>;
-        return ret_type{res.in, res.out};
+        return std::ranges::set_difference(std::forward<decltype(r1)>(r1), std::forward<decltype(r2)>(r2),
+                                           std::ranges::begin(r_out), std::forward<decltype(args)>(args)...);
     };
 
     test_range_algo<0, int, data_in_in_out, div3_t, mul1_t>{big_sz}(dpl_ranges::set_difference, checker);
