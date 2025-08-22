a7ca219805defd8f90653f0442bed61b2c467f73
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-22 19:28:20 +0200

    include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h - struct __adjacent_find_fn : replace auto to std::ranges::borrowed_iterator_t<_R>

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -327,7 +327,7 @@ struct __adjacent_find_fn
              std::indirect_binary_predicate<std::projected<std::ranges::iterator_t<_R>, _Proj>,
              std::projected<std::ranges::iterator_t<_R>, _Proj>> _Pred = std::ranges::equal_to>
     requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> && std::ranges::sized_range<_R>
-    auto
+    std::ranges::borrowed_iterator_t<_R>
     operator()(_ExecutionPolicy&& __exec, _R&& __r, _Pred __pred = {}, _Proj __proj = {}) const
     {
         const auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend(__exec);
