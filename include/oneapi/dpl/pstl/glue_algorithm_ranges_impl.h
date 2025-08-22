f5b29a219eabdb7a2b3136c4d47d28afdd2540ff
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-22 19:26:47 +0200

    include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h - struct __find_if_fn : replace auto to std::ranges::borrowed_iterator_t<_R>

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -146,7 +146,7 @@ struct __find_if_fn
     template<typename _ExecutionPolicy, std::ranges::random_access_range _R, typename _Proj = std::identity,
              std::indirect_unary_predicate<std::projected<std::ranges::iterator_t<_R>, _Proj>> _Pred>
     requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> && std::ranges::sized_range<_R>
-    auto
+    std::ranges::borrowed_iterator_t<_R>
     operator()(_ExecutionPolicy&& __exec, _R&& __r, _Pred __pred, _Proj __proj = {}) const
     {
         const auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend(__exec);
