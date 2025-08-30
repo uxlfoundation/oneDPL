3b3c9e542233a49996bbf2c58d0d1ef6a4f2732b
Sergey Kopienko <sergey.kopienko@intel.com>
2025-08-30 13:13:30 +0200

    include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h - __pattern_search : auto -> std::ranges::borrowed_subrange_t<_R1>

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -416,7 +416,7 @@ __pattern_search(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _Ra
 #if _ONEDPL_CPP20_RANGES_PRESENT
 template<typename _BackendTag, typename _ExecutionPolicy, typename _R1, typename _R2, typename _Pred, typename _Proj1,
          typename _Proj2>
-auto
+std::ranges::borrowed_subrange_t<_R1>
 __pattern_search(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2, _Pred __pred,
                  _Proj1 __proj1, _Proj2 __proj2)
 {
