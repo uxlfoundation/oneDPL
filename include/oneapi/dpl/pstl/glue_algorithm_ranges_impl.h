22912745d39ce9187dcefaf2723a8c803533f58c
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-01 15:09:06 +0200

    Apply GitHUB clang format

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -23,7 +23,7 @@
 #    include <type_traits>
 #    include <iterator>
 #    include <algorithm> // std::ranges::sort, std::ranges::stable_sort
-#endif // _ONEDPL_CPP20_RANGES_PRESENT
+#endif                   // _ONEDPL_CPP20_RANGES_PRESENT
 
 #include "utils_ranges.h" // __difference_t
 #include "utils.h"        // oneapi::dpl::__internal::__swap_ranges_fn
