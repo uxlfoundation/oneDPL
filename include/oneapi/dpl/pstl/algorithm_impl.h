00039b61cb6d3789174cf8411353244ea255385f
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-11 16:30:59 +0200

    Partially apply GitHUB clang format

--- include/oneapi/dpl/pstl/algorithm_impl.h
+++ include/oneapi/dpl/pstl/algorithm_impl.h
@@ -3275,8 +3275,8 @@ template <class _IsVector, class _ExecutionPolicy, class _RandomAccessIterator1,
 _OutputIterator
 __parallel_set_op(__parallel_tag<_IsVector>, _ExecutionPolicy&& __exec, _RandomAccessIterator1 __first1,
                   _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2,
-                  _OutputIterator __result, _SizeFunction __size_func, _SetOP __set_op,
-                  _Compare __comp, _Proj1 __proj1, _Proj2 __proj2)
+                  _OutputIterator __result, _SizeFunction __size_func, _SetOP __set_op, _Compare __comp, _Proj1 __proj1,
+                  _Proj2 __proj2)
 {
     using __backend_tag = typename __parallel_tag<_IsVector>::__backend_tag;
 
@@ -3331,9 +3331,8 @@ __parallel_set_op(__parallel_tag<_IsVector>, _ExecutionPolicy&& __exec, _RandomA
                 {
                     _RandomAccessIterator2 __bb = __last2;
                     if (__b != __last1)
-                        __bb =
-                            __first2 + __internal::__pstl_lower_bound(__first2, _DifferenceType2{0}, __last2 - __first2,
-                                                                      std::invoke(__proj1, *__b), __comp, __proj2);
+                        __bb = __first2 + __internal::__pstl_lower_bound(__first2, _DifferenceType2{0}, __last2 - __first2,
+                                                                         std::invoke(__proj1, *__b), __comp, __proj2);
 
                     const _DifferenceType1 __buf_pos = __size_func((__b - __first1), (__bb - __first2));
                     return _SetRange{0, 0, __buf_pos};
--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -832,7 +832,7 @@ __pattern_set_union(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, 
                 __first1, __last1, __first2, __last2, __result,
                 oneapi::dpl::__internal::__BrickCopyConstruct<_IsVector>(), __comp, __proj1, __proj2);
         },
-         __comp, __proj1, __proj2);
+        __comp, __proj1, __proj2);
 
     return __pattern_set_union_return_t<_R1, _R2, _OutRange>{__first1 + __n1, __first2 + __n2,
                                                              __result + (__out_last - __result)};
