4038fa1c76fe8d4f266c32a58dfa89246c5dd9fb
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2025-09-15 10:26:30 +0100

    Default-construct identity implicitly

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -834,7 +834,7 @@ __parallel_scan_copy(sycl::queue& __q, _InRng&& __in_rng, _OutRng&& __out_rng, _
 
     _Assigner __assign_op;
     _ReduceOp __reduce_op;
-    _DataAcc __get_data_op{oneapi::dpl::identity{}};
+    _DataAcc __get_data_op{};
     _MaskAssigner __add_mask_op;
 
     // temporary buffer to store boolean mask
@@ -1143,7 +1143,7 @@ __parallel_set_scan(_SetTag, sycl::queue& __q, _Range1&& __rng1, _Range2&& __rng
 
     _ReduceOp __reduce_op;
     _Assigner __assign_op;
-    _DataAcc __get_data_op{oneapi::dpl::identity{}};
+    _DataAcc __get_data_op{};
     unseq_backend::__copy_by_mask<_ReduceOp, oneapi::dpl::__internal::__pstl_assign, /*inclusive*/ std::true_type, 2>
         __copy_by_mask_op;
     unseq_backend::__brick_set_op<_SetTag, _Compare, _Size1, _Size2> __create_mask_op{__comp, __n1, __n2};
