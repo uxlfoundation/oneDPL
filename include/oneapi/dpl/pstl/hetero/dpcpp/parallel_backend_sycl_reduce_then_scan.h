d086d1b72826761c5bd9a2b1d48ac26c6e56873a
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-17 13:54:39 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h - refactoring of struct __gen_set_mask::operator() : get source tuple only once

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
@@ -381,11 +381,15 @@ struct __gen_set_mask
     bool
     operator()(const _InRng& __in_rng, std::size_t __id) const
     {
+        // Get tuple from source range
+        auto&& __tuple = __in_rng.tuple();
+        using _tuple_t = decltype(__tuple);
+
         // First we must extract individual sequences from zip iterator because they may not have the same length,
         // dereferencing is dangerous
-        auto __set_a = std::get<0>(__in_rng.tuple());    // first sequence, use with __proj1
-        auto __set_b = std::get<1>(__in_rng.tuple());    // second sequence, use with __proj2
-        auto __set_mask = std::get<2>(__in_rng.tuple()); // mask sequence
+        auto __set_a = std::get<0>(std::forward<_tuple_t>(__tuple));    // first sequence, use with __proj1
+        auto __set_b = std::get<1>(std::forward<_tuple_t>(__tuple));    // second sequence, use with __proj2
+        auto __set_mask = std::get<2>(std::forward<_tuple_t>(__tuple)); // mask sequence
 
         std::size_t __nb = __set_b.size();
 
