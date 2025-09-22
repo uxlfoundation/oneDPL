25a9bae68e24c67b16641c26d70bb9d74ab00442
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-22 17:50:46 +0200

    test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp - log exceptions

--- test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp
+++ test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp
@@ -18,21 +18,24 @@
 int
 main()
 {
-    using policy_t = oneapi::dpl::experimental::round_robin_policy<TestUtils::int_inline_backend_t>;
-    std::vector<int> u{4, 5, 6, 7};
-    auto f = [u](int i) { return u[(i - 1) % 4]; };
+    try
+    {
+        using policy_t = oneapi::dpl::experimental::round_robin_policy<TestUtils::int_inline_backend_t>;
+        std::vector<int> u{4, 5, 6, 7};
+        auto f = [u](int i) { return u[(i - 1) % 4]; };
 
-    constexpr bool just_call_submit = false;
-    constexpr bool call_select_before_submit = true;
+        constexpr bool just_call_submit = false;
+        constexpr bool call_select_before_submit = true;
 
-    EXPECT_EQ(0, (test_initialization<policy_t, int>(u)), "");
-    EXPECT_EQ(0, (test_select<policy_t, decltype(u), decltype(f)&, false>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait<just_call_submit, policy_t>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait<call_select_before_submit, policy_t>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f)), "");
-    EXPECT_EQ(0, (test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_initialization<policy_t, int>(u)), "");
+        EXPECT_EQ(0, (test_select<policy_t, decltype(u), decltype(f)&, false>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait<just_call_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait<call_select_before_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f)), "");
+        EXPECT_EQ(0, (test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f)), "");
+    }
 
     return TestUtils::done();
 }
