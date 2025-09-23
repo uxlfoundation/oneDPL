cd4b416ac96b407448dbecdd524f4c4b3497c476
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-23 17:41:24 +0200

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - fix review comment: restore old predicate name in __pattern_for_each

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -46,10 +46,10 @@ __pattern_for_each(_Tag __tag, _ExecutionPolicy&& __exec, _R&& __r, _Fun __f, _P
 {
     static_assert(__is_parallel_tag_v<_Tag> || typename _Tag::__is_vector{});
 
-    oneapi::dpl::__internal::__unary_op<_Fun, _Proj> __pred_1{__f, __proj};
+    oneapi::dpl::__internal::__unary_op<_Fun, _Proj> __f_1{__f, __proj};
 
     oneapi::dpl::__internal::__pattern_walk1(__tag, std::forward<_ExecutionPolicy>(__exec), std::ranges::begin(__r),
-                                             std::ranges::begin(__r) + std::ranges::size(__r), __pred_1);
+                                             std::ranges::begin(__r) + std::ranges::size(__r), __f_1);
 }
 
 template <typename _ExecutionPolicy, typename _R, typename _Fun, typename _Proj>
