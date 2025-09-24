aa5f8b183fdba2d40aa5b4c98e6d246496022dc2
timmiesmith <timmie.smith@intel.com>
2025-09-24 09:08:46 -0500

    Revert changes to reflect env directory location in oneDPL repo.

--- integration/env/vars.bat
+++ integration/env/vars.bat
@@ -17,9 +17,8 @@ REM
 
 set "VARSDIR=%~dp0"
 
-REM The vars script is in the integration\env directory. The DPL root directory is up two levels
-if not defined DPL_ROOT for /f "delims=" %%F in ("%VARSDIR%..\..") do set "DPL_ROOT=%%~fF"
-if not defined DPLROOT for /f "delims=" %%F in ("%VARSDIR%..\..") do set "DPLROOT=%%~fF"
+if not defined DPL_ROOT for /f "delims=" %%F in ("%VARSDIR%..") do set "DPL_ROOT=%%~fF"
+if not defined DPLROOT for /f "delims=" %%F in ("%VARSDIR%..") do set "DPLROOT=%%~fF"
 
 set "CPLUS_INCLUDE_PATH=%DPL_ROOT%\include;%CPLUS_INCLUDE_PATH%"
 set "PKG_CONFIG_PATH=%DPL_ROOT%\lib\pkgconfig;%PKG_CONFIG_PATH%"
