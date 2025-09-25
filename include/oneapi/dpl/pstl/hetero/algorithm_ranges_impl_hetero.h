44e924cc6bb2ef0d1b6d62d9794344562238a6c1
Sergey Kopienko <sergey.kopienko@intel.com>
2025-09-25 10:38:02 +0200

    include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h - apply GitHUB clang format

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -852,9 +852,8 @@ __pattern_unique(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _R&
 
     auto __beg = std::ranges::begin(__r);
     auto __end = __beg + std::ranges::size(__r);
-    auto __idx = oneapi::dpl::__internal::__ranges::__pattern_unique(
-        __tag, std::forward<_ExecutionPolicy>(__exec), oneapi::dpl::__ranges::views::all(__r),
-        __pred_2);
+    auto __idx = oneapi::dpl::__internal::__ranges::__pattern_unique(__tag, std::forward<_ExecutionPolicy>(__exec),
+                                                                     oneapi::dpl::__ranges::views::all(__r), __pred_2);
 
     return std::ranges::borrowed_subrange_t<_R>(__beg + __idx, __end);
 }
