b84286e2b10893caa1f8118a9992ceb4ca943c7c
Alexey Kukanov <alexey.kukanov@intel.com>
2025-09-26 17:43:59 +0200

    Enable testing with limited output

--- test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
@@ -41,10 +41,10 @@ main()
         return ret_type{it_in, it_out};
     };
 
-    test_range_algo<0, int, data_in_out>{big_sz}(dpl_ranges::copy_if,  copy_if_checker, pred, std::identity{});
-    test_range_algo<1, int, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, proj);
-    test_range_algo<2, P2, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::x);
-    test_range_algo<3, P2, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::proj);
+    test_range_algo<0, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if,  copy_if_checker, pred, std::identity{});
+    test_range_algo<1, int, data_in_out_lim>{}(dpl_ranges::copy_if,  copy_if_checker, pred, proj);
+    test_range_algo<2, P2, data_in_out_lim>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::x);
+    test_range_algo<3, P2, data_in_out_lim>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
