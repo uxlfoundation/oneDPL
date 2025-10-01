55103c0846bdfd358bd49e7b53ec1e950e05d275
Sergey Kopienko <sergey.kopienko@intel.com>
2025-10-01 12:13:39 +0200

    test/parallel_api/ranges/range_minimal_type_requirements.pass.cpp - apply re-designed MinimalisticRange in the code

--- test/parallel_api/ranges/range_minimal_type_requirements.pass.cpp
+++ test/parallel_api/ranges/range_minimal_type_requirements.pass.cpp
@@ -33,7 +33,8 @@ template <typename Policy>
 void test_count(Policy policy)
 {
     std::vector<int> v = {0, 1, 2, 3, 4, 5};
-    TestUtils::MinimalisticRange r{v};
+
+    TestUtils::MinimalisticRange r{v.begin(), v.end()};
     auto count = oneapi::dpl::ranges::count(policy, r, 3);
     std::string msg = "wrong return value from count, " + std::string(typeid(Policy).name());
     EXPECT_EQ(count, 1, msg.c_str());
@@ -46,9 +47,10 @@ void test_merge(Policy policy)
     std::vector<int> v2 = {1, 3, 5, 7, 9, 11};
     std::vector<int> v3_expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     std::vector<int> v3(12, 42);
-    TestUtils::MinimalisticRange r1{v1};
-    TestUtils::MinimalisticRange r2{v2};
-    TestUtils::MinimalisticRange r3{v3};
+
+    TestUtils::MinimalisticRange r1{v1.begin(), v1.end()};
+    TestUtils::MinimalisticRange r2{v2.begin(), v2.end()};
+    TestUtils::MinimalisticRange r3{v3.begin(), v3.end()};
 
     oneapi::dpl::ranges::merge(policy, r1, r2, r3);
     std::string msg = "wrong effect from merge, " + std::string(typeid(Policy).name());
