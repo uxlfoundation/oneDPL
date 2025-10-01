fb55cf1a9e2519c5bb0283a997154e14c351e663
Sergey Kopienko <sergey.kopienko@intel.com>
2025-10-01 14:04:42 +0200

    test/parallel_api/ranges/std_ranges_set_difference.pass.cpp - apply re-designed MinimalisticRange in the code for host policies

--- test/parallel_api/ranges/std_ranges_set_difference.pass.cpp
+++ test/parallel_api/ranges/std_ranges_set_difference.pass.cpp
@@ -25,26 +25,19 @@ inline int out_size_with_empty_in2<std::remove_cvref_t<decltype(oneapi::dpl::ran
 }
 }
 
-void test_mixed_types_host()
+template <typename RngA, typename RngB, typename RngOutExpected>
+void
+test_mixed_types_host(RngA&& __rngA, RngB&& __rngB, const RngOutExpected& out_expected)
 {
-    std::vector<test_std_ranges::A> r1 = {{1}, {2}, {5}};
-    std::vector<test_std_ranges::B> r2 = {{0}, {2}, {2}, {3}};
-
-    std::vector<int> out_expected = {1, 5};
-
-    std::vector<int> out_seq(out_expected.size(), 0xCD);
-    std::vector<int> out_par(out_expected.size(), 0xCD);
-    std::vector<int> out_unseq(out_expected.size(), 0xCD);
+    std::vector<int> out_seq      (out_expected.size(), 0xCD);
+    std::vector<int> out_par      (out_expected.size(), 0xCD);
+    std::vector<int> out_unseq    (out_expected.size(), 0xCD);
     std::vector<int> out_par_unseq(out_expected.size(), 0xCD);
 
-    oneapi::dpl::ranges::set_difference(
-        oneapi::dpl::execution::seq, r1, r2, out_seq, std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
-    oneapi::dpl::ranges::set_difference(
-        oneapi::dpl::execution::par, r1, r2, out_par, std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
-    oneapi::dpl::ranges::set_difference(
-        oneapi::dpl::execution::unseq, r1, r2, out_unseq, std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
-    oneapi::dpl::ranges::set_difference(
-        oneapi::dpl::execution::par_unseq, r1, r2, out_par_unseq, std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
+    oneapi::dpl::ranges::set_difference(oneapi::dpl::execution::seq,       __rngA, __rngB, out_seq,       std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
+    oneapi::dpl::ranges::set_difference(oneapi::dpl::execution::par,       __rngA, __rngB, out_par,       std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
+    oneapi::dpl::ranges::set_difference(oneapi::dpl::execution::unseq,     __rngA, __rngB, out_unseq,     std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
+    oneapi::dpl::ranges::set_difference(oneapi::dpl::execution::par_unseq, __rngA, __rngB, out_par_unseq, std::ranges::less{}, test_std_ranges::proj_a, test_std_ranges::proj_b);
 
     EXPECT_EQ_RANGES(out_expected, out_seq, "wrong result with seq policy");
     EXPECT_EQ_RANGES(out_expected, out_par, "wrong result with par policy");
@@ -122,7 +115,16 @@ main()
     test_range_algo<6, int, data_in_in_out, ls_t, mul1_t>{1000}(dpl_ranges::set_difference, checker);
 
     // Check if projections are applied to the right sequences and trigger a compile-time error if not
-    test_mixed_types_host();
+    std::vector<test_std_ranges::A> vec_a_src = {{1}, {2}, {5}};
+    std::vector<test_std_ranges::B> vec_b_src = {{0}, {2}, {2}, {3}};
+    std::vector<int> out_expected = {1, 5};
+
+    test_mixed_types_host(vec_a_src, vec_b_src, out_expected);
+
+    TestUtils::MinimalisticRange vec_a{vec_a_src.begin(), vec_a_src.end()};
+    TestUtils::MinimalisticRange vec_b{vec_b_src.begin(), vec_b_src.end()};
+    test_mixed_types_host(vec_a, vec_b, out_expected);
+
 #if TEST_DPCPP_BACKEND_PRESENT
     test_mixed_types_device();
 #endif
