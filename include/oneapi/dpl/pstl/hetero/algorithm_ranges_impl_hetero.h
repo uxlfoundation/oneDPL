c01fa4f45cd168813cbb52bc608084d441478e9b
Sergey Kopienko <sergey.kopienko@intel.com>
2025-10-29 13:51:38 +0100

    include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h - additional fixes

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -672,10 +672,12 @@ __pattern_copy_if(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Range1&
     if (__n == 0)
         return 0;
 
+    // We should extend the lifetime of __get_subscription_view() result until future::get() is finished
+    auto&& __keep_rng1_sv = oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range1>(__rng1)); // near __checked_deferrable_wait - FIXED
+    auto&& __keep_rng2_sv = oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range2>(__r__rng2ng1)); // near __checked_deferrable_wait - FIXED
+
     auto __res = oneapi::dpl::__par_backend_hetero::__parallel_copy_if(
-        _BackendTag{}, std::forward<_ExecutionPolicy>(__exec),
-        oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range1>(__rng1)),
-        oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range2>(__rng2)), __n, __pred, __assign);
+        _BackendTag{}, std::forward<_ExecutionPolicy>(__exec), __keep_rng1_sv, __keep_rng2_sv, __n, __pred, __assign);
 
     return __res.get(); //is a blocking call
 }
@@ -966,11 +968,14 @@ __pattern_merge(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _Ran
         return {__res, 0};
     }
 
+    // We should extend the lifetime of __get_subscription_view() result until future::get() is finished
+    auto&& __keep_rng1_sv = oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range1>(__rng1)); // near __checked_deferrable_wait - FIXED
+    auto&& __keep_rng2_sv = oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range2>(__rng2)); // near __checked_deferrable_wait - FIXED
+    auto&& __keep_rng3_sv = oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range3>(__rng3)); // near __checked_deferrable_wait - FIXED
+
     auto __res = __par_backend_hetero::__parallel_merge<std::true_type /*out size limit*/>(
-        _BackendTag{}, ::std::forward<_ExecutionPolicy>(__exec),
-        oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range1>(__rng1)),
-        oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range2>(__rng2)),
-        oneapi::dpl::__ranges::__get_subscription_view(std::forward<_Range3>(__rng3)), __comp, __proj1, __proj2);
+        _BackendTag{}, ::std::forward<_ExecutionPolicy>(__exec), __keep_rng1_sv, __keep_rng2_sv, __keep_rng3_sv, __comp,
+        __proj1, __proj2);
 
     auto __val = __res.get();
     return {__val.first, __val.second};
