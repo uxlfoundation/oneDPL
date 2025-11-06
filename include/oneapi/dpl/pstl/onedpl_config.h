c517a5ed0b1d9597350132c31ee4f5b859cd5d01
Sergey Kopienko <sergey.kopienko@intel.com>
2025-11-06 13:45:47 +0100

    include/oneapi/dpl/pstl/onedpl_config.h - introduce the new macro ONEDPL_CPP20_REMOVE_CVREF_T_PRESENT for check std::remove_cvref_t

--- include/oneapi/dpl/pstl/onedpl_config.h
+++ include/oneapi/dpl/pstl/onedpl_config.h
@@ -272,6 +272,7 @@
 
 #if _ONEDPL_STD_FEATURE_MACROS_PRESENT
 #    define _ONEDPL_CPP20_CONCEPTS_PRESENT (__cpp_concepts >= 201907L && __cpp_lib_concepts >= 202002L)
+#    define _ONEDPL_CPP20_REMOVE_CVREF_T_PRESENT (__cpp_lib_remove_cvref >= 201711L)
 #    define _ONEDPL_CPP20_SPAN_PRESENT (__cpp_lib_span >= 202002L)
 #    define _ONEDPL_CPP23_TUPLE_LIKE_COMMON_REFERENCE_PRESENT                                                          \
         (_ONEDPL___cplusplus >= 202302L && __cpp_lib_tuple_like >= 202207L)
@@ -279,6 +280,7 @@
 #    define _ONEDPL_CPP26_DEFAULT_VALUE_TYPE_PRESENT (__cpp_lib_algorithm_default_value_type >= 202403L)
 #else
 #    define _ONEDPL_CPP20_CONCEPTS_PRESENT 0
+#    define _ONEDPL_CPP20_REMOVE_CVREF_T_PRESENT 0
 #    define _ONEDPL_CPP20_SPAN_PRESENT 0
 #    define _ONEDPL_CPP23_TUPLE_LIKE_COMMON_REFERENCE_PRESENT 0
 #    define _ONEDPL_CPP23_RANGES_ZIP_PRESENT 0
