05b5d3c151abc0e6e4f108640d03705638d9f7dc
Sergey Kopienko <sergey.kopienko@intel.com>
2025-11-11 18:37:44 +0100

    @@@

--- include/oneapi/dpl/pstl/memory_impl.h
+++ include/oneapi/dpl/pstl/memory_impl.h
@@ -35,29 +35,33 @@ namespace __internal
 
 template <typename _ForwardIterator, typename _OutputIterator>
 _OutputIterator
-__brick_uninitialized_move(_ForwardIterator __first, _ForwardIterator __last, _OutputIterator __result,
+__brick_uninitialized_move(_ForwardIterator __first, _ForwardIterator __last,       // bounds for data1
+                           _OutputIterator __result1, _OutputIterator __result2,    // bounds for results
                            /*vector=*/::std::false_type) noexcept
 {
     using _ValueType = typename ::std::iterator_traits<_OutputIterator>::value_type;
-    for (; __first != __last; ++__first, (void)++__result)
+    for (; __first != __last && __result1 != __result2; ++__first, (void)++__result1)
     {
-        ::new (::std::addressof(*__result)) _ValueType(::std::move(*__first));
+        ::new (std::addressof(*__result1)) _ValueType(::std::move(*__first));
     }
-    return __result;
+    return __result1;
 }
 
 template <typename _RandomAccessIterator, typename _OutputIterator>
 _OutputIterator
-__brick_uninitialized_move(_RandomAccessIterator __first, _RandomAccessIterator __last, _OutputIterator __result,
+__brick_uninitialized_move(_RandomAccessIterator __first, _RandomAccessIterator __last, // bounds for data1
+                           _OutputIterator __result1, _OutputIterator __result2,        // bounds for results
                            /*vector=*/::std::true_type) noexcept
 {
     using __ValueType = typename ::std::iterator_traits<_OutputIterator>::value_type;
     using _ReferenceType1 = typename ::std::iterator_traits<_RandomAccessIterator>::reference;
     using _ReferenceType2 = typename ::std::iterator_traits<_OutputIterator>::reference;
 
-    return __unseq_backend::__simd_walk_n(__last - __first,
-        [](_ReferenceType1 __x, _ReferenceType2 __y) { ::new (::std::addressof(__y)) __ValueType(::std::move(__x)); },
-        __first, __result);
+    const auto __n = std::min(__last - __first, __result2 - __result1);
+    return __unseq_backend::__simd_walk_n(
+        __n,
+        [](_ReferenceType1 __x, _ReferenceType2 __y) { ::new (std::addressof(__y)) __ValueType(std::move(__x)); },
+        __first, __result1);
 }
 
 template <typename _Iterator>
@@ -86,29 +90,33 @@ __brick_destroy(_RandomAccessIterator __first, _RandomAccessIterator __last, /*v
 
 template <typename _ForwardIterator, typename _OutputIterator>
 _OutputIterator
-__brick_uninitialized_copy(_ForwardIterator __first, _ForwardIterator __last, _OutputIterator __result,
+__brick_uninitialized_copy(_ForwardIterator __first, _ForwardIterator __last,       // bounds for data1
+                           _OutputIterator __result1, _OutputIterator __result2,    // bounds for results
                            /*vector=*/::std::false_type) noexcept
 {
     using _ValueType = typename ::std::iterator_traits<_OutputIterator>::value_type;
-    for (; __first != __last; ++__first, (void)++__result)
+    for (; __first != __last && __result1 != __result2; ++__first, (void)++__result1)
     {
-        ::new (::std::addressof(*__result)) _ValueType(*__first);
+        ::new (std::addressof(*__result1)) _ValueType(*__first);
     }
-    return __result;
+
+    return __result1;
 }
 
 template <typename _RandomAccessIterator, typename _OutputIterator>
 _OutputIterator
-__brick_uninitialized_copy(_RandomAccessIterator __first, _RandomAccessIterator __last, _OutputIterator __result,
+__brick_uninitialized_copy(_RandomAccessIterator __first, _RandomAccessIterator __last, // bounds for data1
+                           _OutputIterator __result1, _OutputIterator __result2,        // bounds for results
                            /*vector=*/::std::true_type) noexcept
 {
     using __ValueType = typename ::std::iterator_traits<_OutputIterator>::value_type;
     using _ReferenceType1 = typename ::std::iterator_traits<_RandomAccessIterator>::reference;
     using _ReferenceType2 = typename ::std::iterator_traits<_OutputIterator>::reference;
 
-    return __unseq_backend::__simd_walk_n(__last - __first,
-        [](_ReferenceType1 __x, _ReferenceType2 __y) { ::new (::std::addressof(__y)) __ValueType(__x); },
-        __first, __result);
+    const auto __n = std::min(__last - __first, __result2 - __result1);
+    return __unseq_backend::__simd_walk_n(
+        __n, [](_ReferenceType1 __x, _ReferenceType2 __y) { new (std::addressof(__y)) __ValueType(__x); }, __first,
+        __result1);
 }
 
 template <typename _ExecutionPolicy>
