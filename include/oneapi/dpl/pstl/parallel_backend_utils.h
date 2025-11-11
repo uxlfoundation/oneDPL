049a05a37e28d5ed336de5d93045cfa317f65c0d
Sergey Kopienko <sergey.kopienko@intel.com>
2025-11-11 14:59:48 +0100

    include/oneapi/dpl/pstl/parallel_backend_utils.h - __set_difference_construct

--- include/oneapi/dpl/pstl/parallel_backend_utils.h
+++ include/oneapi/dpl/pstl/parallel_backend_utils.h
@@ -253,11 +253,13 @@ __set_union_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1,    
 template <typename _ForwardIterator1, typename _ForwardIterator2, typename _OutputIterator, typename _CopyFunc,
           typename _CopyFromFirstSet, typename _Compare, typename _Proj1, typename _Proj2>
 _OutputIterator
-__set_intersection_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1, _ForwardIterator2 __first2,
-                             _ForwardIterator2 __last2, _OutputIterator __result, _CopyFunc _copy, _CopyFromFirstSet,
+__set_intersection_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1, // bounds for data1
+                             _ForwardIterator2 __first2, _ForwardIterator2 __last2, // bounds for data2
+                             _OutputIterator __result1, _OutputIterator __result2,  // bounds for results
+                             _CopyFunc _copy, _CopyFromFirstSet,
                              _Compare __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
-    while (__first1 != __last1 && __first2 != __last2)
+    while (__first1 != __last1 && __first2 != __last2 && __result1 != __result2)
     {
         if (std::invoke(__comp, std::invoke(__proj1, *__first1), std::invoke(__proj2, *__first2)))
             ++__first1;
@@ -266,36 +268,39 @@ __set_intersection_construct(_ForwardIterator1 __first1, _ForwardIterator1 __las
         else
         {
             if constexpr (_CopyFromFirstSet::value)
-                _copy(*__first1, *__result);
+                _copy(*__first1, *__result1);
             else
-                _copy(*__first2, *__result);
+                _copy(*__first2, *__result1);
 
             ++__first1;
             ++__first2;
-            ++__result;
+            ++__result1;
         }
     }
-    return __result;
+
+    return __result1;
 }
 
 template <typename _ForwardIterator1, typename _ForwardIterator2, typename _OutputIterator,
           typename _CopyConstructRange, typename _Compare, typename _Proj1, typename _Proj2>
 _OutputIterator
-__set_difference_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1, _ForwardIterator2 __first2,
-                           _ForwardIterator2 __last2, _OutputIterator __result, _CopyConstructRange __cc_range,
+__set_difference_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1,   // bounds for data1
+                           _ForwardIterator2 __first2, _ForwardIterator2 __last2,   // bounds for data2
+                           _OutputIterator __result1, _OutputIterator __result2,    // bounds for results
+                           _CopyConstructRange __cc_range,
                            _Compare __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
     using _Tp = typename ::std::iterator_traits<_OutputIterator>::value_type;
 
-    for (; __first1 != __last1;)
+    while (__first1 != __last1 && __result1 != __result2)
     {
         if (__first2 == __last2)
             return __cc_range(__first1, __last1, __result);
 
         if (std::invoke(__comp, std::invoke(__proj1, *__first1), std::invoke(__proj2, *__first2)))
         {
-            ::new (::std::addressof(*__result)) _Tp(*__first1);
-            ++__result;
+            ::new (::std::addressof(*__result1)) _Tp(*__first1);
+            ++__result1;
             ++__first1;
         }
         else
@@ -305,7 +310,8 @@ __set_difference_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1
             ++__first2;
         }
     }
-    return __result;
+
+    return __result1;
 }
 
 template <typename _ForwardIterator1, typename _ForwardIterator2, typename _OutputIterator,
