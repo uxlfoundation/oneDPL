231dd97c416f37b500b5493117617939aeb482b7
Sergey Kopienko <sergey.kopienko@intel.com>
2025-11-12 14:28:11 +0100

    @@@

--- include/oneapi/dpl/pstl/utils_ranges.h
+++ include/oneapi/dpl/pstl/utils_ranges.h
@@ -140,6 +140,8 @@ __empty(_Range&& __rng)
 
 namespace __internal
 {
+template <typename _R>
+using __iterator_t = decltype(oneapi::dpl::__ranges::__begin(std::declval<_R&>()));
 
 template <typename _R>
 auto
@@ -147,8 +149,7 @@ get_value_type(int) -> typename ::std::decay_t<_R>::value_type;
 
 template <typename _R>
 auto
-get_value_type(long) -> typename std::iterator_traits<
-                         std::decay_t<decltype(oneapi::dpl::__ranges::__begin(std::declval<_R&>()))>>::value_type;
+get_value_type(long) -> typename std::iterator_traits<std::decay_t<__iterator_t<_R>>>::value_type;
 
 template <typename _It>
 auto
