75424ce0b0b11243f8e4788e169137e3683f42ad
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-11-14 16:56:31 -0500

    fixing customization tests for coverage
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- test/parallel_api/dynamic_selection/test_customizations.pass.cpp
+++ test/parallel_api/dynamic_selection/test_customizations.pass.cpp
@@ -17,17 +17,27 @@
 int
 test_no_customizations()
 {
+    std::cout << "Starting test_no_customizations...\n";
     int trace = 0;
-    one_with_no_customizations p(trace);
+    no_customizations_policy_base p(trace);
+    p.initialize();
+    EXPECT_EQ((int)(t_init), trace, "ERROR: unexpected trace of initialize function");
 
+    std::cout << "  Testing try_submit...\n";
+    trace = 0;
+    auto opt_sub = oneapi::dpl::experimental::try_submit(p, [](int i) { return i; });
+    EXPECT_TRUE((bool)opt_sub, "ERROR: try_submit should return a value");
+    EXPECT_EQ((int)(t_select), trace, "ERROR: unexpected trace of try_submit function");
+
+    std::cout << "  Testing submit...\n";
     trace = 0;
     oneapi::dpl::experimental::submit(p, [](int i) { return i; });
-    EXPECT_EQ((int)(t_select | t_submit_function), trace, "ERROR: unexpected trace of submit function");
+    EXPECT_EQ((int)(t_select), trace, "ERROR: unexpected trace of submit function");
 
+    std::cout << "  Testing submit_and_wait...\n";
     trace = 0;
     oneapi::dpl::experimental::submit_and_wait(p, [](int i) { return i; });
-    EXPECT_EQ((int)(t_select | t_submit_function | t_wait), trace,
-              "ERROR: unexpected trace of submit_and_wait function");
+    EXPECT_EQ((int)(t_select), trace, "ERROR: unexpected trace of submit_and_wait function");
 
     std::cout << "test_no_customizations: OK\n";
 
@@ -40,15 +50,103 @@ test_all_customizations()
     int trace = 0;
     one_with_all_customizations p(trace);
 
+    std::cout << "  Testing try_submit...\n";
+    trace = 0;
+    auto opt_sub = oneapi::dpl::experimental::try_submit(p, [](int i) { return i; });
+    EXPECT_TRUE((bool)opt_sub, "ERROR: try_submit should return a value");
+    EXPECT_EQ((int)(t_select | t_try_submit_function), trace, "ERROR: unexpected trace of try_submit function");
+
+    std::cout << "  Testing submit...\n";
     trace = 0;
     oneapi::dpl::experimental::submit(p, [](int i) { return i; });
-    EXPECT_EQ((int)t_select | t_submit_function, trace, "ERROR: unexpected trace of submit function");
+    EXPECT_EQ((int)(t_select | t_try_submit_function | t_submit_function), trace,
+              "ERROR: unexpected trace of submit function");
 
+    std::cout << "  Testing submit_and_wait...\n";
     trace = 0;
     oneapi::dpl::experimental::submit_and_wait(p, [](int i) { return i; });
-    EXPECT_EQ((int)t_select | t_submit_and_wait_function, trace, "ERROR: unexpected trace of submit_and_wait function");
+    EXPECT_EQ((int)(t_select | t_try_submit_function | t_submit_function | t_submit_and_wait_function | t_wait), trace,
+              "ERROR: unexpected trace of submit_and_wait function");
 
     std::cout << "test_all_customizations: OK\n";
+    return 0;
+}
+
+int
+test_only_try_submit()
+{
+    std::cout << "Starting test_only_try_submit...\n";
+    int trace = 0;
+    one_with_only_try_submit p(trace);
+
+    std::cout << "  Testing try_submit...\n";
+    // Test try_submit - should use custom try_submit
+    trace = 0;
+    auto opt_sub = oneapi::dpl::experimental::try_submit(p, [](int i) { return i; });
+    EXPECT_TRUE((bool)opt_sub, "ERROR: try_submit should return a value");
+    EXPECT_EQ((int)(t_try_submit_function), trace, "ERROR: try_submit should use custom try_submit");
+
+    std::cout << "  Testing submit (should use generic based on try_submit)...\n";
+    // Test submit - should have generic implementation based on try_submit
+    trace = 0;
+    auto sub = oneapi::dpl::experimental::submit(p, [](int i) { return i; });
+    EXPECT_EQ((int)(t_try_submit_function), trace,
+              "ERROR: submit should use generic submit (which loops on try_submit)");
+
+    std::cout << "  Testing submit_and_wait (should use generic based on try_submit)...\n";
+    // Test submit_and_wait - should use generic implementation based on try_submit
+    trace = 0;
+    oneapi::dpl::experimental::submit_and_wait(p, [](int i) { return i; });
+    EXPECT_EQ((int)(t_try_submit_function | t_wait), trace,
+              "ERROR: submit_and_wait should use generic submit_and_wait (which uses try_submit)");
+
+    std::cout << "test_only_try_submit: OK\n";
+    return 0;
+}
+
+int
+test_only_submit()
+{
+    std::cout << "Starting test_only_submit...\n";
+    int trace = 0;
+    one_with_only_submit p(trace);
+
+    // try_submit should NOT work - only select_impl is implemented, not try_select_impl
+
+    std::cout << "  Testing submit...\n";
+    // Test submit - should use custom submit
+    trace = 0;
+    auto sub = oneapi::dpl::experimental::submit(p, [](int i) { return i; });
+    EXPECT_EQ((int)(t_submit_function), trace, "ERROR: submit should use custom submit");
+
+    std::cout << "  Testing submit_and_wait...\n";
+    // Test submit_and_wait - should use custom submit + wait
+    trace = 0;
+    oneapi::dpl::experimental::submit_and_wait(p, [](int i) { return i; });
+    EXPECT_EQ((int)(t_submit_function | t_wait), trace,
+              "ERROR: submit_and_wait should use custom submit + wait when only submit is customized");
+
+    std::cout << "test_only_submit: OK\n";
+    return 0;
+}
+
+int
+test_only_submit_and_wait()
+{
+    std::cout << "Starting test_only_submit_and_wait...\n";
+    int trace = 0;
+    one_with_only_submit_and_wait p(trace);
+
+    // try_submit should NOT work - no try_select_impl
+    // submit should NOT work - no select_impl or try_select_impl
+
+    std::cout << "  Testing submit_and_wait...\n";
+    // Test submit_and_wait - should use custom submit_and_wait
+    trace = 0;
+    oneapi::dpl::experimental::submit_and_wait(p, [](int i) { return i; });
+    EXPECT_EQ((int)(t_submit_and_wait_function), trace, "ERROR: submit_and_wait should use custom submit_and_wait");
+
+    std::cout << "test_only_submit_and_wait: OK\n";
     return 0;
 }
 
@@ -59,6 +157,9 @@ main()
     {
         test_no_customizations();
         test_all_customizations();
+        test_only_try_submit();
+        test_only_submit();
+        test_only_submit_and_wait();
     }
     catch (const std::exception& exc)
     {
--- test/support/test_dynamic_selection_one_policy.h
+++ test/support/test_dynamic_selection_one_policy.h
@@ -14,84 +14,24 @@
 
 enum tracing_enum
 {
-    t_select = 1,
-    t_submit_function = 1 << 1,
-    t_submit_and_wait_function = 1 << 2,
-    t_wait = 1 << 3
+    t_init = 1 << 0,
+    t_select = 1 << 1,
+    t_try_submit_function = 1 << 2,
+    t_submit_function = 1 << 3,
+    t_submit_and_wait_function = 1 << 4,
+    t_wait = 1 << 5
 };
 
-class one_with_no_customizations
+class no_customizations_policy_base
+    : public oneapi::dpl::experimental::policy_base<no_customizations_policy_base, oneapi::dpl::identity,
+                                                    oneapi::dpl::experimental::default_backend<int>>
 {
+    friend class oneapi::dpl::experimental::policy_base<no_customizations_policy_base, oneapi::dpl::identity,
+                                                        oneapi::dpl::experimental::default_backend<int>>;
     int& trace_;
 
-    class one_selection_t
-    {
-        one_with_no_customizations* p_;
-
-      public:
-        explicit one_selection_t(one_with_no_customizations& p) : p_(&p) {}
-
-        // Make it copyable and movable
-        one_selection_t(const one_selection_t&) = default;
-        one_selection_t(one_selection_t&&) = default;
-        one_selection_t&
-        operator=(const one_selection_t&) = default;
-        one_selection_t&
-        operator=(one_selection_t&&) = default;
-
-        auto
-        unwrap()
-        {
-            return 1;
-        }
-        one_with_no_customizations&
-        get_policy()
-        {
-            return *p_;
-        }
-    };
-
-    class submission
-    {
-        int& trace_;
-
-      public:
-        submission(int& t) : trace_{t} {}
-        void
-        wait()
-        {
-            trace_ = (trace_ | t_wait);
-        }
-        int
-        unwrap()
-        {
-            return 1;
-        }
-    };
-
-    class submission_group
-    {
-      public:
-        void
-        wait()
-        {
-            return;
-        }
-    };
-
-    using selection_type = one_selection_t;
-
-  public:
-    using resource_type = int;
-
-    one_with_no_customizations(int& t) : trace_{t} {}
-
-    auto
-    get_resources() const
-    {
-        return std::vector<int>{1};
-    }
-
+  protected:
+    //required
     template <typename... Args>
     std::shared_ptr<selection_type>
     try_select_impl(Args&&...)
@@ -100,26 +40,17 @@ class one_with_no_customizations
         return std::make_shared<selection_type>(*this);
     }
 
-    // required
-    template <typename Function, typename... Args>
-    auto
-    submit(Function&&, Args&&... args)
+    void
+    initialize_impl()
     {
-        auto e = try_select_impl(args...);
-        while (!e)
-        {
-            e = try_select_impl(args...);
-            std::this_thread::yield();
-        }
-        trace_ = (trace_ | t_submit_function);
-        return submission{trace_};
+        std::cout << "init\n";
+        trace_ = (trace_ | t_init);
     }
 
-    auto
-    get_submission_group()
-    {
-        return submission_group{};
-    }
+  public:
+    using resource_type = int;
+
+    no_customizations_policy_base(int& t) : trace_{t} {}
 };
 
 class one_with_all_customizations
@@ -155,14 +86,20 @@ class one_with_all_customizations
 
     class submission
     {
-        int& trace_;
+        int* trace_;
 
       public:
-        submission(int& t) : trace_{t} {}
+        submission(int& t) : trace_{&t} {}
+        submission(const submission&) = default;
+        submission(submission&&) = default;
+        submission&
+        operator=(const submission&) = default;
+        submission&
+        operator=(submission&&) = default;
         void
         wait()
         {
-            trace_ = (trace_ | t_wait);
+            *trace_ = (*trace_ | t_wait);
         }
         int
         unwrap()
@@ -203,15 +140,29 @@ class one_with_all_customizations
         return std::make_shared<selection_type>(*this);
     }
 
+    // generic try_submit based on try_select_impl
+    template <typename Function, typename... Args>
+    auto
+    try_submit(Function&&, Args&&... args)
+    {
+        auto e = try_select_impl(args...);
+        if (!e)
+        {
+            return std::shared_ptr<submission>{};
+        }
+        trace_ = (trace_ | t_try_submit_function);
+        return std::make_shared<submission>(trace_);
+    }
+
     // required
     template <typename Function, typename... Args>
     auto
-    submit(Function&&, Args&&... args)
+    submit(Function&& f, Args&&... args)
     {
-        auto e = try_select_impl(args...);
+        auto e = try_submit(f, args...);
         while (!e)
         {
-            e = try_select_impl(args...);
+            e = try_submit(f, args...);
             std::this_thread::yield();
         }
         trace_ = (trace_ | t_submit_function);
@@ -221,14 +172,9 @@ class one_with_all_customizations
     // optional
     template <typename Function, typename... Args>
     void
-    submit_and_wait(Function&&, Args&&... args)
+    submit_and_wait(Function&& f, Args&&... args)
     {
-        auto e = try_select_impl(args...);
-        while (!e)
-        {
-            e = try_select_impl(args...);
-            std::this_thread::yield();
-        }
+        submit(f, args...).wait();
         trace_ = (trace_ | t_submit_and_wait_function);
         return;
     }
@@ -237,6 +183,195 @@ class one_with_all_customizations
     get_submission_group()
     {
         return submission_group{};
+    }
+};
+
+class one_with_only_try_submit
+{
+    int& trace_;
+
+    class one_selection_t
+    {
+        one_with_only_try_submit* p_;
+
+      public:
+        explicit one_selection_t(one_with_only_try_submit& p) : p_(&p) {}
+
+        // Make it copyable and movable
+        one_selection_t(const one_selection_t&) = default;
+        one_selection_t(one_selection_t&&) = default;
+        one_selection_t&
+        operator=(const one_selection_t&) = default;
+        one_selection_t&
+        operator=(one_selection_t&&) = default;
+
+        auto
+        unwrap()
+        {
+            return 1;
+        }
+        one_with_only_try_submit&
+        get_policy()
+        {
+            return *p_;
+        }
+    };
+
+    class submission
+    {
+        int* trace_;
+
+      public:
+        submission(int& t) : trace_{&t} {}
+        submission(const submission&) = default;
+        submission(submission&&) = default;
+        submission&
+        operator=(const submission&) = default;
+        submission&
+        operator=(submission&&) = default;
+        void
+        wait()
+        {
+            *trace_ = (*trace_ | t_wait);
+        }
+        int
+        unwrap()
+        {
+            return 1;
+        }
+    };
+
+    using selection_type = one_selection_t;
+
+  public:
+    using resource_type = int;
+
+    one_with_only_try_submit(int& t) : trace_{t} {}
+
+    // Only try_submit is customized
+    template <typename Function, typename... Args>
+    auto
+    try_submit(Function&&, Args&&... args)
+    {
+        // built in selection
+        trace_ = (trace_ | t_try_submit_function);
+        return std::make_shared<submission>(submission{trace_});
+    }
+};
+
+class one_with_only_submit
+{
+    int& trace_;
+
+    class one_selection_t
+    {
+        one_with_only_submit* p_;
+
+      public:
+        explicit one_selection_t(one_with_only_submit& p) : p_(&p) {}
+
+        // Make it copyable and movable
+        one_selection_t(const one_selection_t&) = default;
+        one_selection_t(one_selection_t&&) = default;
+        one_selection_t&
+        operator=(const one_selection_t&) = default;
+        one_selection_t&
+        operator=(one_selection_t&&) = default;
+
+        auto
+        unwrap()
+        {
+            return 1;
+        }
+        one_with_only_submit&
+        get_policy()
+        {
+            return *p_;
+        }
+    };
+
+    class submission
+    {
+        int* trace_;
+
+      public:
+        submission(int& t) : trace_{&t} {}
+        void
+        wait()
+        {
+            *trace_ = (*trace_ | t_wait);
+        }
+        int
+        unwrap()
+        {
+            return 1;
+        }
+    };
+
+    using selection_type = one_selection_t;
+
+  public:
+    using resource_type = int;
+
+    one_with_only_submit(int& t) : trace_{t} {}
+
+    // Only submit is customized
+    template <typename Function, typename... Args>
+    auto
+    submit(Function&&, Args&&... args)
+    {
+        //built in selection
+        trace_ = (trace_ | t_submit_function);
+        return submission{trace_};
+    }
+};
+
+class one_with_only_submit_and_wait
+{
+    int& trace_;
+
+    class one_selection_t
+    {
+        one_with_only_submit_and_wait* p_;
+
+      public:
+        explicit one_selection_t(one_with_only_submit_and_wait& p) : p_(&p) {}
+
+        // Make it copyable and movable
+        one_selection_t(const one_selection_t&) = default;
+        one_selection_t(one_selection_t&&) = default;
+        one_selection_t&
+        operator=(const one_selection_t&) = default;
+        one_selection_t&
+        operator=(one_selection_t&&) = default;
+
+        auto
+        unwrap()
+        {
+            return 1;
+        }
+        one_with_only_submit_and_wait&
+        get_policy()
+        {
+            return *p_;
+        }
+    };
+
+    using selection_type = one_selection_t;
+
+  public:
+    using resource_type = int;
+
+    one_with_only_submit_and_wait(int& t) : trace_{t} {}
+
+    // Only submit_and_wait is customized - no selection methods
+    template <typename Function, typename... Args>
+    void
+    submit_and_wait(Function&&, Args&&...)
+    {
+        //built in selection, etc.
+        trace_ = (trace_ | t_submit_and_wait_function);
+        return;
     }
 };
 
