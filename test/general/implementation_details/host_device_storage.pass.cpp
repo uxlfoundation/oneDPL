02c5ddd17a19e1886aa789b505dcdabedee061db
Alexey Kukanov <alexey.kukanov@intel.com>
2025-11-17 19:58:02 +0100

    Add a test that uses storage in two dependent kernels

--- test/general/implementation_details/host_device_storage.pass.cpp
+++ test/general/implementation_details/host_device_storage.pass.cpp
@@ -64,7 +64,7 @@ struct Test
     void run_single_kernel(int n_scratch, int n_result)
     {
         using ValueType = int;
-        using SingleKernel = KernelName<1, Storage<ValueType>>;
+        using SingleKernel = KernelName<0, Storage<ValueType>>;
 
         Storage<ValueType> result_and_scratch(q, n_scratch, n_result);
 
@@ -99,12 +99,71 @@ struct Test
     }
 
     template <template <typename> typename Storage>
+    void run_two_kernels(int n_scratch, int n_result)
+    {
+        using ValueType = int;
+        using FirstKernel = KernelName<1, Storage<ValueType>>;
+        using SecondKernel = KernelName<2, Storage<ValueType>>;
+
+        Storage<ValueType> result_and_scratch(q, n_scratch, n_result);
+
+        sycl::event ev = q.submit([&](sycl::handler& cgh) {
+            auto scratch_acc =
+                __get_accessor(sycl::write_only, result_and_scratch, cgh, sycl::property::no_init{});
+            auto result_acc =
+                __get_result_accessor(sycl::write_only, result_and_scratch, cgh, sycl::property::no_init{});
+            cgh.parallel_for<FirstKernel>(sycl::range<1>(n_scratch), [=](sycl::item<1> wi){
+                std::size_t idx = wi.get_linear_id();
+                ValueType* scratch = scratch_acc.__data();
+                ValueType* result = result_acc.__data();
+
+                scratch[idx] = ValueType(n_scratch - idx); // n_scratch .. 1
+                if (idx == 0)
+                    result[0] = n_scratch + 1;
+                else if (idx * 2 < n_result)
+                    result[idx * 2] = n_result - idx; // to be rewritten
+            });
+        });
+
+        q.submit([&](sycl::handler& cgh) {
+            cgh.depends_on(ev);
+            auto scratch_acc =
+                __get_accessor(sycl::read_only, result_and_scratch, cgh);
+            auto result_acc =
+                __get_result_accessor(sycl::write_only, result_and_scratch, cgh);
+            cgh.parallel_for<SecondKernel>(sycl::range<1>(n_scratch), [=](sycl::item<1> wi){
+                std::size_t idx = wi.get_linear_id();
+                std::size_t r_idx = n_scratch - idx - 1;
+                const ValueType* scratch = scratch_acc.__data();
+                ValueType* result = result_acc.__data();
+
+                if (r_idx > 0 && r_idx < n_result) // the last n_result items
+                    result[r_idx] = scratch[idx] + n_scratch;
+            });
+        }).wait();
+
+        ValueType single_val;
+        result_and_scratch.__copy_result(&single_val, 1);
+        EXPECT_EQ(1 + n_scratch, single_val, "Incorrect first value copied");
+        
+        std::vector<ValueType> expected{n_result};
+        ValueType i = 0;
+        for (ValueType& v: expected)
+            v = ++i + n_scratch;
+
+        std::vector<ValueType> result_host{n_result};
+        result_and_scratch.__copy_result(result_host.data(), result_host.size());
+        EXPECT_EQ_RANGES(expected, result_host, "Incorrect data copied");
+    }
+
+    template <template <typename> typename Storage>
     void run()
     {
         for (int n_scratch = 1, inc = 3; n_scratch < 2000; n_scratch += inc, inc += 2)
             for (int n_result = 1; n_result <= n_scratch && n_result < 10; ++n_result)
             {
                 run_single_kernel<Storage>(n_scratch, n_result);
+                run_two_kernels<Storage>(n_scratch, n_result);
             }
     }
 
