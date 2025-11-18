e29dc4f6baef338d126cd0c288646352c14aeacb
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-11-18 16:55:51 -0500

    fixing a couple missing changes
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/experimental/kt/internal/esimd_radix_sort_dispatchers.h
+++ include/oneapi/dpl/experimental/kt/internal/esimd_radix_sort_dispatchers.h
@@ -328,12 +328,12 @@ __onesweep(sycl::queue __q, _RngPack1&& __pack, _RngPack2&& __pack_out, ::std::s
     __mem_holder.__allocate();
 
     auto __get_tmp_pack = [&]() {
-        auto __keys_tmp_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write, _KeyT*>();
+        auto __keys_tmp_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
         auto __keys_tmp_rng = __keys_tmp_keep(__mem_holder.__keys_ptr(), __mem_holder.__keys_ptr() + __n).all_view();
 
         if constexpr (__has_values)
         {
-            auto __vals_tmp_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write, _ValT*>();
+            auto __vals_tmp_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
             auto __vals_tmp_rng =
                 __vals_tmp_keep(__mem_holder.__vals_ptr(), __mem_holder.__vals_ptr() + __n).all_view();
             return __rng_pack(::std::move(__keys_tmp_rng), ::std::move(__vals_tmp_rng));
