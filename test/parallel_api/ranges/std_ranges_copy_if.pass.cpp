2eac0e3d2941ceecd1eb6fc348e910444257ddcb
Alexey Kukanov <alexey.kukanov@intel.com>
2025-11-25 19:36:28 +0100

    Move the test checker into a struct to compile with MSVS 2019

--- test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
@@ -15,19 +15,15 @@
 
 #include "std_ranges_test.h"
 
-std::int32_t
-main()
-{
 #if _ENABLE_STD_RANGES_TESTING
-    using namespace test_std_ranges;
-    namespace dpl_ranges = oneapi::dpl::ranges;
-
-    auto copy_if_checker = []<typename Proj = std::identity>(std::ranges::random_access_range auto&& r_in,
-                           std::ranges::random_access_range auto&& r_out, auto pred, Proj proj = {})
+struct
+{
+    template <std::ranges::random_access_range InRange, std::ranges::random_access_range OutRange,
+              typename Pred, typename Proj = std::identity>
+    auto operator()(InRange&& r_in, OutRange&& r_out, Pred pred, Proj proj = {})
     {
         using ret_type = std::ranges::copy_if_result<std::ranges::borrowed_iterator_t<decltype(r_in)>,
-            std::ranges::borrowed_iterator_t<decltype(r_out)>>;
-
+                                                     std::ranges::borrowed_iterator_t<decltype(r_out)>>;
         auto in = std::ranges::begin(r_in);
         auto out = std::ranges::begin(r_out);
         std::size_t i = 0, j = 0;
@@ -42,7 +38,14 @@ main()
              }
         }
         return ret_type{in + i, out + j};
-    };
+    }
+} copy_if_checker;
+
+std::int32_t
+main()
+{
+    using namespace test_std_ranges;
+    namespace dpl_ranges = oneapi::dpl::ranges;
 
     test_range_algo<0, int, data_in_out_lim>{217}(dpl_ranges::copy_if, copy_if_checker, pred);
     test_range_algo<1, int, data_in_out_lim>{1234}(dpl_ranges::copy_if, copy_if_checker, select_many);
@@ -51,7 +54,9 @@ main()
     test_range_algo<4, P2, data_in_out_lim>{}(dpl_ranges::copy_if, copy_if_checker, pred, &P2::proj);
     test_range_algo<5, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if, copy_if_checker, pred);
     test_range_algo<6, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if, copy_if_checker, select_many);
-#endif //_ENABLE_STD_RANGES_TESTING
 
-    return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
+    return TestUtils::done();
 }
+#else
+std::int32_t main() { return TestUtils::done(0); } // skipped
+#endif //_ENABLE_STD_RANGES_TESTING
