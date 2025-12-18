88ed4a9cfb276d7195f282fc2af25786b3b30f2d
Sergey Kopienko <sergey.kopienko@intel.com>
2025-12-18 15:15:16 +0100

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - implements __serial_set_symmetric_difference()

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -1310,6 +1310,19 @@ __serial_set_symmetric_difference(std::ranges::iterator_t<_R1> __it1, std::range
 }
 
 template <typename _R1, typename _R2, typename _OutRange, typename _Comp, typename _Proj1, typename _Proj2>
+std::ranges::set_symmetric_difference_result<std::ranges::borrowed_iterator_t<_R1>,
+                                             std::ranges::borrowed_iterator_t<_R2>,
+                                             std::ranges::borrowed_iterator_t<_OutRange>>
+__serial_set_symmetric_difference(_R1&& __r1, _R2&& __r2, _OutRange&& __out_r, _Comp __comp, _Proj1 __proj1,
+                                  _Proj2 __proj2)
+{
+    return __serial_set_symmetric_difference<_R1, _R2, _OutRange>(
+        std::ranges::begin(__r1), std::ranges::begin(__r1) + std::ranges::size(__r1), std::ranges::begin(__r2),
+        std::ranges::begin(__r2) + std::ranges::size(__r2), std::ranges::begin(__out_r),
+        std::ranges::begin(__out_r) + std::ranges::size(__out_r), __comp, __proj1, __proj2);
+}
+
+template <typename _R1, typename _R2, typename _OutRange, typename _Comp, typename _Proj1, typename _Proj2>
 __set_symmetric_difference_return_t<_R1, _R2, _OutRange>
 __brick_set_symmetric_difference(_R1&& __r1, _R2&& __r2, _OutRange&& __out_r, _Comp __comp, _Proj1 __proj1,
                                  _Proj2 __proj2,
