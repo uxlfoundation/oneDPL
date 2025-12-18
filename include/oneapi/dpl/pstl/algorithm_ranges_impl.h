d32130d1d50bca2c35da6f2a5cdd3e597531d218
Sergey Kopienko <sergey.kopienko@intel.com>
2025-12-18 15:02:53 +0100

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - re-design __pattern_set_union + __parallel_tag

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -794,44 +794,51 @@ __pattern_set_union(_Tag __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r
 {
     static_assert(__is_serial_tag_v<_Tag>);
 
-    return __brick_set_union(std::forward<_R1>(__r1), std::forward<_R2>(__r2), std::forward<_OutRange>(__out_r), __comp,
-                             __proj1, __proj2, typename _Tag::__is_vector{});
+    return __brick_set_union(std::forward<_R1>(__r1), std::forward<_R2>(__r2), std::forward<_OutRange>(__out_r),
+                             __comp, __proj1, __proj2,
+                             typename _Tag::__is_vector{});
 }
 
 template <class _IsVector, typename _ExecutionPolicy, typename _R1, typename _R2, typename _OutRange, typename _Comp,
           typename _Proj1, typename _Proj2>
 __set_union_return_t<_R1, _R2, _OutRange>
-__pattern_set_union(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2,
-                    _OutRange&& __out_r, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2)
+__pattern_set_union(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec,
+                    _R1&& __r1, _R2&& __r2, _OutRange&& __out_r,
+                    _Comp __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
     using _RandomAccessIterator1 = std::ranges::iterator_t<_R1>;
     using _RandomAccessIterator2 = std::ranges::iterator_t<_R2>;
     using _Tp = std::ranges::range_value_t<_OutRange>;
 
-    const auto __n1 = std::ranges::size(__r1);
-    const auto __n2 = std::ranges::size(__r2);
+    auto [__first1, __last1, __n1] = oneapi::dpl::__ranges::__get_range_bounds_n(__r1);
+    auto [__first2, __last2, __n2] = oneapi::dpl::__ranges::__get_range_bounds_n(__r2);
+    auto [__result1, __result2] = oneapi::dpl::__ranges::__get_range_bounds(__out_r);
 
     // use serial algorithm
     if (!oneapi::dpl::__internal::__is_great_that_set_algo_cut_off(__n1 + __n2))
-        return std::ranges::set_union(__r1, __r2, std::begin(__out_r), __comp, __proj1, __proj2);
+        return __serial_set_union(std::forward<_R1>(__r1), std::forward<_R2>(__r2), std::forward<_OutRange>(__out_r),
+                                  __comp, __proj1, __proj2);
 
-    auto __first1 = std::ranges::begin(__r1);
-    auto __last1 = __first1 + __n1;
-    auto __first2 = std::ranges::begin(__r2);
-    auto __last2 = __first2 + __n2;
-    auto __result = std::ranges::begin(__out_r);
-
-    auto __out_last = oneapi::dpl::__internal::__parallel_set_union_op(
-        __tag, std::forward<_ExecutionPolicy>(__exec), __first1, __last1, __first2, __last2, __result,
-        [](_RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2,
-           _RandomAccessIterator2 __last2, _Tp* __result, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2) {
-            return oneapi::dpl::__utils::__set_union_construct(
-                __first1, __last1, __first2, __last2, __result,
-                oneapi::dpl::__internal::__BrickCopyConstruct<_IsVector>(), __comp, __proj1, __proj2);
+    return oneapi::dpl::__internal::__parallel_set_union_op(
+        __tag, std::forward<_ExecutionPolicy>(__exec),
+        __first1, __last1,                                                  // bounds for data1
+        __first2, __last2,                                                  // bounds for data2
+        __result1, __result2,                                               // bounds for results
+        [](_RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1, // _SetUnionOp __set_union_op
+           _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2,
+            _Tp* __result1, _Tp* __result2,
+            auto __mask1, auto __mask2,
+           _Comp __comp, _Proj1 __proj1, _Proj2 __proj2) {
+            return oneapi::dpl::__utils::__set_union_bounded_construct(
+                __first1, __last1,                                          // bounds for data1
+                __first2, __last2,                                          // bounds for data2
+                __result1, __result2,                                       // bounds for results
+                __mask1, __mask2,                                           // itrator usage masks
+                oneapi::dpl::__internal::__BrickCopyConstruct<_IsVector>(), // _CopyConstructRange __cc_range
+                __comp, __proj1, __proj2);
         },
-        __comp, __proj1, __proj2);
-
-    return {__first1 + __n1, __first2 + __n2, __result + (__out_last - __result)};
+        __comp, __proj1, __proj2)
+        .template __get_reached_in1_in2_out<__set_union_return_t<_R1, _R2, _OutRange>>();
 }
 
 //---------------------------------------------------------------------------------------------------------------------
