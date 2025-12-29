9f84ff1f765c1cbe641654a93f0e236e4f7bc12f
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-12-29 16:48:58 -0500

    move lazy storage out of loop
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -606,26 +606,28 @@ __radix_sort_reorder_submit(sycl::queue& __q, std::size_t __segments, std::size_
                 _OffsetT __wi_exclusive_prefix[__radix_states];
                 if (__sg_size == 32)
                 {
+                    oneapi::dpl::__internal::__lazy_ctor_storage<_OffsetT> __carry;
                     for (::std::uint32_t __radix_state_idx = 0; __radix_state_idx < __radix_states; ++__radix_state_idx)
                     {
                         __wi_exclusive_prefix[__radix_state_idx] = __local_counts_arr[__radix_state_idx];
-                        oneapi::dpl::__internal::__lazy_ctor_storage<_OffsetT> __carry;
                         __carry.__setup(_OffsetT{0});
                         __sub_group_scan<32, /*__is_inclusive=*/false, /*__init_present=*/true>(
                             __sub_group, __wi_exclusive_prefix[__radix_state_idx], __dpl_sycl::__plus<_OffsetT>(), __carry);
                         __subgroup_totals[__radix_state_idx] = __carry.__v;
+                        __carry.__destroy();
                     }
                 }
                 else if (__sg_size == 16)
                 {
+                    oneapi::dpl::__internal::__lazy_ctor_storage<_OffsetT> __carry;
                     for (::std::uint32_t __radix_state_idx = 0; __radix_state_idx < __radix_states; ++__radix_state_idx)
                     {
                         __wi_exclusive_prefix[__radix_state_idx] = __local_counts_arr[__radix_state_idx];
-                        oneapi::dpl::__internal::__lazy_ctor_storage<_OffsetT> __carry;
                         __carry.__setup(_OffsetT{0});
                         __sub_group_scan<16, /*__is_inclusive=*/false, /*__init_present=*/true>(
                             __sub_group, __wi_exclusive_prefix[__radix_state_idx], __dpl_sycl::__plus<_OffsetT>(), __carry);
                         __subgroup_totals[__radix_state_idx] = __carry.__v;
+                        __carry.__destroy();
                     }
                 }
                 else
