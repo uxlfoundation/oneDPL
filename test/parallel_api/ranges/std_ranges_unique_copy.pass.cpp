1706aab99f8774426862381473b13457046795e2
Alexey Kukanov <alexey.kukanov@intel.com>
2026-01-08 18:20:20 +0100

    Add a serial checker with the proper bounded output semantics

--- test/parallel_api/ranges/std_ranges_unique_copy.pass.cpp
+++ test/parallel_api/ranges/std_ranges_unique_copy.pass.cpp
@@ -15,26 +15,98 @@
 
 #include "std_ranges_test.h"
 
+#if _ENABLE_STD_RANGES_TESTING
+#include <span>
+#include <initializer_list>
+
+struct
+{
+    template <std::ranges::random_access_range InRange, std::ranges::random_access_range OutRange,
+              typename Comp, typename Proj = std::identity>
+    auto operator()(InRange&& r_in, OutRange&& r_out, Comp comp, Proj proj = {})
+    {
+        using ret_type = std::ranges::unique_copy_result<std::ranges::borrowed_iterator_t<InRange>,
+                                                         std::ranges::borrowed_iterator_t<OutRange>>;
+        auto in = std::ranges::begin(r_in);
+        auto out = std::ranges::begin(r_out);
+        std::size_t i = 0, j = 0;
+        for(; i < std::ranges::size(r_in); ++i)
+        {
+             if (i == 0 || !bool(std::invoke(comp, std::invoke(proj, in[i - 1]), std::invoke(proj, in[i]))))
+             {
+                 if (j < std::ranges::size(r_out))
+                     out[j++] = in[i];
+                 else
+                     break;
+             }
+        }
+        return ret_type{in + i, out + j};
+    }
+    
+    void test_self()
+    {
+        int input[10] = {0,0, 1, 2,2, 8, 1,1,1, 8};
+        int output[9] = {-9, -8, -7, -6, -5, -4, -3, -2, -1};
+
+        // Define test cases with expected outputs and expected end positions
+        struct TestCase {
+            std::size_t in_size;
+            std::size_t out_size;
+            std::initializer_list<int> expected_output;
+            std::size_t expected_in_end;
+            std::size_t expected_out_end;
+        } test_cases[] = {
+        // insz, outsz,  expected,  instop, outstop
+            {0,  0, {},                 0,  0}, // Empty ranges
+            {10, 0, {},                 0,  0}, // Empty output range
+            {1,  1, {0},                1,  1}, // One element ranges
+            {10, 1, {0},                2,  1}, // One element output range
+            {10, 5, {0, 1, 2, 8, 1},    9,  5}, // Output range is not big enough
+            {10, 6, {0, 1, 2, 8, 1, 8}, 10, 6}, // Output range is just enough
+            {10, 7, {0, 1, 2, 8, 1, 8}, 10, 6}, // Output range is bigger
+        };
+
+        auto& self = *this;
+        for (const TestCase& test_case : test_cases) {
+            constexpr int shift = 1;
+            std::span<int> in_span(input, test_case.in_size);
+            std::span<int> out_span(output + shift, test_case.out_size);
+
+            auto result = self(in_span, out_span, std::equal_to<int>{});
+
+            // Verify the returned iterators point to the correct end positions
+            EXPECT_EQ(in_span.begin() + test_case.expected_in_end, result.in, "Checker problem: wrong input stop");
+            EXPECT_EQ(out_span.begin() + test_case.expected_out_end, result.out, "Checker problem: wrong output stop");
+
+            // Verify the output matches the expected result and nothing is overwritten
+            for (int i = 0; i < 9; ++i)
+            {
+                if (i < shift || i >= shift + test_case.expected_out_end)
+                {
+                    EXPECT_EQ(i - 9, output[i], "Checker problem: out of range modification");
+                }
+                else
+                {
+                    EXPECT_EQ(test_case.expected_output.begin()[i - shift], output[i], "Checker problem: wrong output");
+                    output[i] = i - 9; // Restore the original output data
+                }
+            }
+        }
+    }
+} unique_copy_checker;
+#endif // _ENABLE_STD_RANGES_TESTING
+
 int
 main()
 {
 #if _ENABLE_STD_RANGES_TESTING
     using namespace test_std_ranges;
     namespace dpl_ranges = oneapi::dpl::ranges;
-
-    auto unique_copy_checker = [](std::ranges::random_access_range auto&& r_in,
-                           std::ranges::random_access_range auto&& r_out, auto&&... args)
-    {
-        auto res = std::ranges::unique_copy(std::forward<decltype(r_in)>(r_in), std::ranges::begin(r_out),
-            std::forward<decltype(args)>(args)...);
-
-        using ret_type = std::ranges::unique_copy_result<std::ranges::borrowed_iterator_t<decltype(r_in)>,
-            std::ranges::borrowed_iterator_t<decltype(r_out)>>;
-        return ret_type{res.in, res.out};
-    };
+    
+    unique_copy_checker.test_self();
 
     test_range_algo<0, int, data_in_out>{big_sz}(dpl_ranges::unique_copy,  unique_copy_checker, std::ranges::equal_to{});
-    test_range_algo<1, int, data_in_out>{}(dpl_ranges::unique_copy,  unique_copy_checker, std::not_equal_to{}, proj);
+    test_range_algo<1, int, data_in_out>{}(dpl_ranges::unique_copy,  unique_copy_checker, std::ranges::not_equal_to{}, proj);
     test_range_algo<2, int, data_in_out>{}(dpl_ranges::unique_copy,  unique_copy_checker, std::ranges::equal_to{}, proj);
     test_range_algo<3, P2, data_in_out>{}(dpl_ranges::unique_copy,  unique_copy_checker, std::ranges::equal_to{}, &P2::x);
     test_range_algo<4, P2, data_in_out>{}(dpl_ranges::unique_copy,  unique_copy_checker, std::ranges::equal_to{}, &P2::proj);
