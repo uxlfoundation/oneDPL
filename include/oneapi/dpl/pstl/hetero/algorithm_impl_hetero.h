f86c53fd653c3ae8f077f364b13d35d5c9b28715
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-01-08 12:03:18 -0500

    access mode changes for transform_if

--- include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
@@ -320,10 +320,11 @@ _ForwardIterator
 __pattern_fill(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _ForwardIterator __first,
                _ForwardIterator __last, const _T& __value)
 {
-    __pattern_walk1(__tag, ::std::forward<_ExecutionPolicy>(__exec),
-                    __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__first),
-                    __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__last),
-                    fill_functor<_T>{__value});
+    __pattern_walk1<__par_backend_hetero::access_mode::write, /*_NoInit=*/true>(
+        __tag, ::std::forward<_ExecutionPolicy>(__exec),
+        __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__first),
+        __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__last),
+        fill_functor<_T>{__value});
     return __last;
 }
 
@@ -357,10 +358,11 @@ _ForwardIterator
 __pattern_generate(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _ForwardIterator __first,
                    _ForwardIterator __last, _Generator __g)
 {
-    __pattern_walk1(__tag, ::std::forward<_ExecutionPolicy>(__exec),
-                    __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__first),
-                    __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__last),
-                    generate_functor<_Generator>{__g});
+    __pattern_walk1<__par_backend_hetero::access_mode::write, /*_NoInit=*/true>(
+        __tag, ::std::forward<_ExecutionPolicy>(__exec),
+        __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__first),
+        __par_backend_hetero::make_iter_mode<__par_backend_hetero::access_mode::write>(__last),
+        generate_functor<_Generator>{__g});
     return __last;
 }
 
