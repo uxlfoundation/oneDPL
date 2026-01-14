0d65b10a556d91956a7116024bea220b003ba98d
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-01-14 11:51:10 -0500

    Renaming NoInit
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
@@ -406,7 +406,7 @@ struct __range_holder
     }
 };
 
-template <sycl::access::mode AccMode, bool _NoInit = false>
+template <sycl::access::mode AccMode, bool _IsNoInitRequested = false>
 struct __get_sycl_range
 {
     __get_sycl_range()
@@ -728,7 +728,7 @@ struct __get_sycl_range
     operator()(_ArgTypes... __args)
     {
         //when called using operator(), use access mode and no_init flag provided by the struct template parameters
-        return __process_input_iter<AccMode, _NoInit>(::std::forward<_ArgTypes>(__args)...);
+        return __process_input_iter<AccMode, _IsNoInitRequested>(::std::forward<_ArgTypes>(__args)...);
     }
 };
 
