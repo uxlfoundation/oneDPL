ec4b1c2d09b1f3ccbf5ecae2b6c5c2ed3d378d6a
Sergey Kopienko <sergey.kopienko@intel.com>
2026-01-22 14:12:28 +0100

    include/oneapi/dpl/pstl/algorithm_impl.h - restore cut_off (1024) in __is_great_that_set_algo_cut_off()

--- include/oneapi/dpl/pstl/algorithm_impl.h
+++ include/oneapi/dpl/pstl/algorithm_impl.h
@@ -3300,8 +3300,7 @@ constexpr bool
 __is_great_that_set_algo_cut_off(Size size)
 {
     // 1024 is chosen as a cut-off value based on benchmarking source data sizes
-    //constexpr auto __set_algo_cut_off = 1024;
-    constexpr auto __set_algo_cut_off = 0;
+    constexpr auto __set_algo_cut_off = 1024;
     return size > __set_algo_cut_off;
 }
 
