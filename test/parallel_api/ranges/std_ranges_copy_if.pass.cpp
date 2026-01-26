e5b1c741836363518f2ff8d20612f2a16101256d
Alexey Kukanov <alexey.kukanov@intel.com>
2026-01-26 16:16:16 +0100

    [debug] Add a pseudo-random data input to std_ranges_copy_if.pass

--- test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
@@ -49,13 +49,26 @@ main()
     using namespace test_std_ranges;
     namespace dpl_ranges = oneapi::dpl::ranges;
 
+    // input generator with a fair chance of repeating the previous value
+    auto repeat_sometimes = [](auto i) {
+        static decltype(i) last = 0;
+        if (i == 0)
+            last = 0; // reset
+        else if (i%7 > 0 && (last + i - 1)%3 == 0)
+            last = i;
+        return last;
+    };
+    using repeating_gen = decltype(repeat_sometimes);
+    auto modulo_3_is_1 = [](int val) { return (val % 3) == 1; };
+
     test_range_algo<0, int, data_in_out_lim>{217}(dpl_ranges::copy_if, copy_if_checker, pred);
     test_range_algo<1, int, data_in_out_lim>{1234}(dpl_ranges::copy_if, copy_if_checker, select_many);
     test_range_algo<2, int, data_in_out_lim>{}(dpl_ranges::copy_if, copy_if_checker, select_many, proj);
     test_range_algo<3, P2, data_in_out_lim>{}(dpl_ranges::copy_if, copy_if_checker, pred, &P2::x);
     test_range_algo<4, P2, data_in_out_lim>{}(dpl_ranges::copy_if, copy_if_checker, pred, &P2::proj);
     test_range_algo<5, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if, copy_if_checker, pred);
-    test_range_algo<6, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if, copy_if_checker, select_many);
+//    test_range_algo<6, int, data_in_out_lim>{big_sz}(dpl_ranges::copy_if, copy_if_checker, select_many);
+    test_range_algo<6, int, data_in_out_lim, repeating_gen>{big_sz}(dpl_ranges::copy_if, copy_if_checker, modulo_3_is_1);
 #endif // _ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
