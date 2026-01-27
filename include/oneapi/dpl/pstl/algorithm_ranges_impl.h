052850222737242cdbcbc714920eee828ae12f20
Sergey Kopienko <sergey.kopienko@intel.com>
2026-01-27 10:43:38 +0100

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - revert extra changes in __pattern_merge_ranges

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -639,20 +639,25 @@ std::ranges::merge_result<std::ranges::borrowed_iterator_t<_R1>, std::ranges::bo
 __pattern_merge_ranges(_Tag __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2, _OutRange&& __out_r, _Comp __comp,
                        _Proj1 __proj1, _Proj2 __proj2)
 {
-    [[maybe_unused]] auto [__first1, __last1, __n1] = oneapi::dpl::__ranges::__get_range_bounds_n(__r1);
-    [[maybe_unused]] auto [__first2, __last2, __n2] = oneapi::dpl::__ranges::__get_range_bounds_n(__r2);
-    [[maybe_unused]] auto [__result1, __result2, __n_out] = oneapi::dpl::__ranges::__get_range_bounds_n(__out_r);
+    using _Index1 = std::ranges::range_difference_t<_R1>;
+    using _Index2 = std::ranges::range_difference_t<_R2>;
+    using _Index3 = std::ranges::range_difference_t<_OutRange>;
+
+    const _Index1 __n_1 = std::ranges::size(__r1);
+    const _Index2 __n_2 = std::ranges::size(__r2);
+    const _Index3 __n_out = std::min<_Index3>(__n_1 + __n_2, std::ranges::size(__out_r));
+
+    auto __it_1 = std::ranges::begin(__r1);
+    auto __it_2 = std::ranges::begin(__r2);
+    auto __it_out = std::ranges::begin(__out_r);
 
     if (__n_out == 0)
-        return {__first1, __first2, __result1};
+        return {__it_1, __it_2, __it_out};
 
-    auto [__res1, __res2] = ___merge_path_out_lim(__tag, std::forward<_ExecutionPolicy>(__exec),
-                                                  __first1, __n1,
-                                                  __first2, __n2,
-                                                  __result1, __n_out,
-                                                  __comp, __proj1, __proj2);
+    auto [__res1, __res2] = ___merge_path_out_lim(__tag, std::forward<_ExecutionPolicy>(__exec), __it_1, __n_1, __it_2,
+                                                  __n_2, __it_out, __n_out, __comp, __proj1, __proj2);
 
-    return {__res1, __res2, __result1 + __n_out};
+    return {__res1, __res2, __it_out + __n_out};
 }
 
 //---------------------------------------------------------------------------------------------------------------------
