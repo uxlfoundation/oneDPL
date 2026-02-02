2da16f3d16bead94cea46aa822d7228dad3ecf17
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-02-02 15:27:49 -0500

    fixing destruction
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
@@ -305,7 +305,18 @@ struct __subgroup_radix_sort
                                     {
                                         const uint16_t __r = __indices[__i];
                                         if (__r < __n)
+                                        {
                                             __src[__r] = ::std::move(__values.__v[__i]);
+                                            __values.__v[__i].~_ValT();
+                                        }
+                                    }
+                                    //destroy values in exchange buffer
+                                    _ONEDPL_PRAGMA_UNROLL
+                                    for (uint16_t __i = 0; __i < __block_size; ++__i)
+                                    {
+                                        const uint16_t __idx = __wi * __block_size + __i;
+                                        if (__idx < __n)
+                                            __exchange_lacc[__idx].~_ValT();
                                     }
                                     return;
                                 }
