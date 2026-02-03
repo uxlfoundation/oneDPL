eee3d7b3c2fdc37a9ecff2276262de818e49919b
Matthew Michel <matthew.michel@intel.com>
2026-02-02 17:24:52 -0800

    update checks on params

--- include/oneapi/dpl/experimental/kt/internal/radix_sort_utils.h
+++ include/oneapi/dpl/experimental/kt/internal/radix_sort_utils.h
@@ -25,17 +25,15 @@ namespace oneapi::dpl::experimental::kt::gpu::__impl
 struct __esimd_tag{};
 struct __sycl_tag{};
 
-
 //-----------------------------------------------------------------------------
 // Parameter validation
 //-----------------------------------------------------------------------------
 template <std::uint8_t __radix_bits, std::uint16_t __data_per_workitem, std::uint16_t __workgroup_size>
 constexpr void
-__check_onesweep_params()
+__check_sycl_sort_params()
 {
     static_assert(__radix_bits == 8);
-    //static_assert(__data_per_workitem % 32 == 0);
-    //static_assert(__workgroup_size == 32 || __workgroup_size == 64);
+    static_assert(__workgroup_size == 1024);
 }
 
 //-----------------------------------------------------------------------------
