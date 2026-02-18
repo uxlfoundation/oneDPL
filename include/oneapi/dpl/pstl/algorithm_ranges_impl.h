e34b9d77f41c69975c1deff4dc3fcf2dbfe77496
Sergey Kopienko <sergey.kopienko@intel.com>
2026-02-18 17:54:55 +0100

    include/oneapi/dpl/pstl/algorithm_ranges_impl.h - fix review comment

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -1168,7 +1168,7 @@ __serial_set_difference(_R1&& __r1, _R2&& __r2, _OutRange&& __out_r, _Comp __com
         return {__it1, __it2, __out_it};
     };
 
-    // 1. Main set_union operation
+    // 1. Main set_difference operation
     bool __output_full = false;
     while (__it1 != __end1 && __it2 != __end2 && !__output_full)
     {
