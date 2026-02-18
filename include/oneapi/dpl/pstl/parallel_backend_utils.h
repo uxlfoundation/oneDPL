c01b5ee9de32f900411155083e744fc3423eaf9e
Sergey Kopienko <sergey.kopienko@intel.com>
2026-02-18 16:01:53 +0100

    Fix spelling check errors

--- include/oneapi/dpl/pstl/parallel_backend_utils.h
+++ include/oneapi/dpl/pstl/parallel_backend_utils.h
@@ -504,7 +504,7 @@ __set_symmetric_difference_construct(_ForwardIterator1 __first1, _ForwardIterato
     // __proj1_val < __proj2_val
     auto __op_val1_lt_val2 = [](_ForwardIterator1 __first1, _ForwardIterator2 __first2, _OutputIterator __result,
                                 _MaskIterator __mask) -> _OperationRes {
-        // We should use placement new here because this method really works with raw unitialized memory
+        // We should use placement new here because this method really works with raw uninitialized memory
         _UninitializedCopyItem<_ForwardIterator1, _OutputIterator>{}(__first1++, __result++);
         __mask = __set_iterator_mask(__mask, __parallel_set_op_mask::eData1Out);
 
@@ -514,7 +514,7 @@ __set_symmetric_difference_construct(_ForwardIterator1 __first1, _ForwardIterato
     // __proj2_val < __proj1_val
     auto __op_val2_lt_val1 = [](_ForwardIterator1 __first1, _ForwardIterator2 __first2, _OutputIterator __result,
                                 _MaskIterator __mask) -> _OperationRes {
-        // We should use placement new here because this method really works with raw unitialized memory
+        // We should use placement new here because this method really works with raw uninitialized memory
         _UninitializedCopyItem<_ForwardIterator2, _OutputIterator>{}(__first2++, __result++);
         __mask = __set_iterator_mask(__mask, __parallel_set_op_mask::eData2Out);
 
