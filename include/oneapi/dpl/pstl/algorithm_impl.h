672de8e6a0c7a20ad4c969ab238d19027ce396d1
Sergey Kopienko <sergey.kopienko@intel.com>
2026-02-23 14:17:14 +0100

    include/oneapi/dpl/pstl/algorithm_impl.h - fix compile error: : lambda capture '__result1' is not used

--- include/oneapi/dpl/pstl/algorithm_impl.h
+++ include/oneapi/dpl/pstl/algorithm_impl.h
@@ -4324,7 +4324,7 @@ __parallel_set_op(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec,
                           __buf_raw_data_begin,
                           __mask_bufs};
 
-        auto __apex_pred = [__n_out, __result1, __result2, &__scan_pred](const _SetRange& __total) {
+        auto __apex_pred = [&__scan_pred](const _SetRange& __total) {
             //final scan
             __scan_pred(/* 0 */ _DifferenceType1{}, /* 0 */ _DifferenceType1{}, __total);
 
