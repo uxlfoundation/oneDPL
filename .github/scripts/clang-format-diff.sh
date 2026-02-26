eb5c375c98d1821314b41ab66e137c7af3b49481
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-02-26 12:22:47 -0500

    minor assorted improvements from review
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- .github/scripts/clang-format-diff.sh
+++ .github/scripts/clang-format-diff.sh
@@ -13,4 +13,4 @@ BASE_SHA="$2"
 MERGE_BASE=$(git merge-base "$HEAD_SHA" "$BASE_SHA")
 FILES=$(git diff --diff-filter=d --name-only "$MERGE_BASE" | grep ^include | grep -v 'nanorange\.hpp$' || true)
 CLANG_FORMAT_DIFF_PATH=$(which clang-format-diff)
-echo $FILES | xargs -n1 -t -r git diff -U0 --no-color --relative "$MERGE_BASE" | python3 "$CLANG_FORMAT_DIFF_PATH" -i -p1 -style file
+echo "$FILES" | xargs -n1 -t -r git diff -U0 --no-color --relative "$MERGE_BASE" | python3 "$CLANG_FORMAT_DIFF_PATH" -i -p1 -style file
