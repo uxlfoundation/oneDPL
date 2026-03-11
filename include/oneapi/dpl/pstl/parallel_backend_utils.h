152ced56ee5f1727a850edbb805cf0891fa723db
Sergey Kopienko <sergey.kopienko@intel.com>
2026-03-11 17:12:51 +0100

    include/oneapi/dpl/pstl/parallel_backend_utils.h - rewrite __set_union_construct() without if...else statement inside loop

--- include/oneapi/dpl/pstl/parallel_backend_utils.h
+++ include/oneapi/dpl/pstl/parallel_backend_utils.h
@@ -226,23 +226,42 @@ __set_union_construct(_ForwardIterator1 __first1, _ForwardIterator1 __last1, _Fo
 {
     using _Tp = typename ::std::iterator_traits<_OutputIterator>::value_type;
 
-    for (; __first1 != __last1; ++__result)
+    using _OperationRes = std::tuple<_ForwardIterator1, _ForwardIterator2, _OutputIterator>;
+
+    // __proj2_val < __proj1_val
+    auto __op_val2_lt_val1 = [](_ForwardIterator1 __it1, _ForwardIterator2 __it2, _OutputIterator __out_it) -> _OperationRes {
+        new (std::addressof(*__out_it)) _Tp(*__it2);
+        return {__it1, ++__it2, ++__out_it};
+    };
+
+    // __proj1_val < __proj2_val
+    auto __op_val1_lt_val2 = [](_ForwardIterator1 __it1, _ForwardIterator2 __it2, _OutputIterator __out_it) -> _OperationRes {
+        new (std::addressof(*__out_it)) _Tp(*__it1);
+        return {++__it1, __it2, ++__out_it};
+    };
+
+    // __proj1_val == __proj2_val
+    auto __op_val1_eq_val2 = [](_ForwardIterator1 __it1, _ForwardIterator2 __it2, _OutputIterator __out_it) -> _OperationRes {
+        new (std::addressof(*__out_it)) _Tp(*__it1);
+        return {++__it1, ++__it2, ++__out_it};
+    };
+
+    // 1. Main set_union operation
+    while (__first1 != __last1 && __first2 != __last2)
     {
-        if (__first2 == __last2)
-            return __cc_range(__first1, __last1, __result);
-        if (std::invoke(__comp, std::invoke(__proj2, *__first2), std::invoke(__proj1, *__first1)))
-        {
-            ::new (::std::addressof(*__result)) _Tp(*__first2);
-            ++__first2;
-        }
-        else
-        {
-            ::new (::std::addressof(*__result)) _Tp(*__first1);
-            if (!std::invoke(__comp, std::invoke(__proj1, *__first1), std::invoke(__proj2, *__first2)))
-                ++__first2;
-            ++__first1;
-        }
+        const bool __val1_lt_val2 = std::invoke(__comp, std::invoke(__proj1, *__first1), std::invoke(__proj2, *__first2));
+        const bool __val2_lt_val1 = !__val1_lt_val2 && std::invoke(__comp, std::invoke(__proj2, *__first2), std::invoke(__proj1, *__first1));
+
+        std::tie(__first1, __first2, __result) = __val2_lt_val1
+                                               ? __op_val2_lt_val1(__first1, __first2, __result)
+                                               : (__val1_lt_val2 ? __op_val1_lt_val2(__first1, __first2, __result)
+                                                                 : __op_val1_eq_val2(__first1, __first2, __result));
     }
+
+    // 2. Copying the residual elements if one of the input sequences is exhausted
+    if (__first1 != __last1)
+        return __cc_range(__first1, __last1, __result);
+
     return __cc_range(__first2, __last2, __result);
 }
 
