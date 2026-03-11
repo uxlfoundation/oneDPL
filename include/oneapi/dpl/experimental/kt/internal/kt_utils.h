684086242dbc105ad4a20985ddfdc8c30bf51e28
Matthew Michel <matthew.michel@intel.com>
2026-03-11 09:04:50 -0500

    Implement a SYCL Onesweep Radix Sort KT (#2575)
    * Adds `oneapi::dpl::experimental::kt::gpu::radix_sort`, a SYCL radix sort implementation based on oneDPL's ESIMD radix sort.
    * Utilizes L0 cooperative kernels via `sycl_ext_oneapi_forward_progress` to guarantee safe inter-workgroup synchronization when performing decoupled lookback.
    * Supports work-group sizes of 512 and 1024 and 8 radix bits.
    
    ---------
    
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    Co-authored-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

---
+++ include/oneapi/dpl/experimental/kt/internal/kt_utils.h
@@ -0,0 +1,76 @@
+// -*- C++ -*-
+//===------------------------------------------------------===//
+//
+// Copyright (C) UXL Foundation Contributors
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===------------------------------------------------------===//
+
+#ifndef _ONEDPL_KT_UTILS_H
+#define _ONEDPL_KT_UTILS_H
+
+#include "../../../pstl/hetero/dpcpp/sycl_defs.h"
+#include "../../../pstl/utils.h"
+#include "kt_defs.h"
+
+#include <cstdint>
+#include <algorithm>
+#include <iterator>
+
+namespace oneapi::dpl::experimental::kt::gpu::__impl
+{
+
+// The number of groups that should be launched in a cooperative kernel.
+// Returns the min of the max groups supported by the HW and the tile count
+template <typename _Kernel>
+std::uint32_t
+__get_num_cooperative_groups(const _Kernel& __kernel, sycl::queue& __q, std::uint32_t __work_group_size,
+                             std::uint32_t __tile_count, std::uint32_t __slm_size_bytes)
+{
+    // Exceptions for excessive SLM usage will be thrown by SYCL.
+    __slm_size_bytes = std::min<std::uint32_t>(__slm_size_bytes, 1 << 17);
+
+    std::uint32_t __max_num_cooperative_groups = 1;
+#if _ONEDPL_KT_COOPERATIVE_KERNELS_PRESENT
+    std::uint32_t __max_work_group_kernel_query =
+        __kernel.template ext_oneapi_get_info<syclex::info::kernel_queue_specific::max_num_work_groups>(
+            __q, __work_group_size, __slm_size_bytes);
+
+    // There is a bug produced on BMG where zeKernelSuggestMaxCooperativeGroupCount suggests too large of a
+    // work-group count when we are beyond half SLM capacity, causing a hang. To fix this, we can manually compute
+    // the safe number of groups to launch and take the min with the root group query for any kernel specific
+    // restrictions that may limit the number of groups
+    constexpr std::uint32_t __xve_per_xe = 8;
+    constexpr std::uint32_t __lanes_per_xe = 2048;
+    const std::uint32_t __max_groups_per_xe = __lanes_per_xe / __work_group_size;
+
+    const std::uint32_t __max_slm_xe = __q.get_device().get_info<sycl::info::device::local_mem_size>();
+    const std::uint32_t __xes_on_device =
+        __q.get_device().get_info<sycl::info::device::max_compute_units>() / __xve_per_xe;
+
+    // The HW reserves SLM for a work group on a limited number of granularities. We must account for this to avoid
+    // launching too many groups.
+    constexpr std::uint32_t __kib = 1 << 10;
+    constexpr std::uint32_t __slm_granularity_table[] = {0,          1 * __kib,  2 * __kib,  4 * __kib,
+                                                         8 * __kib,  16 * __kib, 24 * __kib, 32 * __kib,
+                                                         48 * __kib, 64 * __kib, 96 * __kib, 128 * __kib};
+    constexpr std::uint32_t __slm_granularity_table_size = sizeof(__slm_granularity_table) / sizeof(std::uint32_t);
+    const std::uint32_t* __slm_granularity_it = std::lower_bound(
+        __slm_granularity_table, __slm_granularity_table + __slm_granularity_table_size, __slm_size_bytes);
+    assert(__slm_granularity_it != std::cend(__slm_granularity_table));
+    const std::uint32_t __true_slm_size_bytes = *__slm_granularity_it;
+
+    const std::uint32_t __groups_per_xe_slm_adj = std::min(__max_groups_per_xe, __max_slm_xe / __true_slm_size_bytes);
+    const std::uint32_t __concurrent_groups_est = __groups_per_xe_slm_adj * __xes_on_device;
+    __max_num_cooperative_groups = std::min({__max_work_group_kernel_query, __tile_count, __concurrent_groups_est});
+#else
+    static_assert(oneapi::dpl::__internal::__always_false_v<_Kernel>,
+                  "_ONEDPL_KT_COOPERATIVE_KERNELS_PRESENT must be defined to call __get_num_cooperative_groups");
+#endif
+    return __max_num_cooperative_groups;
+}
+
+} // namespace oneapi::dpl::experimental::kt::gpu::__impl
+
+#endif
---
+++ include/oneapi/dpl/experimental/kt/sycl_radix_sort.h
@@ -0,0 +1,220 @@
+// -*- C++ -*-
+//===-- sycl_radix_sort.h ------------------------------------------------===//
+//
+// Copyright (C) UXL Foundation Contributors
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _ONEDPL_KT_SYCL_RADIX_SORT_H
+#define _ONEDPL_KT_SYCL_RADIX_SORT_H
+
+#include <cstdint>
+#include <type_traits>
+
+#include "../../pstl/hetero/dpcpp/utils_ranges_sycl.h"
+#include "internal/kt_defs.h"
+#include "internal/radix_sort_utils.h"
+#include "internal/radix_sort_dispatchers.h"
+
+namespace oneapi::dpl::experimental::kt::gpu
+{
+// TODO: make sure to provide sufficient diagnostic if input does not allow either reading or writing
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysRng>
+std::enable_if_t<!oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysRng>, sycl::event>
+radix_sort(sycl::queue __q, _KeysRng&& __keys_rng, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_rng.size();
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n < 2)
+        return {};
+
+    auto __pack = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng>(__keys_rng))};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/true>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, __pack, __pack, __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysRng>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysIterator>
+std::enable_if_t<oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysIterator>, sycl::event>
+radix_sort(sycl::queue __q, _KeysIterator __keys_first, _KeysIterator __keys_last, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_last - __keys_first;
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n < 2)
+        return {};
+
+    auto __keys_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_rng = __keys_keep(__keys_first, __keys_last).all_view();
+    auto __pack = __impl::__rng_pack{std::move(__keys_rng)};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/true>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, __pack, __pack, __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysIterator>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysRng,
+          typename _ValsRng>
+std::enable_if_t<!oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysRng>, sycl::event>
+radix_sort_by_key(sycl::queue __q, _KeysRng&& __keys_rng, _ValsRng&& __vals_rng, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_rng.size();
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n < 2)
+        return {};
+
+    auto __pack = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng>(__keys_rng)),
+                                     oneapi::dpl::__ranges::views::all(std::forward<_ValsRng>(__vals_rng))};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/true>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, __pack, __pack, __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysRng>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysIterator,
+          typename _ValsIterator>
+std::enable_if_t<oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysIterator>, sycl::event>
+radix_sort_by_key(sycl::queue __q, _KeysIterator __keys_first, _KeysIterator __keys_last, _ValsIterator __vals_first,
+                  _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_last - __keys_first;
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n < 2)
+        return {};
+
+    auto __keys_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_rng = __keys_keep(__keys_first, __keys_last).all_view();
+
+    auto __vals_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __vals_rng = __vals_keep(__vals_first, __vals_first + __n).all_view();
+    auto __pack = __impl::__rng_pack{std::move(__keys_rng), std::move(__vals_rng)};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/true>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, __pack, __pack, __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysIterator>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysRng1,
+          typename _KeysRng2>
+std::enable_if_t<!oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysRng1>, sycl::event>
+radix_sort(sycl::queue __q, _KeysRng1&& __keys_rng, _KeysRng2&& __keys_rng_out, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_rng.size();
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+    if (__n == 0)
+        return {};
+
+    auto __pack = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng1>(__keys_rng))};
+    auto __pack_out = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng2>(__keys_rng_out))};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/false>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, std::move(__pack), std::move(__pack_out),
+        __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysRng1>();
+
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysIterator1,
+          typename _KeysIterator2>
+std::enable_if_t<oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysIterator1>, sycl::event>
+radix_sort(sycl::queue __q, _KeysIterator1 __keys_first, _KeysIterator1 __keys_last, _KeysIterator2 __keys_out_first,
+           _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_last - __keys_first;
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n == 0)
+        return {};
+
+    auto __keys_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_rng = __keys_keep(__keys_first, __keys_last).all_view();
+    auto __pack = __impl::__rng_pack{std::move(__keys_rng)};
+    auto __keys_out_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_out_rng = __keys_out_keep(__keys_out_first, __keys_out_first + __n).all_view();
+    auto __pack_out = __impl::__rng_pack{std::move(__keys_out_rng)};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/false>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, std::move(__pack), std::move(__pack_out),
+        __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysIterator1>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysRng1,
+          typename _ValsRng1, typename _KeysRng2, typename _ValsRng2>
+std::enable_if_t<!oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysRng1>, sycl::event>
+radix_sort_by_key(sycl::queue __q, _KeysRng1&& __keys_rng, _ValsRng1&& __vals_rng, _KeysRng2&& __keys_out_rng,
+                  _ValsRng2&& __vals_out_rng, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_rng.size();
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+    if (__n == 0)
+        return {};
+
+    auto __pack = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng1>(__keys_rng)),
+                                     oneapi::dpl::__ranges::views::all(std::forward<_ValsRng1>(__vals_rng))};
+    auto __pack_out = __impl::__rng_pack{oneapi::dpl::__ranges::views::all(std::forward<_KeysRng2>(__keys_out_rng)),
+                                         oneapi::dpl::__ranges::views::all(std::forward<_ValsRng2>(__vals_out_rng))};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/false>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, std::move(__pack), std::move(__pack_out),
+        __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysRng1>();
+#endif
+}
+
+template <bool __is_ascending = true, std::uint8_t __radix_bits = 8, typename _KernelParam, typename _KeysIterator1,
+          typename _ValsIterator1, typename _KeysIterator2, typename _ValsIterator2>
+std::enable_if_t<oneapi::dpl::__internal::__is_type_with_iterator_traits_v<_KeysIterator1>, sycl::event>
+radix_sort_by_key(sycl::queue __q, _KeysIterator1 __keys_first, _KeysIterator1 __keys_last, _ValsIterator1 __vals_first,
+                  _KeysIterator2 __keys_out_first, _ValsIterator2 __vals_out_first, _KernelParam __param = {})
+{
+#if _ONEDPL_ENABLE_SYCL_RADIX_SORT_KT
+    auto __n = __keys_last - __keys_first;
+    __impl::__check_sycl_sort_params<__radix_bits, _KernelParam::data_per_workitem, _KernelParam::workgroup_size>(__n);
+
+    if (__n == 0)
+        return {};
+
+    auto __keys_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_rng = __keys_keep(__keys_first, __keys_last).all_view();
+
+    auto __vals_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __vals_rng = __vals_keep(__vals_first, __vals_first + __n).all_view();
+    auto __pack = __impl::__rng_pack{std::move(__keys_rng), std::move(__vals_rng)};
+
+    auto __keys_out_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __keys_out_rng = __keys_out_keep(__keys_out_first, __keys_out_first + __n).all_view();
+
+    auto __vals_out_keep = oneapi::dpl::__ranges::__get_sycl_range<sycl::access_mode::read_write>();
+    auto __vals_out_rng = __vals_out_keep(__vals_out_first, __vals_out_first + __n).all_view();
+    auto __pack_out = __impl::__rng_pack{std::move(__keys_out_rng), std::move(__vals_out_rng)};
+    return __impl::__radix_sort<__is_ascending, __radix_bits, /*__in_place=*/false>(
+        oneapi::dpl::experimental::kt::gpu::__impl::__sycl_tag{}, __q, std::move(__pack), std::move(__pack_out),
+        __param);
+#else
+    __impl::__sycl_radix_sort_unsupported_msg<_KeysIterator1>();
+#endif
+}
+
+} // namespace oneapi::dpl::experimental::kt::gpu
+
+#endif // _ONEDPL_KT_SYCL_RADIX_SORT_H
