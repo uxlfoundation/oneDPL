f80ed0c0b4db0de7dd345d7f18906ed67e79705c
Sergey Kopienko <sergey.kopienko@intel.com>
2026-03-17 15:50:35 +0100

    include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h - fix call of __parallel_transform_scan in __pattern_transform_scan_base + __hetero_tag<_BackendTag>
       - as far as the function __parallel_transform_scan() now return result with the amount of saved data items into output range let's use it

--- include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
@@ -97,13 +97,10 @@ __pattern_transform_scan_base(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __ex
     if (__n == 0)
         return 0;
 
-    oneapi::dpl::__par_backend_hetero::__parallel_transform_scan</*_Bounded*/ true>(
-        _BackendTag{}, std::forward<_ExecutionPolicy>(__exec), std::forward<_Range1>(__rng1),
-        std::forward<_Range2>(__rng2), __n, __unary_op, __init, __binary_op, _Inclusive{})
-        .__checked_deferrable_wait();
-
-    // KSATODO : probably this return value is not quite correct if __rng2 is too small
-    return __n;
+    return oneapi::dpl::__par_backend_hetero::__parallel_transform_scan</*_Bounded*/ true>(
+               _BackendTag{}, std::forward<_ExecutionPolicy>(__exec), std::forward<_Range1>(__rng1),
+               std::forward<_Range2>(__rng2), __n, __unary_op, __init, __binary_op, _Inclusive{})
+        .get();
 }
 
 template <typename _BackendTag, typename _ExecutionPolicy, typename _Range1, typename _Range2, typename _UnaryOperation,
