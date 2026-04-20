acdc14d37fc4de8cfb54c806524476d49ef91b55
Alexey Kukanov <alexey.kukanov@intel.com>
2026-04-20 22:05:07 +0200

    Change file headers

--- test/parallel_api/ranges/std_ranges_replace_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_replace_copy_if.pass.cpp
@@ -1,17 +1,11 @@
 // -*- C++ -*-
-//===----------------------------------------------------------------------===//
+//===------------------------------------------------------===//
 //
-// Copyright (C) Intel Corporation
+// Copyright (C) UXL Foundation Contributors
 //
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// This file incorporates work covered by the following copyright and permission
-// notice:
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-//
-//===----------------------------------------------------------------------===//
+//===------------------------------------------------------===//
 
 #include "std_ranges_test.h"
 
