98e0bb9620a8af547227054b3792d66b836d38e7
Sergey Kopienko <sergey.kopienko@intel.com>
2026-04-22 13:42:47 +0200

    include/oneapi/dpl/internal/async_impl/async_impl_hetero.h - fix compile error

--- include/oneapi/dpl/internal/async_impl/async_impl_hetero.h
+++ include/oneapi/dpl/internal/async_impl/async_impl_hetero.h
@@ -203,7 +203,8 @@ __pattern_transform_scan_base_async(__hetero_tag<_BackendTag>, _ExecutionPolicy&
                   "__parallel_transform_scan<_Bounded=false> must return a 2-element tuple."
                   "A 3-element tuple would cause the stop_pos storage to be destroyed before the kernel completes.");
 
-    return __create_future(/*event*/ std::move(std::get<0>(__res)), __result + __n, /*result*/ std::move(std::get<1>(__res)))
+    return __create_future(/*event*/ std::move(std::get<0>(__res)), __result + __n,
+                           /*result*/ std::move(std::get<1>(__res)));
 }
 
 template <typename _BackendTag, typename _ExecutionPolicy, typename _Iterator1, typename _Iterator2,
