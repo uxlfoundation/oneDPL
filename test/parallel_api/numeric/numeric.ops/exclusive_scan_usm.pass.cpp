5485206ca9ecc8cc271975849281780fb4d35364
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-04-30 11:58:15 -0400

    further scan splitting
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

---
+++ test/parallel_api/numeric/numeric.ops/exclusive_scan_usm.pass.cpp
@@ -0,0 +1,246 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_config.h"
+
+#include "oneapi/dpl/execution"
+#include "oneapi/dpl/numeric"
+#include "oneapi/dpl/iterator"
+
+#include "support/utils.h"
+#include "support/utils_invoke.h" // CLONE_TEST_POLICY_IDX
+#include "support/scan_serial_impl.h"
+
+using namespace TestUtils;
+
+#if TEST_DPCPP_BACKEND_PRESENT
+static const char kMsgExclusiveScanNormal [] = "Wrong effect from exclusive scan (non-inplace)";
+static const char kMsgExclusiveScanInplace[] = "Wrong effect from exclusive scan (inplace)";
+
+// TODO: replace data generation with random data and update check to compare result to
+// the result of a serial implementation of the algorithm
+template <typename Iterator1, typename Size>
+void
+initialize_data(Iterator1 host_keys, Size n)
+{
+    const Size kStartVal = 1;
+    for (Size i = 0; i != n; ++i)
+        host_keys[i] = kStartVal + i;
+}
+
+template <int InitValue>
+struct TestingAlgoritmExclusiveScan
+{
+    template <typename... TArgs>
+    void
+    call_onedpl(TArgs&&... args)
+    {
+        oneapi::dpl::exclusive_scan(std::forward<TArgs>(args)..., InitValue);
+    }
+
+    template <typename... TArgs>
+    void
+    call_serial(TArgs&&... args)
+    {
+        exclusive_scan_serial(std::forward<TArgs>(args)..., InitValue);
+    }
+
+    const char*
+    getMsg(bool bInplace) const
+    {
+        return bInplace ? kMsgExclusiveScanInplace : kMsgExclusiveScanNormal;
+    }
+};
+
+template <int InitValue, typename BinaryOp>
+struct TestingAlgoritmExclusiveScanExt
+{
+    template <typename... TArgs>
+    void
+    call_onedpl(TArgs&&... args)
+    {
+        oneapi::dpl::exclusive_scan(std::forward<TArgs>(args)..., InitValue, BinaryOp());
+    }
+
+    template <typename... TArgs>
+    void
+    call_serial(TArgs&&... args)
+    {
+        exclusive_scan_serial(std::forward<TArgs>(args)..., InitValue, BinaryOp());
+    }
+
+    const char*
+    getMsg(bool bInplace) const
+    {
+        return bInplace ? kMsgExclusiveScanInplace : kMsgExclusiveScanNormal;
+    }
+};
+
+DEFINE_TEST_1(test_scan_non_inplace, TestingAlgoritm)
+{
+    DEFINE_TEST_CONSTRUCTOR(test_scan_non_inplace, 1.0f, 1.0f)
+
+    // specialization for hetero policy
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Size>
+    std::enable_if_t<
+        oneapi::dpl::__internal::__is_hetero_execution_policy_v<std::decay_t<Policy>> &&
+            is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&& exec,
+               Iterator1 keys_first, Iterator1 keys_last,
+               Iterator2 vals_first, Iterator2 /*vals_last*/,
+               Size n)
+    {
+        using ValT = typename std::iterator_traits<Iterator2>::value_type;
+
+        TestingAlgoritm testingAlgo;
+
+        TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);
+        TestDataTransfer<UDTKind::eVals, Size> host_vals(*this, n);
+
+        initialize_data(host_keys.get(), n);
+        update_data(host_keys);
+
+        testingAlgo.call_onedpl(CLONE_TEST_POLICY_IDX(exec, 0), keys_first, keys_last, vals_first);
+
+        retrieve_data(host_vals);
+
+        std::vector<ValT> expected(n);
+        testingAlgo.call_serial(host_keys.get(), host_keys.get() + n, expected.data());
+        EXPECT_EQ_N(expected.cbegin(), host_vals.get(), n, TestingAlgoritm().getMsg(false));
+    }
+
+    // specialization for host execution policies
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Size>
+    std::enable_if_t<
+        !oneapi::dpl::__internal::__is_hetero_execution_policy_v<std::decay_t<Policy>> &&
+            is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&& exec,
+               Iterator1 keys_first, Iterator1 keys_last,
+               Iterator2 vals_first, Iterator2 /*vals_last*/,
+               Size n)
+    {
+        using ValT = typename std::iterator_traits<Iterator2>::value_type;
+
+        TestingAlgoritm testingAlgo;
+
+        initialize_data(keys_first, n);
+
+        testingAlgo.call_onedpl(std::forward<Policy>(exec), keys_first, keys_last, vals_first);
+
+        std::vector<ValT> expected(n);
+        testingAlgo.call_serial(keys_first, keys_last + n, expected.data());
+        EXPECT_EQ_N(expected.cbegin(), vals_first, n, TestingAlgoritm().getMsg(false));
+    }
+
+    // specialization for non-random_access iterators
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Size>
+    std::enable_if_t<!is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&&, Iterator1, Iterator1, Iterator2, Iterator2, Size)
+    {
+    }
+};
+
+DEFINE_TEST_1(test_scan_inplace, TestingAlgoritm)
+{
+    DEFINE_TEST_CONSTRUCTOR(test_scan_inplace, 1.0f, 1.0f)
+
+    // specialization for host execution policies
+    template <typename Policy, typename Iterator1, typename Size>
+    std::enable_if_t<
+        !oneapi::dpl::__internal::__is_hetero_execution_policy_v<std::decay_t<Policy>> &&
+            is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last, Size n)
+    {
+        using KeyT = typename std::iterator_traits<Iterator1>::value_type;
+
+        TestingAlgoritm testingAlgo;
+
+        initialize_data(keys_first, n);
+        const std::vector<KeyT> source_host_keys_state(keys_first, keys_first + n);
+
+        testingAlgo.call_onedpl(std::forward<Policy>(exec), keys_first, keys_last, keys_first);
+
+        std::vector<KeyT> expected(n);
+        testingAlgo.call_serial(source_host_keys_state.cbegin(), source_host_keys_state.cend(), expected.data());
+        EXPECT_EQ_N(expected.cbegin(), keys_first, n, testingAlgo.getMsg(true));
+    }
+
+    // specialization for hetero policy
+    template <typename Policy, typename Iterator1, typename Size>
+    std::enable_if_t<
+        oneapi::dpl::__internal::__is_hetero_execution_policy_v<std::decay_t<Policy>> &&
+            is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last,
+               Size n)
+    {
+        using KeyT = typename std::iterator_traits<Iterator1>::value_type;
+
+        TestingAlgoritm testingAlgo;
+
+        TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);
+
+        initialize_data(host_keys.get(), n);
+        const std::vector<KeyT> source_host_keys_state(host_keys.get(), host_keys.get() + n);
+
+        update_data(host_keys);
+
+        testingAlgo.call_onedpl(CLONE_TEST_POLICY_IDX(exec, 0), keys_first, keys_last, keys_first);
+
+        retrieve_data(host_keys);
+
+        std::vector<KeyT> expected(n);
+        testingAlgo.call_serial(source_host_keys_state.cbegin(), source_host_keys_state.cend(), expected.data());
+        EXPECT_EQ_N(expected.cbegin(), host_keys.get(), n, testingAlgo.getMsg(true));
+    }
+
+    // specialization for non-random_access iterators
+    template <typename Policy, typename Iterator1, typename Size>
+    std::enable_if_t<!is_base_of_iterator_category_v<std::random_access_iterator_tag, Iterator1>>
+    operator()(Policy&&, Iterator1, Iterator1, Size)
+    {
+    }
+};
+
+template <sycl::usm::alloc alloc_type, typename ValueType, typename BinaryOperation>
+void
+run_test()
+{
+    // Non inplace
+    test2buffers<alloc_type, test_scan_non_inplace<ValueType, TestingAlgoritmExclusiveScan<2>>>();
+    test2buffers<alloc_type, test_scan_non_inplace<ValueType, TestingAlgoritmExclusiveScanExt<2, BinaryOperation > > >();
+
+    // Inplace
+    test1buffer<alloc_type, test_scan_inplace<ValueType, TestingAlgoritmExclusiveScan<2>>>();
+    test1buffer<alloc_type, test_scan_inplace<ValueType, TestingAlgoritmExclusiveScanExt<2, BinaryOperation>>>();
+}
+#endif // TEST_DPCPP_BACKEND_PRESENT
+
+int
+main()
+{
+#if TEST_DPCPP_BACKEND_PRESENT
+
+    using ValueType = int;
+    using BinaryOperation = std::plus<ValueType>;
+
+    // Run tests for USM shared memory
+    run_test<sycl::usm::alloc::shared, ValueType, BinaryOperation>();
+    // Run tests for USM device memory
+    run_test<sycl::usm::alloc::device, ValueType, BinaryOperation>();
+
+#endif // TEST_DPCPP_BACKEND_PRESENT
+
+    return TestUtils::done(TEST_DPCPP_BACKEND_PRESENT);
+}
---
+++ test/parallel_api/numeric/numeric.ops/inclusive_scan_multiplies.pass.cpp
@@ -0,0 +1,91 @@
+// -*- C++ -*-
+//===-- inclusive_scan_multiplies.pass.cpp --------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_config.h"
+
+#include _PSTL_TEST_HEADER(execution)
+#include _PSTL_TEST_HEADER(numeric)
+
+#include "support/utils.h"
+#include "support/scan_serial_impl.h"
+
+#include <random>
+#include <algorithm>
+#include <cstdint>
+
+using namespace TestUtils;
+
+template <typename Type>
+struct test_inclusive_scan_with_binary_op
+{
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Iterator3, typename Size, typename T,
+              typename BinaryOp>
+    std::enable_if_t<!TestUtils::is_reverse_v<Iterator1>>
+    operator()(Policy&& exec, Iterator1 in_first, Iterator1 in_last, Iterator2 out_first, Iterator2 out_last,
+               Iterator3 expected_first, Iterator3 /* expected_last */, Size n, T init, BinaryOp binary_op, T trash)
+    {
+        using namespace std;
+
+        inclusive_scan_serial(in_first, in_last, expected_first, binary_op, init);
+        auto orr = inclusive_scan(std::forward<Policy>(exec), in_first, in_last, out_first, binary_op, init);
+
+        EXPECT_TRUE(out_last == orr, "inclusive_scan with binary operator returned wrong iterator");
+        EXPECT_EQ_N(expected_first, out_first, n, "wrong result from inclusive_scan with binary operator");
+        std::fill_n(out_first, n, trash);
+    }
+
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Iterator3, typename Size, typename T,
+              typename BinaryOp>
+    std::enable_if_t<TestUtils::is_reverse_v<Iterator1>>
+    operator()(Policy&& /* exec */, Iterator1 /* in_first */, Iterator1 /* in_last */, Iterator2 /* out_first */, Iterator2 /* out_last */,
+               Iterator3 /* expected_first */, Iterator3 /* expected_last */, Size /* n */, T /* init */, BinaryOp /* binary_op */, T /* trash */)
+    {
+    }
+};
+
+template <typename T>
+void
+test_with_multiplies()
+{
+#if TEST_DPCPP_BACKEND_PRESENT
+    T trash = 666;
+    T init = 1;
+    const std::size_t custom_item_count = 10;
+
+    for (size_t n = custom_item_count; n <= 100000; n = n <= 16 ? n + 1 : size_t(3.1415 * n))
+    {
+        Sequence<T> out(n, [&](size_t) { return trash; });
+        Sequence<T> expected(n, [&](size_t) { return trash; });
+
+        Sequence<T> in(n, [](size_t /*index*/) { return 1; });
+        std::size_t counter = 0;
+        std::generate_n(in.begin(), custom_item_count, [&counter]() { return (counter++) % 3 + 2; } );
+        std::default_random_engine gen{42};
+        std::shuffle(in.begin(), in.end(), gen);
+
+        invoke_on_all_hetero_policies<20>()(test_inclusive_scan_with_binary_op<T>(), in.begin(), in.end(),
+                                            out.begin(), out.end(), expected.begin(), expected.end(), in.size(),
+                                            init, std::multiplies{}, trash);
+    }
+#endif // TEST_DPCPP_BACKEND_PRESENT
+}
+
+int
+main()
+{
+    test_with_multiplies<std::uint64_t>();
+
+    return done();
+}
