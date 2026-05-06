b31b54ebc0952456f76489cffb6505cb72302519
Sergey Kopienko <sergey.kopienko@intel.com>
2026-05-06 14:55:59 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h - pass _ExecuteAssign state into struct __parallel_reduce_then_scan_scan_submitter::__scan_through_elements_helper() through __call_scan_through_elements_helper()

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
@@ -3086,11 +3086,14 @@ struct __parallel_reduce_then_scan_scan_submitter<_Bounded, __max_inputs_per_ite
                 const std::size_t __start_id = __subgroup_start_id + __sub_group_local_id;
 
                 auto __call_scan_through_elements_helper =
-                    [&](const auto& __sub_group, auto&& __out_rng_arg, bool __sub_group_carry_initialized_arg,
-                        auto& __sub_group_carry_arg, auto&& __temp_out_arg, auto& __processed_info_arg) {
+                    [&](auto __execute_assign_tag, const auto& __sub_group, auto&& __out_rng_arg,
+                        bool __sub_group_carry_initialized_arg, auto& __sub_group_carry_arg, auto&& __temp_out_arg,
+                        auto& __processed_info_arg) {
+                        constexpr bool _ExecuteAssign = decltype(__execute_assign_tag)::value;
+
                         if (__sub_group_carry_initialized_arg)
                         {
-                            __scan_through_elements_helper<_Bounded, __sub_group_size, __is_inclusive,
+                            __scan_through_elements_helper<_Bounded, _ExecuteAssign, __sub_group_size, __is_inclusive,
                                                            /*__init_present=*/true,
                                                            /*__capture_output=*/true, __max_inputs_per_item>(
                                 __sub_group, __gen_scan_input, __scan_input_transform, __reduce_op, __write_op,
@@ -3100,7 +3103,7 @@ struct __parallel_reduce_then_scan_scan_submitter<_Bounded, __max_inputs_per_ite
                         }
                         else // first group first block, no subgroup carry
                         {
-                            __scan_through_elements_helper<_Bounded, __sub_group_size, __is_inclusive,
+                            __scan_through_elements_helper<_Bounded, _ExecuteAssign, __sub_group_size, __is_inclusive,
                                                            /*__init_present=*/false,
                                                            /*__capture_output=*/true, __max_inputs_per_item>(
                                 __sub_group, __gen_scan_input, __scan_input_transform, __reduce_op, __write_op,
@@ -3122,8 +3125,9 @@ struct __parallel_reduce_then_scan_scan_submitter<_Bounded, __max_inputs_per_ite
                         _ProcessedInfo __processed_info{};
 
                         // The first normal call of __scan_through_elements_helper
-                        __call_scan_through_elements_helper(__sub_group, __out_rng, __sub_group_carry_initialized,
-                                                            __sub_group_carry, __temp_out, __processed_info);
+                        __call_scan_through_elements_helper(/*_ExecuteAssign*/ std::true_type{}, __sub_group, __out_rng,
+                                                            __sub_group_carry_initialized, __sub_group_carry,
+                                                            __temp_out, __processed_info);
                         if constexpr (__oob_replay_enabled)
                         {
                             __oob_reached_in_this_wi = __processed_info.get_oob_reached();
