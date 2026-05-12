fb85facc244196ec11d0616d26c820c01862e231
Sergey Kopienko <sergey.kopienko@intel.com>
2026-05-12 09:57:56 +0200

    test/support/utils.h - fix review comment: remove extra changes

--- test/support/utils.h
+++ test/support/utils.h
@@ -160,88 +160,31 @@ std::string log_value_title(TagActual)
     return " got ";
 }
 
-template <typename TStream, typename TValue>
-std::enable_if_t<!IsOutputStreamable<TValue, TStream>::value>
-log_value_to_stream(TStream& os, const TValue&, bool& commaNeeded)
-{
-    if (commaNeeded)
-        os << ",";
-
-    os << "(unable to log value)";
-
-    commaNeeded = true;
-}
-
-template <typename TValue>
-constexpr bool is_any_char_type_v =
-    std::is_same_v<TValue, char> || std::is_same_v<TValue, signed char> || std::is_same_v<TValue, unsigned char>
-#if defined(__cpp_char8_t)
-    || std::is_same_v<TValue, char8_t>
-#endif
-    || std::is_same_v<TValue, char16_t> || std::is_same_v<TValue, char32_t>;
-
-template <typename TStream, typename TValue>
-std::enable_if_t<IsOutputStreamable<TValue, TStream>::value && !std::is_enum_v<TValue> && !is_any_char_type_v<TValue>>
-log_value_to_stream(TStream& os, const TValue& value, bool& commaNeeded)
-{
-    if (commaNeeded)
-        os << ",";
-
-    os << std::boolalpha << value << std::noboolalpha;
-
-    commaNeeded = true;
-}
-
-template <typename TStream, typename TValue>
-std::enable_if_t<IsOutputStreamable<TValue, TStream>::value && is_any_char_type_v<TValue>>
-log_value_to_stream(TStream& os, const TValue& value, bool& commaNeeded)
-{
-    if (commaNeeded)
-        os << ",";
-
-    os << static_cast<std::uint32_t>(value);
-
-    commaNeeded = true;
-}
-
-template <typename TStream, typename TValue>
-std::enable_if_t<IsOutputStreamable<TValue, TStream>::value && std::is_enum_v<TValue> && !is_any_char_type_v<TValue>>
-log_value_to_stream(TStream& os, const TValue& value, bool& commaNeeded)
-{
-    log_value_to_stream(os, static_cast<std::underlying_type_t<TValue>>(value), commaNeeded);
-}
-
-template <typename TStream, typename... T>
-void
-log_value_to_stream(TStream& os, const oneapi::dpl::__internal::tuple<T...>& value, bool& commaNeeded)
-{
-    using std_tuple_t = typename oneapi::dpl::__internal::tuple<T...>::tuple_type;
-    std_tuple_t std_tuple = value;
-
-    if (commaNeeded)
-        os << ",";
-
-    bool bInternalCommaNeeded = false;
-    os << "(";
-    std::apply([&os, &bInternalCommaNeeded](
-                   const auto&... elems) { (log_value_to_stream(os, elems, bInternalCommaNeeded), ...); },
-               std_tuple);
-    os << ")";
-
-    commaNeeded = true;
-}
-
 template <typename TStream, typename Tag, typename TValue>
-void
-log_value(TStream& os, Tag, const TValue& value, bool commaNeeded)
+ void log_value(TStream& os, Tag, const TValue& value, bool bCommaNeeded)
 {
-    if (commaNeeded)
+    if (bCommaNeeded)
         os << ",";
-
     os << log_value_title(Tag{});
 
-    bool bInternalCommaNeeded = false;
-    log_value_to_stream(os, value, bInternalCommaNeeded);
+    if constexpr (IsOutputStreamable<TValue, decltype(os)>::value)
+    {
+        if constexpr (std::is_same_v<bool, std::decay_t<TValue>>)
+        {
+            if (value)
+                os << "true";
+            else
+                os << "false";
+        }
+        else
+        {
+            os << value;
+        }
+    }
+    else
+    {
+        os << "(unable to log value)";
+    }
 }
 
 // Do not change signature to const T&.
