f6ae57d5a9a51cc9395abf9a09aae1f034f8d92a
Sergey Kopienko <sergey.kopienko@intel.com>
2026-05-13 12:30:48 +0200

    Remove extra changes

--- include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
@@ -927,10 +927,11 @@ __pattern_partition_copy(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _
                                                            /*_IsNoInitRequested=*/true>();
     auto __buf2 = __keep2(__zipped_res, __zipped_res + __n);
 
-    auto __res = oneapi::dpl::__par_backend_hetero::__parallel_partition_copy(
+    auto __result = oneapi::dpl::__par_backend_hetero::__parallel_partition_copy(
         _BackendTag{}, std::forward<_ExecutionPolicy>(__exec), __buf1.all_view(), __buf2.all_view(), __pred);
 
-    _It1DifferenceType __num_true = __wait_and_get_result(std::get<0>(std::move(__res)), std::get<1>(std::move(__res)));
+    _It1DifferenceType __num_true =
+        __wait_and_get_result(std::get<0>(std::move(__result)), std::get<1>(std::move(__result))); // blocking call
 
     return std::make_pair(__result1 + __num_true, __result2 + (__last - __first - __num_true));
 }
