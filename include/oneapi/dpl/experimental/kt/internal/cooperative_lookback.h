5bfc1bd31aa796226422eb9ee9b11df4473ea744
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-05-18 16:56:10 -0400

    removing extra template variable
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/experimental/kt/internal/cooperative_lookback.h
+++ include/oneapi/dpl/experimental/kt/internal/cooperative_lookback.h
@@ -342,7 +342,7 @@ struct __cooperative_lookback
             if (__is_full_ballot_bits)
             {
                 oneapi::dpl::__par_backend_hetero::__sub_group_scan_partial<
-                    __sub_group_size, /*__use_subgroup_ops=*/true, /*__is_inclusive*/ true,
+                    __sub_group_size, /*__is_inclusive*/ true,
                     /*__init_present*/ decltype(__is_initialized)::value>(
                     __subgroup, __tile_value, __binary_op, __running, __lowest_item_with_full + 1,
                     static_cast<decltype(__tile_value)*>(nullptr));
@@ -351,7 +351,7 @@ struct __cooperative_lookback
             else
             {
                 oneapi::dpl::__par_backend_hetero::__sub_group_scan<
-                    __sub_group_size, /*__use_subgroup_ops=*/true, /*__is_inclusive*/ true,
+                    __sub_group_size, /*__is_inclusive*/ true,
                     /*__init_present*/ decltype(__is_initialized)::value>(
                     __subgroup, __tile_value, __binary_op, __running, static_cast<decltype(__tile_value)*>(nullptr));
                 return false;
