c1f8a8897c5d82217141b0aa822c3b4440aeee70
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-05-20 09:04:57 -0400

    fix macro to be if/else
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -2273,12 +2273,13 @@ __parallel_reduce_by_segment(oneapi::dpl::__internal::__device_backend_tag, _Exe
     // Because our init type ends up being tuple<std::size_t, ValType>, return the first component which is the write index. Add 1 to return the
     // past-the-end iterator pair of segmented reduction.
     return std::get<0>(__res.get()) + 1;
-#endif
+#else
     return __parallel_reduce_by_segment_fallback(
         oneapi::dpl::__internal::__device_backend_tag{}, std::forward<_ExecutionPolicy>(__exec),
         std::forward<_Range1>(__keys), std::forward<_Range2>(__values), std::forward<_Range3>(__out_keys),
         std::forward<_Range4>(__out_values), __binary_pred, __binary_op,
         oneapi::dpl::unseq_backend::__has_known_identity<_BinaryOperator, __val_type>{});
+#endif
 }
 
 //------------------------------------------------------------------------
