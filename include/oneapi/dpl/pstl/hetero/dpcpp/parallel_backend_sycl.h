63678ee428ce79e0517ac1777eacdcb0d1d1b577
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-05-26 16:30:58 -0400

    fix for unused param
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -783,18 +783,18 @@ __parallel_reduce_then_scan_copy(sycl::queue& __q, _InRng&& __in_rng, _OutRng&& 
 
     const std::size_t __n = oneapi::dpl::__ranges::__size(__in_rng);
 
-    auto __get_transform_result = [&__out_rng]() {
+    auto __transform_result = []([[maybe_unused]] auto&& __rng) {
         if constexpr (_Bounded)
-            return __clamp_max<_Size>{oneapi::dpl::__ranges::__size(__out_rng)};
+            return __clamp_max<_Size>{oneapi::dpl::__ranges::__size(__rng)};
         else
             return oneapi::dpl::identity{};
-    };
+    }(__out_rng);
 
     return __parallel_transform_reduce_then_scan<_Bounded, sizeof(_Size), _CustomName>(
         __q, __n, std::forward<_InRng>(__in_rng), std::forward<_OutRng>(__out_rng), _GenReduceInput{__generate_mask},
         _ReduceOp{}, _GenScanInput{__generate_mask}, _ScanInputTransform{}, __write_op,
         oneapi::dpl::unseq_backend::__no_init_value<_Size>{},
-        /*_Inclusive=*/std::true_type{}, __is_unique_pattern, __get_transform_result());
+        /*_Inclusive=*/std::true_type{}, __is_unique_pattern, __transform_result);
 }
 
 template <typename _CustomName, typename _InRng, typename _OutRng, typename _Size, typename _IndexPred,
