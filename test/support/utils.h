d12875b6315d90d75c83c218d761757685a72107
Sergey Kopienko <sergey.kopienko@intel.com>
2026-06-05 14:19:26 +0200

    test/support/utils.h - re-design issue_error_message() function

--- test/support/utils.h
+++ test/support/utils.h
@@ -86,10 +86,9 @@ const_size(const T (&)[N]) noexcept
 // Issue error message from outstr, adding a newline.
 // Real purpose of this routine is to have a place to hang a breakpoint.
 inline void
-issue_error_message(std::stringstream& outstr)
+issue_error_message(const std::stringstream& outstr)
 {
-    outstr << std::endl;
-    std::cerr << outstr.str();
+    std::cerr << outstr.str() << std::endl;
     std::exit(EXIT_FAILURE);
 }
 
