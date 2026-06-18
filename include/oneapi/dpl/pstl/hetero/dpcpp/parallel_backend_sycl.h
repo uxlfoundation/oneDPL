e84d41273129e41e7adf9f6237079e0727b96d80
Sergey Kopienko <sergey.kopienko@intel.com>
2026-06-18 14:14:25 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h - fix review comment: use auto for result of __compute_stop_pos() call
    oneapi::dpl::__internal::tuple is a class template, so const oneapi::dpl::__internal::tuple __stop_pos_tuple = ...; is ill-formed (missing template arguments) and should not compile. Prefer auto (or spell the full tuple type) when capturing the result of __compute_stop_pos().

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1333,7 +1333,7 @@ __parallel_set_op(oneapi::dpl::__internal::__device_backend_tag, _SetTag __set_t
     if constexpr (_Bounded)
     {
         // Workaround because std::tie() doesn't work with oneapi::dpl::__internal::tuple
-        const oneapi::dpl::__internal::tuple __stop_pos_tuple = __load_result(std::get<2>(__res)).__compute_stop_pos();
+        const auto __stop_pos_tuple = __load_result(std::get<2>(__res)).__compute_stop_pos();
         __stop_pos1 = std::get<0>(__stop_pos_tuple);
         __stop_pos2 = std::get<1>(__stop_pos_tuple);
     }
