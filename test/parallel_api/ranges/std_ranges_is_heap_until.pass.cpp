f98b206d93624bc37f67507112602bc0c8164c2d
Sergey Kopienko <sergey.kopienko@intel.com>
2026-06-19 16:36:12 +0200

    test/parallel_api/ranges/std_ranges_is_heap_until.pass.cpp - new test for std::ranges::is_heap_until

--- test/parallel_api/ranges/std_ranges_is_heap_until.pass.cpp
+++ test/parallel_api/ranges/std_ranges_is_heap_until.pass.cpp
@@ -24,8 +24,20 @@ main()
 
     auto is_heap_until_checker = TEST_PREPARE_CALLABLE(std::ranges::is_heap_until);
 
-    //...
+    // A descending sequence is a max-heap (std::ranges::less); the spike at index 42 breaks the
+    // heap property there, so is_heap_until is expected to stop at a non-trivial position.
+    auto generator = [](auto i) { return i == 42 ? 1 : -i; };
+    test_range_algo<0, int, data_in, decltype(generator)>{big_sz}(
+        dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::less{});
+    test_range_algo<1>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::less{}, proj);
+    test_range_algo<2, P2>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::less{}, &P2::x);
+    test_range_algo<3, P2>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::less{}, &P2::proj);
 
+    test_range_algo<4>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::greater{}, proj);
+    test_range_algo<5, P2>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::greater{}, &P2::x);
+    test_range_algo<6, P2>{}(dpl_ranges::is_heap_until, is_heap_until_checker, std::ranges::greater{}, &P2::proj);
+
+    test_range_algo<7>{}(dpl_ranges::is_heap_until, is_heap_until_checker);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
