437672e25812a3d6017f418383e005cd84570126
Sergey Kopienko <sergey.kopienko@intel.com>
2026-06-19 16:34:36 +0200

    include/oneapi/dpl/pstl/glue_algorithm_ranges_defs.h - add forward declaration for struct __is_heap_fn

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_defs.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_defs.h
@@ -56,6 +56,7 @@ struct __stable_sort_leaf;
 struct __stable_sort_fn;
 struct __sort_leaf;
 struct __sort_fn;
+struct __is_heap_fn;
 struct __min_element_fn;
 struct __max_element_fn;
 struct __minmax_element_fn;
