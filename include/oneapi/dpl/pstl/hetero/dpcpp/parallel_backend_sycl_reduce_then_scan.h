94bee176d5c38cbd0e58cc4451a060a08b74ae8c
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-06-24 22:31:47 -0400

    accounting for the case where it does not fit
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan.h
@@ -2382,18 +2382,27 @@ __parallel_transform_reduce_then_scan_impl(sycl::queue& __q, const std::size_t _
 
         const std::size_t __last_level_cache_size_bytes = __q.get_device().template get_info<sycl::info::device::global_mem_cache_size>();
 
-        // start with some multiple of the number of Xe cores, and reduce until the input fits in the last level cache
+        // try to use 2x number of cores as they can be scheduled concurrently on an xe-core. If that does not fit in
+        // last level cache, check 1x
         __num_work_groups = __num_xe_cores * 2;
-        while (__last_level_cache_size_bytes < __bytes_per_work_item_iter * __work_group_size * __num_work_groups
-            && __num_work_groups > __num_xe_cores)
+        if (__last_level_cache_size_bytes < __bytes_per_work_item_iter * __work_group_size * __num_work_groups)
         {
             __num_work_groups -= __num_xe_cores;
         }
 
-        // maximize the number of inputs per work item while still fitting in the last level cache
-        __max_inputs_per_item =
-            std::max(std::uint16_t{1},
-                    std::uint16_t(__last_level_cache_size_bytes / (__bytes_per_work_item_iter * __work_group_size * __num_work_groups)));
+        if (__last_level_cache_size_bytes < __bytes_per_work_item_iter * __work_group_size * __num_work_groups)
+        {
+            // maximize the number of inputs per work item while still fitting in the last level cache
+            __max_inputs_per_item = std::uint16_t(__last_level_cache_size_bytes / (__bytes_per_work_item_iter * __work_group_size * __num_work_groups));
+        }
+        else
+        {
+            // If it still doesn't fit in last level cache, just use a single block and maximize the number of inputs
+            // per work item
+            __num_work_groups = __num_xe_cores * 2;
+            __max_inputs_per_item = oneapi::dpl::__internal::__dpl_ceil_div(__n, (__work_group_size * __num_work_groups));
+        }
+
     }
     else // target is cpu
     {
