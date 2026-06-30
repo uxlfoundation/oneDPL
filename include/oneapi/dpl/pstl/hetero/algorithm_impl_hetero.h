f43ec073dcd6f1e35b055d52619947adbcdd9574
Alexey Kukanov <alexey.kukanov@intel.com>
2026-06-30 19:48:09 +0200

    Correct the pivot point

--- include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_impl_hetero.h
@@ -1643,7 +1643,7 @@ __pattern_rotate(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Iterator
         // 1. In-place reverse of the ranges before and after the shift point, done in one kernel
         auto __dbrick = oneapi::dpl::__par_backend_hetero::__dual_brick{
             unseq_backend::__reverse_functor{__shift}, unseq_backend::__reverse_functor{__n - __shift, __shift},
-            __shift};
+            __shift / 2};
         oneapi::dpl::__par_backend_hetero::__parallel_for(
             _BackendTag{}, oneapi::dpl::__par_backend_hetero::make_wrapped_policy<__rotate_wrapper>(__exec), __dbrick,
             __n / 2, __buf.all_view())
