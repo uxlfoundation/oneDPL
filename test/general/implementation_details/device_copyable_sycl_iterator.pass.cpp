6cd0797d474dc115cad2016514f27e22c5a6d013
Dan Hoeflinger <dan.hoeflinger@intel.com>
2026-07-07 15:54:10 -0400

    standalone test device_copyable_sycl_iterator
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

---
+++ test/general/implementation_details/device_copyable_sycl_iterator.pass.cpp
@@ -0,0 +1,39 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_config.h"
+
+#include _PSTL_TEST_HEADER(execution)
+#include _PSTL_TEST_HEADER(algorithm)
+
+#include "support/utils.h"
+using namespace TestUtils;
+
+#if TEST_DPCPP_BACKEND_PRESENT
+
+#    include "support/utils_device_copyable.h"
+#    include "support/utils_sycl_defs.h"
+
+
+// The following is ill-advised, but we need to guard against such actions a user may take
+template <class T>
+requires(true)
+struct sycl::is_device_copyable<T> : std::true_type {};
+
+#endif // TEST_DPCPP_BACKEND_PRESENT
+
+std::int32_t
+main()
+{
+#if TEST_DPCPP_BACKEND_PRESENT
+    static_assert(!sycl::is_device_copyable_v<oneapi::dpl::__internal::sycl_iterator<sycl::access_mode::read, int>>,
+                  "sycl_iterator is device copyable but should never be");
+#endif // TEST_DPCPP_BACKEND_PRESENT
+    return done(TEST_DPCPP_BACKEND_PRESENT);
+}
