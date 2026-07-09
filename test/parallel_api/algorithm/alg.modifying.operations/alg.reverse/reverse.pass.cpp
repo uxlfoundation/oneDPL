f983368889cd20f68160b56a4c02c50a8cc6a82b
Alexey Kukanov <alexey.kukanov@intel.com>
2026-07-09 19:20:27 +0200

    Add another data size to test with

--- test/parallel_api/algorithm/alg.modifying.operations/alg.reverse/reverse.pass.cpp
+++ test/parallel_api/algorithm/alg.modifying.operations/alg.reverse/reverse.pass.cpp
@@ -70,9 +70,11 @@ void
 test()
 {
     std::vector<std::size_t> test_sizes = TestUtils::get_pattern_for_test_sizes();
-    const std::size_t max_len = test_sizes.back();
+    std::size_t max_len = test_sizes.back();
     // Add a value to cover the case of len modulo 8 == 3
-    test_sizes.push_back((max_len / 8) * 8 - 5);
+    max_len = (max_len / 8) * 8 + 11;
+    test_sizes.push_back(max_len);
+    test_sizes.push_back(max_len - 16);
 
     Sequence<T> actual(max_len);
     Sequence<T> data(max_len, [](std::size_t i) { return T(i); });
