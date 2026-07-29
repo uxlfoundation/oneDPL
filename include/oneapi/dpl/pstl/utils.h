28b07e1190bfc5dc8bc28588c7538952030790ea
Sergey Kopienko <sergey.kopienko@intel.com>
2026-07-29 16:01:59 +0200

    include/oneapi/dpl/pstl/utils.h - introduce oneapi::dpl::__iternal::__cmp_less()

--- include/oneapi/dpl/pstl/utils.h
+++ include/oneapi/dpl/pstl/utils.h
@@ -668,6 +668,19 @@ __dpl_signbit(const _T& __x)
     return (__x & __mask) != 0;
 }
 
+// This prevents warning about comparison of integer expressions of different signedness
+template <typename T, typename U>
+constexpr std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+__cmp_less(T __t, U __u)
+{
+    if constexpr (std::is_signed_v<T> == std::is_signed_v<U>)
+        return __t < __u;
+    else if constexpr (std::is_signed_v<T>)
+        return __t < 0 || static_cast<std::make_unsigned_t<T>>(__t) < __u;
+    else
+        return __u >= 0 && __t < static_cast<std::make_unsigned_t<U>>(__u);
+}
+
 template <typename _Size, typename _Comparator>
 _Size
 __pstl_lower_bound_impl(_Size __first, _Size __last, _Comparator&& __comp)
