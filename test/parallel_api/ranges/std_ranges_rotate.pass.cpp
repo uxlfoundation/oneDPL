7a51749924bd6222042e68afa1eb2543609a70e0
Alexey Kukanov <alexey.kukanov@intel.com>
2026-08-03 18:55:23 +0200

    make return value type explicit in rotate_tester

--- test/parallel_api/ranges/std_ranges_rotate.pass.cpp
+++ test/parallel_api/ranges/std_ranges_rotate.pass.cpp
@@ -14,7 +14,8 @@
 struct
 {
     template <typename Policy, std::ranges::random_access_range Range>
-    auto operator()(Policy&& exec, Range&& r, int pivot_pos = -1)
+    std::ranges::borrowed_subrange_t<Range>
+    operator()(Policy&& exec, Range&& r, int pivot_pos = -1)
     {
         const int in_size = std::ranges::size(r);
         auto middle = std::ranges::begin(r) + ((pivot_pos < 0)? in_size/3 : std::min<int>(pivot_pos, in_size));
