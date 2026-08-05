e1de1dac6364721a4d04f854d4531dcc6476301c
Sergey Kopienko <sergey.kopienko@intel.com>
2026-08-05 16:43:33 +0200

    std_ranges_partial_sort.pass.cpp - add new test for std::ranges::partial_sort

---
+++ test/parallel_api/ranges/std_ranges_partial_sort.pass.cpp
@@ -0,0 +1,67 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "std_ranges_test.h"
+
+#if _ENABLE_STD_RANGES_TESTING
+template <std::ranges::range R>
+auto
+get_middle(R&& r)
+{
+    return std::ranges::begin(r) + std::ranges::size(r) / 2;
+}
+
+struct partial_sort_fn
+{
+    template <typename Policy, typename R, typename... Args>
+    std::ranges::borrowed_iterator_t<R>
+    operator()(Policy&& exec, R&& r, Args&&... args) const
+    {
+        auto middle = get_middle(r);
+        return oneapi::dpl::ranges::partial_sort(std::forward<Policy>(exec), std::forward<R>(r), middle,
+                                                 std::forward<Args>(args)...);
+    }
+};
+
+template <>
+constexpr std::pair<int, int>
+test_std_ranges::range_to_verify<partial_sort_fn>(int total_size, int /*result_size*/)
+{
+    return {0, total_size / 2};
+}
+#endif //_ENABLE_STD_RANGES_TESTING
+
+std::int32_t
+main()
+{
+#if _ENABLE_STD_RANGES_TESTING
+    using namespace test_std_ranges;
+
+    auto partial_sort_algo = partial_sort_fn{};
+
+    auto partial_sort_checker = [](auto&& r, auto&&... args) {
+        auto middle = get_middle(r);
+        return std::ranges::partial_sort(std::forward<decltype(r)>(r), middle, std::forward<decltype(args)>(args)...);
+    };
+
+    test_range_algo<0>{big_sz}(partial_sort_algo, partial_sort_checker);
+    test_range_algo<1>{}(partial_sort_algo, partial_sort_checker, std::ranges::less{});
+
+    test_range_algo<2>{}(partial_sort_algo, partial_sort_checker, std::ranges::less{}, proj);
+    test_range_algo<3>{}(partial_sort_algo, partial_sort_checker, std::ranges::greater{}, proj);
+
+    test_range_algo<4, P2>{}(partial_sort_algo, partial_sort_checker, std::ranges::less{}, &P2::x);
+    test_range_algo<5, P2>{}(partial_sort_algo, partial_sort_checker, std::ranges::greater{}, &P2::x);
+
+    test_range_algo<6, P2>{}(partial_sort_algo, partial_sort_checker, std::ranges::less{}, &P2::proj);
+    test_range_algo<7, P2>{}(partial_sort_algo, partial_sort_checker, std::ranges::greater{}, &P2::proj);
+#endif //_ENABLE_STD_RANGES_TESTING
+
+    return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
+}
