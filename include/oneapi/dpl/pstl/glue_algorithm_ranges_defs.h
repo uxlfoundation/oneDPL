cadab4cebc136d5a4985a38bf6eb02bfed61da0a
Sergey Kopienko <sergey.kopienko@intel.com>
2026-08-05 15:24:09 +0200

    glue_algorithm_ranges_defs.h - add declaration of struct __partial_sort_fn;

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_defs.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_defs.h
@@ -57,6 +57,7 @@ struct __stable_sort_leaf;
 struct __stable_sort_fn;
 struct __sort_leaf;
 struct __sort_fn;
+struct __partial_sort_fn;
 struct __is_heap_fn;
 struct __is_heap_until_fn;
 struct __min_element_fn;
