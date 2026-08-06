53b4c9c099488723c368777dabcc9b3c523c9f78
Alexey Kukanov <alexey.kukanov@intel.com>
2026-08-06 18:33:26 +0200

    use std::ranges algorithm for unseq

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -515,11 +515,13 @@ __pattern_partial_sort_copy_ranges(_Tag __tag, _ExecutionPolicy&& __exec, _R&& _
     return {__last, __out_finish};
 }
 
-template <typename _ExecutionPolicy, typename _R, typename _OutR, typename _Comp, typename _Proj1, typename _Proj2>
+template <typename _IsVector, typename _ExecutionPolicy, typename _R, typename _OutR, typename _Comp, typename _Proj1,
+          typename _Proj2>
 std::ranges::partial_sort_copy_result<std::ranges::borrowed_iterator_t<_R>, std::ranges::borrowed_iterator_t<_OutR>>
-__pattern_partial_sort_copy_ranges(__serial_tag</*IsVector*/ std::false_type>, _ExecutionPolicy&& __exec, _R&& __r,
+__pattern_partial_sort_copy_ranges(__serial_tag<_IsVector>, _ExecutionPolicy&& __exec, _R&& __r,
                                    _OutR&& __out_r, _Comp __comp, _Proj1 __proj1, _Proj2 __proj2)
 {
+    // Use the standard implementation for both seq and unseq policies
     return std::ranges::partial_sort_copy(
         std::forward<_R>(__r), std::forward<_OutR>(__out_r),__comp, __proj1, __proj2);
 }
