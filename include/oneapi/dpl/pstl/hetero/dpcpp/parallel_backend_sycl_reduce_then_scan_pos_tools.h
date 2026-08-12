aed4881a7c6ef3bffafa8698294b0577c29e6790
Sergey Kopienko <sergey.kopienko@intel.com>
2026-08-12 10:07:07 +0200

    parallel_backend_sycl_reduce_then_scan_pos_tools.h - apply __temp_data_array_idx_t in struct __src_pos_capturing_temp_data

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan_pos_tools.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce_then_scan_pos_tools.h
@@ -99,11 +99,11 @@ struct __src_pos_capturing_temp_data
     // We should capture source data indexes in this structure
     static constexpr bool __capture_indexes_flag = true;
 
-    __src_pos_capturing_temp_data(std::uint16_t __idx_for_src_pos) : __idx_for_src_pos(__idx_for_src_pos) {}
+    __src_pos_capturing_temp_data(__temp_data_array_idx_t __idx_for_src_pos) : __idx_for_src_pos(__idx_for_src_pos) {}
 
     template <typename _ValueT2>
     void
-    set(std::uint16_t __idx, const _ValueT2&, _FinalPosT __src_idx)
+    set(__temp_data_array_idx_t __idx, const _ValueT2&, _FinalPosT __src_idx)
     {
         if (__idx == __idx_for_src_pos)
             __saved_src_pos = __src_idx;
@@ -116,7 +116,7 @@ struct __src_pos_capturing_temp_data
     }
 
   private:
-    const std::uint16_t __idx_for_src_pos = 0;
+    const __temp_data_array_idx_t __idx_for_src_pos = 0;
     _FinalPosT __saved_src_pos = {};
 };
 
