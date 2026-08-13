1d3d7d1b50e84a798607d39a867c44d854c96576
Sobolev, Dmitriy <dmitriy.sobolev@intel.com>
2026-08-13 13:09:53 +0100

    Small namespace change

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1430,9 +1430,9 @@ struct __is_radix_sort_usable_for_type
 {
     static constexpr bool value =
 #if _ONEDPL_USE_RADIX_SORT
-        (::std::is_integral_v<_T> || oneapi::dpl::__internal::__is_radix_sort_float_v<_T>) &&
-        (__internal::__is_comp_ascending<::std::decay_t<_Compare>>::value ||
-         __internal::__is_comp_descending<::std::decay_t<_Compare>>::value);
+        (std::is_integral_v<_T> || __internal::__is_radix_sort_float_v<_T>) &&
+        (__internal::__is_comp_ascending<std::decay_t<_Compare>>::value ||
+         __internal::__is_comp_descending<std::decay_t<_Compare>>::value);
 #else
         false;
 #endif // _ONEDPL_USE_RADIX_SORT
