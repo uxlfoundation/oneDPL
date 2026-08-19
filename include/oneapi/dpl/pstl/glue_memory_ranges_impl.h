3bc4812ffd49407e4b286fb8b4249ee66894bfb8
Sergey Kopienko <sergey.kopienko@intel.com>
2026-08-19 17:59:31 +0200

    include/oneapi/dpl/pstl/glue_memory_ranges_impl.h - uninitialized_move - unify requires order
    // uninitialized_move
    template <typename ExecutionPolicy, std::random_access_range IR,
              /*nothrow-random-access-range*/ OR>
      requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>> &&
               std::ranges::sized_range<IR> && std::ranges::sized_range<OR> &&
               std::constructible_from<std::ranges::range_value_t<OR>,
                                       std::ranges::range_rvalue_reference_t<IR>>
      std::ranges::uninitialized_move_result<std::ranges::borrowed_iterator_t<IR>,
                                             std::ranges::borrowed_iterator_t<OR>>
        uninitialized_move (ExecutionPolicy&& pol, IR&& in_range, OR&& out_range);

--- include/oneapi/dpl/pstl/glue_memory_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_memory_ranges_impl.h
@@ -131,10 +131,10 @@ struct __uninitialized_move_fn
 {
     template <typename _ExecutionPolicy, std::ranges::random_access_range _InRange,
               __nothrow_random_access_range _OutRange>
-        requires std::constructible_from<std::ranges::range_value_t<_OutRange>,
-                                         std::ranges::range_rvalue_reference_t<_InRange>> &&
-                 oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> &&
-                 std::ranges::sized_range<_InRange> && std::ranges::sized_range<_OutRange>
+        requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> &&
+                 std::ranges::sized_range<_InRange> && std::ranges::sized_range<_OutRange> &&
+                 std::constructible_from<std::ranges::range_value_t<_OutRange>,
+                                         std::ranges::range_rvalue_reference_t<_InRange>>
 
     std::ranges::uninitialized_move_result<std::ranges::borrowed_iterator_t<_InRange>,
                                            std::ranges::borrowed_iterator_t<_OutRange>>
