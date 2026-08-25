94b1d64a5d3b53a3d01ea5ddc3b9501241d9e79c
Sergey Kopienko <sergey.kopienko@intel.com>
2026-08-25 15:57:18 +0200

    include/oneapi/dpl/pstl/unseq_backend_simd.h - add requirement into __simd_minmax_element() that value type should be default-constructible

--- include/oneapi/dpl/pstl/unseq_backend_simd.h
+++ include/oneapi/dpl/pstl/unseq_backend_simd.h
@@ -674,7 +674,8 @@ __simd_min_element(_ForwardIterator __first, _Size __n, _Compare __comp) noexcep
 // [restriction] - ::std::iterator_traits<_ForwardIterator>::value_type should be DefaultConstructible.
 // complexity [violation] - We will have at most (2*(__n-1) + 4*number_of_lanes) comparisons instead of at most [1.5*(__n-1)].
 template <typename _ForwardIterator, typename _Size, typename _Compare>
-::std::pair<_ForwardIterator, _ForwardIterator>
+std::enable_if_t<std::is_default_constructible_v<typename std::iterator_traits<_ForwardIterator>::value_type>,
+                 std::pair<_ForwardIterator, _ForwardIterator>>
 __simd_minmax_element(_ForwardIterator __first, _Size __n, _Compare __comp) noexcept
 {
     if (__n == 0)
