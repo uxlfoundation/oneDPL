d0bb5bd4334041d442cf37b31d998592eb46aeff
Alexey Kukanov <alexey.kukanov@intel.com>
2026-09-14 01:02:03 +0200

    [perf] change cache-per-iteration to a single input element

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -608,11 +608,9 @@ __parallel_compact_reduce_then_scan(sycl::queue& __q, _InRng&& __in_rng, _Size _
     using _GenScanInput = __par_backend_hetero::__gen_expand_count_mask_from_copy<_GenMask, _Size>;
     using _ScanInputTransform = __par_backend_hetero::__get_zeroth_element;
 
-    // Reduce reads one input element and stores it in the buffer, then scan reads the element from the buffer.
-    // To ensure the buffer is in cache, all input reads need to be in cache as well; but that will double
-    // kernel launches.
-    // TODO: check if performance difference between 1 and 2 element sizes warrants additional tuning
-    constexpr std::uint32_t __bytes_per_iter = sizeof(_ElementT) * 2;
+    // Each work-item iteration reads a single input element to evaluate the mask.
+    // While the value might be stored in a buffer and read from there, counting for that does not improve performance.
+    constexpr std::uint32_t __bytes_per_iter = sizeof(_ElementT);
     __transform_scan_storage_holder_simple<_Size> __holder(__q);
 
     sycl::event __event = __parallel_transform_reduce_then_scan</*_Bounded=*/false, __bytes_per_iter, _CustomName,
